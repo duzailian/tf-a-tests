b120873bd2b5ebbf337175cceb24b29ff494acda
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2018-11-30 10:51:26 +0000

    Makefile: Add target to build SP package
    Example command line:
    
        CROSS_COMPILE=aarch64-linux-gnu- make PLAT=fvp \
        DEBUG=1 LOG_LEVEL=50 TESTS=spm                 \
        tftf sp_package
    
    Also, add a disclaimer to explain that the current implementation of SPM
    is a prototype that is going to undergo a lot of rework.
    
    Change-Id: Iebc3ac28d20019b59f07f70f96fefc030f1a79ce
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

---
+++ spm/sp_package.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (c) 2018, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+CACTUS_DTB		:= $(BUILD_PLAT)/cactus/cactus.dtb
+IVY_DTB			:= $(BUILD_PLAT)/ivy/ivy.dtb
+SPTOOL			:= build/sptool
+
+$(CACTUS_DTB) : cactus
+	@echo "  DTBGEN  spm/cactus/cactus.dts"
+	${Q}bash tools/generate_dtb/generate_dtb.sh \
+		cactus spm/cactus/cactus.dts $(BUILD_PLAT)
+	@echo
+	@echo "Built $@ successfully"
+	@echo
+
+$(IVY_DTB) : ivy
+	@echo "  DTBGEN  spm/ivy/ivy.dts"
+	${Q}bash tools/generate_dtb/generate_dtb.sh \
+		ivy spm/ivy/ivy.dts $(BUILD_PLAT)
+	@echo
+	@echo "Built $@ successfully"
+	@echo
+
+$(SPTOOL):
+	${Q}make -C ../arm-trusted-firmware/tools/sptool
+	${Q}mv ../arm-trusted-firmware/tools/sptool/sptool $(SPTOOL)
+	@echo
+	@echo "Built $@ successfully"
+	@echo
+
+sp_package : $(CACTUS_DTB) $(IVY_DTB) build/sptool
+	${Q}$(SPTOOL)								\
+		-o $(BUILD_PLAT)/sp_package.bin					\
+		-i $(BUILD_PLAT)/cactus.bin:$(BUILD_PLAT)/cactus/cactus.dtb	\
+		-i $(BUILD_PLAT)/ivy.bin:$(BUILD_PLAT)/ivy/ivy.dtb
+	@echo
+	@echo "Built $@ successfully"
+	@echo
