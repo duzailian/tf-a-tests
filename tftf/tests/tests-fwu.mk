0b62890a7d64b4fb7cf102781f1de8612a757836
Paul Beesley <paul.beesley@arm.com>
2019-02-06 14:09:29 +0000

    Set required NVM defaults for FWU test suite
    The firmware update (FWU) test suite needs to retain its state across
    a system reboot. This means that it depends on the USE_NVM build
    variable being set to 1, and the NEW_TEST_SESSION being set to 0.
    Together these variables ensure that ongoing test results are stored in
    non-volatile memory (NVM), and that the NVM is not cleared between
    reboots.
    
    Currently these variables are set externally for this suite from the CI
    scripts but the correct behaviour should be for this suite's makefile
    to contain the correct defaults so that no external arguments are
    needed.
    
    Change-Id: I06da6f42ae72ad0485d22f8573a25e5a17505227
    Signed-off-by: Paul Beesley <paul.beesley@arm.com>

--- tftf/tests/tests-fwu.mk
+++ tftf/tests/tests-fwu.mk
@@ -10,3 +10,20 @@ TESTS_SOURCES	+=	$(addprefix tftf/tests/fwu_tests/,	\
 )
 
 TESTS_SOURCES	+=	plat/common/fwu_nvm_accessors.c
+
+#
+# By default, a new test session is initialized each time the platform is
+# booted, clearing any test state data stored in non-volatile memory (NVM).
+# Because this test suite depends on preserving the test state across reboots,
+# it is necessary to override the default behaviour and to prevent the NVM
+# from being wiped indiscriminately. Note that the NVM is still cleared at the
+# end of the test suite, after the last test has completed.
+#
+NEW_TEST_SESSION := 0
+
+#
+# By default, test results are stored in volatile memory as this is faster than
+# using non-volatile memory (NVM). Since these tests require the system to reset
+# the default behaviour must be to use the NVM instead.
+#
+USE_NVM := 1
