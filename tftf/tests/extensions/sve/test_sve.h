84954d45d37ca5b8e13a5a43a058553fe62d11b8
Ambroise Vincent <ambroise.vincent@arm.com>
2019-03-07 10:17:15 +0000

    Introduce test for SVE support
    When SVE extension is enabled, test that it correctly performs simple
    operations.
    
    Generating SVE instructions requires O3 compilation optimization. Since
    the build structure does not allow compilation flag modification for
    specific files, the function testing SVE support has been pre-compiled
    and added as an assembly file.
    
    Change-Id: Id9ba7d9e1de9bcbae3065cad2dd3e1dbe87ef03a
    Signed-off-by: Ambroise Vincent <ambroise.vincent@arm.com>

---
+++ tftf/tests/extensions/sve/test_sve.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef TEST_SVE_H
+#define TEST_SVE_H
+
+#define SVE_ARRAYSIZE 1024
+
+#endif /* TEST_SVE_H */
