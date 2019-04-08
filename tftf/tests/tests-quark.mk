5bd0300e683346902da1d6d5a30f0acc48374800
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2019-04-08 14:49:01 +0100

    quark: Introduce simple TFTF test
    Introduce test to verify that Quark is present in the system.
    
    Change-Id: Ia31e65e737b8909f7caecae830fcb217e88a9b01
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

---
+++ tftf/tests/tests-quark.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_SOURCES	+=							\
+	tftf/tests/runtime_services/secure_service/spci_helpers.c	\
+	tftf/tests/misc_tests/armv8_4_ttst.c
