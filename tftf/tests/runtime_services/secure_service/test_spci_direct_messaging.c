b0d264c081014ca27cc7a8e0426c1cc056900d60
Olivier Deprez <olivier.deprez@arm.com>
2019-12-06 17:45:07 +0100

    tftf: SPCI Beta1 add direct messaging test
    This patch strips out former SPCI Alpha sample test code. Removing SPRT
    references will be done in another coming patch. Version check is adapted
    to SPCI Beta1. Version is now returned in x2.
    
    The first test is a direct messaging test using SPCI_MSG_SEND_DIRECT_REQ
    targetting a bare-metal cactus SP. TFTF expects a response from the SP
    returning with SPCI_MSG_SEND_DIRECT_RESP.
    
    Note: this patch also provides an initial SPCI_RUN interface. This API
    may not be used in the mid-term because VM to SP communication is supposed
    to be done only through direct messaging. Though the SPM boot-up for now
    is only launching the first SP in the list of declared SP in SPMC manifest.
    In order to make 2nd-VM ready, TFTF has to "boot-up" the SP through a single
    SPCI_RUN invocation till it reaches SPCI_MSG_WAIT in the SP. Once SPM
    implements boot up through all SPs, this SPCI_RUN invocation will no longer
    be required.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I141abd3e348409b3d34a911d0552570f49e85846

---
+++ tftf/tests/runtime_services/secure_service/test_spci_direct_messaging.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2018-2020, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch_helpers.h>
+#include <cactus_def.h>
+#include <platform.h>
+#include <smccc.h>
+#include <spci_helpers.h>
+#include <spci_svc.h>
+#include <test_helpers.h>
+
+/* Hypervisor ID at physical SPCI instance */
+#define HYP_ID		(0)
+
+/* By convention, SP IDs (as opposed to VM IDs) have bit 15 set */
+#define SP_ID(x)	(x | (1 << 15))
+
+test_result_t test_spci_direct_messaging(void)
+{
+	smc_ret_values ret_values;
+
+	/**********************************************************************
+	 * Verify that SPCI is there and that it has the correct version.
+	 **********************************************************************/
+	SKIP_TEST_IF_SPCI_VERSION_LESS_THAN(0, 9);
+
+	/**********************************************************************
+	 * Send a message to SP1 through direct messaging
+	 **********************************************************************/
+
+	/* Send a message to SP1 through direct messaging */
+	ret_values = spci_msg_send_direct_req(HYP_ID, SP_ID(1), 0xaaaa0000);
+
+	/*
+	 * Return responses may be SPCI_MSG_SEND_DIRECT_RESP or SPCI_INTERRUPT,
+	 * but only expect the former. Expect SMC32 convention from SP.
+	 */
+	if (ret_values.ret0 != SPCI_MSG_SEND_DIRECT_RESP_SMC32) {
+		tftf_testcase_printf("spci_msg_send_direct_req returned %lx\n",
+				     (u_register_t)ret_values.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	/*
+	 * Message loop in SP returns initial message with the running VM id
+	 * into the lower 16 bits of initial message.
+	 */
+	if (ret_values.ret3 != 0xaaaa0001) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/**********************************************************************
+	 * Send a message to SP2 through direct messaging
+	 **********************************************************************/
+	/*
+	 * NOTICE: for now, the SPM does not initially run each SP sequentially
+	 * on boot up so we explicitelly run the SP once by invoking SPCI_RUN so
+	 * it reaches spci_msg_wait in the message loop function.
+	 */
+
+	/* Run SP2 on VCPU0 */
+	ret_values = spci_run(2 /*SP_ID(2)*/, 0);
+	if (ret_values.ret0 != SPCI_MSG_WAIT) {
+		tftf_testcase_printf("spci_run returned %lx\n",
+				     (u_register_t)ret_values.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Send a message to SP2 through direct messaging */
+	ret_values = spci_msg_send_direct_req(HYP_ID, SP_ID(2), 0xbbbb0000);
+
+	/*
+	 * Return responses may be SPCI_MSG_SEND_DIRECT_RESP or SPCI_INTERRUPT,
+	 * but only expect the former. Expect SMC32 convention from SP.
+	 */
+	if (ret_values.ret0 != SPCI_MSG_SEND_DIRECT_RESP_SMC32) {
+		tftf_testcase_printf("spci_msg_send_direct_req returned %lx\n",
+				     (u_register_t)ret_values.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	/*
+	 * Message loop in SP returns initial message with the running VM id
+	 * into the lower 16 bits of initial message.
+	 */
+	if (ret_values.ret3 != 0xbbbb0002) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/**********************************************************************
+	 * Send a message to SP1 through direct messaging
+	 **********************************************************************/
+
+	/* Send a second message to SP1 through direct messaging */
+	ret_values = spci_msg_send_direct_req(HYP_ID, SP_ID(1), 0xcccc0000);
+
+	/*
+	 * Return responses may be SPCI_MSG_SEND_DIRECT_RESP or SPCI_INTERRUPT,
+	 * but only expect the former. Expect SMC32 convention from SP.
+	 */
+	if (ret_values.ret0 != SPCI_MSG_SEND_DIRECT_RESP_SMC32) {
+		tftf_testcase_printf("spci_msg_send_direct_req returned %lx\n",
+				     (u_register_t)ret_values.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	if (ret_values.ret3 != 0xcccc0001) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/**********************************************************************
+	 * All tests passed.
+	 **********************************************************************/
+
+	return TEST_RESULT_SUCCESS;
+}
