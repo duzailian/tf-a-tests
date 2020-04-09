8fdd36389cd45b0b360b5b3913629f6dfbd0f764
Manish Pandey <manish.pandey2@arm.com>
2020-04-09 15:19:26 +0100

    cactus: select different stdout device at runtime
    cactus is used as both primary and secondary VM and for debug logging
    primary VM can access to UART while secodary VM's use hypervisor call to
    SPM.
    
    Based on VM id it will be decided whether to use UART or hypervisor call
    for debug logging.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I97786b893c0156815969692582b4de62c1b568fd

---
+++ spm/cactus/cactus_debug.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <drivers/console.h>
+#include "spci_helpers.h"
+
+#include "cactus.h"
+
+extern int console_pl011_putc(int);
+
+static int (*putc_impl)(int);
+
+static int putc_hypcall(int c)
+{
+	spm_debug_log((char)c);
+
+	return c;
+}
+
+static int putc_uart(int c)
+{
+	console_pl011_putc(c);
+
+	return c;
+}
+
+void set_putc_impl(enum stdout_route route)
+{
+	switch (route) {
+
+	case HVC_CALL_AS_STDOUT:
+		putc_impl = putc_hypcall;
+		return;
+
+	case PL011_AS_STDOUT:
+	default:
+		break;
+	}
+
+	putc_impl = putc_uart;
+}
+
+int console_putc(int c)
+{
+	if (!putc_impl) {
+		return -1;
+	}
+
+	return putc_impl(c);
+}
