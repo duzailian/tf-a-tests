b5e633156ccc855cb7081c11ff7b6e1a619c0e58
Jimmy Brisson <jimmy.brisson@arm.com>
2020-04-16 10:54:51 -0500

    Test that TF-A supports ARMv8.6-FGT
    Note: This test will cause an unhandled exception in the case that TF-A
    is not doing its job and enabling ARMv8.6-FGT when the hardware supports
    it.
    
    Change-Id: Iae0fe39895909248b5e7b07a1a73f7702adce7dd
    Signed-off-by: Jimmy Brisson <jimmy.brisson@arm.com>

--- tftf/tests/tests-cpu-extensions.mk
+++ tftf/tests/tests-cpu-extensions.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018-2019, Arm Limited. All rights reserved.
+# Copyright (c) 2018-2020, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -9,6 +9,7 @@ TESTS_SOURCES	+=	$(addprefix tftf/tests/,			\
 	extensions/mte/test_mte.c					\
 	extensions/sve/sve_operations.S					\
 	extensions/sve/test_sve.c					\
+	extensions/fgt/test_fgt.c					\
 	runtime_services/arm_arch_svc/smccc_arch_workaround_1.c		\
 	runtime_services/arm_arch_svc/smccc_arch_workaround_2.c		\
 	extensions/pauth/test_pauth.c					\
