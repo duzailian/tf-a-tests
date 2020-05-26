e6a762a9e0c6717801a4903ce1360eb2df363b0d
J-Alves <joao.alves@arm.com>
2020-05-26 18:22:25 +0100

    Cactus: FFA Version Test
    Added simple test of the FFA interface to cactus:
    - FFA version ABI helper;
    - New test file for test to FFA ABIs;
    - Invoking "ffa_tests" from cactus main.
    
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Change-Id: I8c8445ad6d9da79f1880d143836e7a6da68eaff7

---
+++ spm/cactus/cactus_ffa_tests.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <debug.h>
+#include <errno.h>
+#include <sp_helpers.h>
+
+#include "ffa_helpers.h"
+
+/* FFA version test helpers */
+#define FFA_MAJOR 1u
+#define FFA_MINOR 0u
+
+void ffa_tests(void) {
+	const char *test_ffa = "FFA Interfaces";
+	const char* test_ffa_version = "FFA Version interface";
+
+	announce_test_section_start(test_ffa);
+
+	announce_test_start(test_ffa_version);
+
+	smc_ret_values ret = ffa_version(MAKE_FFA_VERSION(FFA_MAJOR, FFA_MINOR));
+	uint32_t spm_version = (0xFFFFFFFF & ret.ret0);
+
+	if (spm_version == FFA_ERROR_INVALID_PARAMETER) {
+		ERROR("FFA_VERSION Invalid Parameter\n");
+	} else if(spm_version == FFA_ERROR_NOT_SUPPORTED) {
+		ERROR("FFA_VERSION not supported\n");
+	} else {
+		NOTICE("FFA_VERSION returned %x.%x\n",
+				spm_version >> FFA_VERSION_MAJOR_SHIFT,
+				spm_version & FFA_VERSION_MINOR_MASK);
+	}
+
+	announce_test_end(test_ffa_version);
+
+	announce_test_section_end(test_ffa);
+}
