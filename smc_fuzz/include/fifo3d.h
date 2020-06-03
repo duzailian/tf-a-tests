ff55990ed9b017bcb37dffcb16439eb7e79b170a
Mark Dykes <mark.dykes@arm.com>
2020-06-03 15:46:55 -0500

    SMC fuzzing module integration.
    This includes one test with one seed as
    the initial implementation.  A future upgrade will include an enhanced
    seeding strategy.  The patch includes an example device tree file with
    the actual test (sdei.dts) leveraging the SDEI functions that can be called
    without reference to system state.  Platform CI will have a single
    TFTF config to be used in all future testing.  Once both branches
    of TFA tests and platform CI are checked in a user can invoke the
    testing with:
    
    workspace=<workspace location> test_groups=fvp-aarch64-sdei,fvp-smcfuzzing:fvp-tftf-fip.tftf-aemv8a test_run=1 bin_mode=debug retain_paths=1 ./platform-ci/script/run_local_ci.sh
    
    Signed-off-by: Mark Dykes <mark.dykes@arm.com>
    Change-Id: Ic290e7255bcfd845c0d22037e0b670a6691541df

---
+++ smc_fuzz/include/fifo3d.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+#ifndef __FIFO3D__
+#define __FIFO3D__
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "smcmalloc.h"
+
+
+struct fifo3d {
+	char ***nnfifo;
+	char ***fnamefifo;
+	int **biasfifo;
+	int col;
+	int curr_col;
+	int *row;
+};
+
+/*
+ * Push function name string into raw data structure
+ */
+void push_3dfifo_fname(struct fifo3d *,char *);
+
+/*
+ * Push bias value into raw data structure
+ */
+void push_3dfifo_bias(struct fifo3d *,int);
+
+/*
+ * Create new column and/or row for raw data structure for newly
+ * found node from device tree
+ */
+void push_3dfifo_col(struct fifo3d *,char *,struct memmod *);
+
+#endif /* __FIFO3D__ */
