23b27eeee6b5dba03a6542fbdd5704b6d3fbf4ea
Ruari Phipps <ruari.phipps@arm.com>
2020-09-10 14:05:55 +0100

    SPM: split S-EL1 shim from S-EL0 application
    Signed-off-by: Ruari Phipps <ruari.phipps@arm.com>
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I203bbc0d379b12bd5cf1991b95e4f3a7d78d63d4

--- spm/ivy/ivy.h
+++ spm/ivy/ivy.h
@@ -9,7 +9,24 @@
 
 #include <stdint.h>
 
-/* Linker symbols used to figure out the memory layout of Ivy. */
+/* Linker symbols used to figure out the memory layout of the S-EL1 shim. */
+extern uintptr_t __SHIM_TEXT_START__, __SHIM_TEXT_END__;
+#define SHIM_TEXT_START		((uintptr_t)&__SHIM_TEXT_START__)
+#define SHIM_TEXT_END		((uintptr_t)&__SHIM_TEXT_END__)
+
+extern uintptr_t __SHIM_RODATA_START__, __SHIM_RODATA_END__;
+#define SHIM_RODATA_START	((uintptr_t)&__SHIM_RODATA_START__)
+#define SHIM_RODATA_END		((uintptr_t)&__SHIM_RODATA_END__)
+
+extern uintptr_t __SHIM_DATA_START__, __SHIM_DATA_END__;
+#define SHIM_DATA_START		((uintptr_t)&__SHIM_DATA_START__)
+#define SHIM_DATA_END		((uintptr_t)&__SHIM_DATA_END__)
+
+extern uintptr_t __SHIM_BSS_START__, __SHIM_BSS_END__;
+#define SHIM_BSS_START		((uintptr_t)&__SHIM_BSS_START__)
+#define SHIM_BSS_END		((uintptr_t)&__SHIM_BSS_END__)
+
+/* Linker symbols used to figure out the memory layout of Ivy (S-EL0). */
 extern uintptr_t __TEXT_START__, __TEXT_END__;
 #define IVY_TEXT_START		((uintptr_t)&__TEXT_START__)
 #define IVY_TEXT_END		((uintptr_t)&__TEXT_END__)
@@ -25,5 +42,6 @@ extern uintptr_t __DATA_START__, __DATA_END__;
 extern uintptr_t __BSS_START__, __BSS_END__;
 #define IVY_BSS_START		((uintptr_t)&__BSS_START__)
 #define IVY_BSS_END		((uintptr_t)&__BSS_END__)
+
 
 #endif /* __IVY_H__ */
