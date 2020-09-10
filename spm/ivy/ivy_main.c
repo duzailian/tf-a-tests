4cb56fc388502e6135725406d6d36718d4f914a7
Ruari Phipps <ruari.phipps@arm.com>
2020-09-10 14:05:55 +0100

    SPM: split S-EL1 shim from S-EL0 application
    The shim prepares the S-EL1/0 environment (stack, VBAR, MMU, I-cache,
    BSS clear, PIE fixup) for usage by the S-EL0 application.
    
    Signed-off-by: Ruari Phipps <ruari.phipps@arm.com>
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: I203bbc0d379b12bd5cf1991b95e4f3a7d78d63d4

--- spm/ivy/ivy_main.c
+++ spm/ivy/ivy_main.c
@@ -6,61 +6,26 @@
 
 #include <assert.h>
 #include <debug.h>
-#include <drivers/console.h>
-#include <drivers/arm/pl011.h>
 #include <errno.h>
+#include <ffa_helpers.h>
 #include <ivy_def.h>
-#include <plat_arm.h>
 #include <platform_def.h>
 #include <sp_helpers.h>
 
 #include "ivy.h"
-#include "ivy_def.h"
 
 /* Host machine information injected by the build system in the ELF file. */
 extern const char build_message[];
 extern const char version_string[];
-
-static void ivy_print_memory_layout(void)
-{
-	NOTICE("Secure Partition memory layout:\n");
-
-	NOTICE("  Image regions\n");
-	NOTICE("    Text region            : %p - %p\n",
-		(void *)IVY_TEXT_START, (void *)IVY_TEXT_END);
-	NOTICE("    Read-only data region  : %p - %p\n",
-		(void *)IVY_RODATA_START, (void *)IVY_RODATA_END);
-	NOTICE("    Data region            : %p - %p\n",
-		(void *)IVY_DATA_START, (void *)IVY_DATA_END);
-	NOTICE("    BSS region             : %p - %p\n",
-		(void *)IVY_BSS_START, (void *)IVY_BSS_END);
-	NOTICE("    Total image memory     : %p - %p\n",
-		(void *)IVY_IMAGE_BASE,
-		(void *)(IVY_IMAGE_BASE + IVY_IMAGE_SIZE));
-	NOTICE("  SPM regions\n");
-	NOTICE("    SPM <-> SP buffer      : %p - %p\n",
-		(void *)IVY_SPM_BUF_BASE,
-		(void *)(IVY_SPM_BUF_BASE + IVY_SPM_BUF_SIZE));
-	NOTICE("    NS <-> SP buffer       : %p - %p\n",
-		(void *)IVY_NS_BUF_BASE,
-		(void *)(IVY_NS_BUF_BASE + IVY_NS_BUF_SIZE));
-}
 
 void __dead2 ivy_main(void)
 {
 	u_register_t ret;
 	svc_args args;
 
-	console_init(PL011_UART3_BASE,
-		     PL011_UART3_CLK_IN_HZ,
-		     PL011_BAUDRATE);
-
-	NOTICE("Booting test Secure Partition Ivy\n");
+	NOTICE("Entering S-EL0 Secure Partition\n");
 	NOTICE("%s\n", build_message);
 	NOTICE("%s\n", version_string);
-	NOTICE("Running at S-EL0\n");
-
-	ivy_print_memory_layout();
 
 init:
 	args = (svc_args){.fid = FFA_MSG_WAIT};
