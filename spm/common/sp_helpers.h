0cf0c93eca7283c76bed9a29fcff6a4ec709d6d9
Manish Pandey <manish.pandey2@arm.com>
2020-09-15 22:31:58 +0100

    cactus: implement hvc call to get interrupt id
    Since an SP cannot access GIC directly, it relies on SPMC(S-EL2) to
    get the interrupt ID.
    This patch introduces a new hvc call "SPM_INTERRUPT_GET" to get
    interrupt ID in S-EL1.
    
    Change-Id: I37626f907174ae57a24dc6b86e89d26ba8ebbddf
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>

--- spm/common/sp_helpers.h
+++ spm/common/sp_helpers.h
@@ -13,8 +13,10 @@
 
 #define SPM_VM_ID_FIRST                 SP_ID(1)
 
+/* Should match with IDs defined in SPM/Hafnium */
 #define SPM_VM_GET_COUNT                (0xFF01)
 #define SPM_VCPU_GET_COUNT              (0xFF02)
+#define SPM_INTERRUPT_GET               (0xFF04)
 #define SPM_DEBUG_LOG                   (0xBD000000)
 
 typedef struct {
@@ -72,6 +74,8 @@ void sp_sleep(uint32_t ms);
 ffa_vcpu_count_t spm_vcpu_get_count(ffa_vm_id_t vm_id);
 
 ffa_vm_count_t spm_vm_get_count(void);
+
+ffa_int_id_t spm_interrupt_get(void);
 
 void spm_debug_log(char c);
 
