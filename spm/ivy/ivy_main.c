b056d3b49646b33dc29cbbd071e5873993816c6f
Olivier Deprez <olivier.deprez@arm.com>
2020-12-16 15:46:14 +0100

    SPM: S-EL0 partitions support build for TC0
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I47298f00ba91f498826dd3ddd690309df01cc13e

--- spm/ivy/ivy_main.c
+++ spm/ivy/ivy_main.c
@@ -5,6 +5,7 @@
  */
 
 #include <ffa_helpers.h>
+#include <sp_debug.h>
 #include <sp_helpers.h>
 #include <debug.h>
 
@@ -16,14 +17,32 @@ extern const char version_string[];
 
 void __dead2 ivy_main(void)
 {
+	u_register_t ret;
+	svc_args args;
+
+	set_putc_impl(SVC_CALL_AS_STDOUT);
+
 	NOTICE("Entering S-EL0 Secure Partition\n");
 	NOTICE("%s\n", build_message);
 	NOTICE("%s\n", version_string);
 
+init:
+	args = (svc_args){.fid = FFA_MSG_WAIT};
+	ret = sp_svc(&args);
+
 	while (1) {
-		svc_args args = {
-			.fid = FFA_MSG_WAIT
-		};
-		sp_svc(&args);
+		if (ret != FFA_MSG_SEND_DIRECT_REQ_SMC32) {
+			ERROR("unknown FF-A request %lx\n", ret);
+			goto init;
+		}
+
+		VERBOSE("Received request: %lx\n", args.arg3);
+
+		args.fid = FFA_MSG_SEND_DIRECT_RESP_SMC32;
+		args.arg1 = 0x80020000;
+		args.arg2 = 0;
+		args.arg3 = 0;
+
+		ret = sp_svc(&args);
 	}
 }
