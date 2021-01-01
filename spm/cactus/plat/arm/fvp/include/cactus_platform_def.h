88e0d075626efd7ede4bd4eb2d1bfc457e7a7ef3
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2020-12-31 19:25:33 -0600

    Cactus test for exercising SMMUv3 driver to perform stage2 translation
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>
    Change-Id: I498cb63aed497ab469a38e486a7943dd634e5b36

--- spm/cactus/plat/arm/fvp/include/cactus_platform_def.h
+++ spm/cactus/plat/arm/fvp/include/cactus_platform_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Arm Limited. All rights reserved.
+ * Copyright (c) 2020-2021, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -17,6 +17,9 @@
 
 #define PLAT_CACTUS_RX_BASE		ULL(0x7300000)
 #define PLAT_CACTUS_CORE_COUNT		(8U)
+
+/* Auxiliary memory used for testing purposes in Cactus SP */
+#define PLAT_CACTUS_AUX_BASE		ULL(0x7400000)
 
 #define CACTUS_PRIMARY_EC_COUNT		(8U)
 #define CACTUS_SECONDARY_EC_COUNT	(8U)
