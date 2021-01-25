eefe806b783c960afe443020cf7c3bd3aae32c80
J-Alves <joao.alves@arm.com>
2021-01-25 14:11:06 +0000

    cactus: Refactor handling of commands
    Added helper macros to define a command handler, build a command table
    in which each element is a pair of the handler and respective command
    ID.
    Message loop has been refactored to traverse the command table looking
    for the ID, and to call the respective command handler.
    Available tests have been moved to their own command handler.
    
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Change-Id: I38a50a8d47b083c81c21950d98341de660891c61

---
+++ spm/cactus/cactus_test_cmds.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "cactus_test_cmds.h"
+#include <ffa_helpers.h>
+
+/**
+ * Begin and end of command handler table, respectively. Both symbols defined by
+ * the linker.
+ */
+extern struct cactus_cmd_handler cactus_cmd_handler_begin[];
+extern struct cactus_cmd_handler cactus_cmd_handler_end[];
+
+/**
+ * Traverses command table from section ".cactus_handler", searches for a
+ * registered command and invokes the respective handler.
+ */
+bool cactus_handle_cmd(smc_ret_values* cmd_args, smc_ret_values* ret,
+		       struct mailbox_buffers *mb)
+{
+	if (cmd_args == NULL || ret == NULL) {
+		ERROR("Invalid argumentos passed to %s!\n", __func__);
+		return false;
+	}
+
+	uint64_t in_cmd = cactus_get_cmd(*cmd_args);
+	uint32_t sp_response;
+
+	for (struct cactus_cmd_handler *it_cmd = cactus_cmd_handler_begin;
+	     it_cmd < cactus_cmd_handler_end;
+	     it_cmd++)
+	{
+		if (it_cmd->id == in_cmd) {
+			sp_response = it_cmd->fn(cmd_args, mb);
+
+			*ret = cactus_response(ffa_dir_msg_dest(*cmd_args),
+					       ffa_dir_msg_source(*cmd_args),
+					       sp_response);
+			return true;
+		}
+	}
+
+	return false;
+}
---
+++ spm/cactus/cactus_tests/cactus_test_memory_sharing.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "cactus_test_cmds.h"
+#include "cactus_tests.h"
+#include "cactus_def.h"
+#include <xlat_tables_defs.h>
+#include "sp_helpers.h"
+#include <ffa_helpers.h>
+
+/* Memory section to be used for memory share operations */
+static __aligned(PAGE_SIZE) uint8_t share_page[PAGE_SIZE];
+
+CACTUS_CMD_HANDLER(mem_send_cmd, CACTUS_MEM_SEND_CMD)
+{
+	ffa_memory_management_test(mb,
+				   ffa_dir_msg_dest(*args),
+	 			   ffa_dir_msg_source(*args),
+				   cactus_req_mem_send_get_mem_func(*args),
+				   cactus_mem_send_get_handle(*args));
+
+	return CACTUS_SUCCESS;
+}
+
+CACTUS_CMD_HANDLER(req_mem_send_cmd, CACTUS_REQ_MEM_SEND_CMD)
+{
+	smc_ret_values ffa_ret;
+	uint32_t mem_func = cactus_req_mem_send_get_mem_func(*args);
+	ffa_vm_id_t receiver = cactus_req_mem_send_get_receiver(*args);
+	ffa_memory_handle_t handle;
+	ffa_vm_id_t vm_id = ffa_dir_msg_dest(*args);
+
+	VERBOSE("%x requested to send memory to %x (func: %x)\n",
+		ffa_dir_msg_source(*args), receiver, mem_func);
+
+	const struct ffa_memory_region_constituent constituents[] = {
+		{(void *)share_page, 1, 0}
+	};
+
+	const uint32_t constituents_count = ( sizeof(constituents) /
+					      sizeof(constituents[0]));
+
+	handle = ffa_memory_init_and_send(
+		(struct ffa_memory_region *)mb->send, PAGE_SIZE,
+		vm_id, receiver, constituents,
+		constituents_count, mem_func);
+
+	/*
+	* If returned an invalid handle, we should break the test.
+	*/
+	expect(handle != FFA_MEMORY_HANDLE_INVALID, true);
+
+	ffa_ret = cactus_mem_send_cmd(vm_id, receiver, mem_func, handle);
+
+	if (ffa_func_id(ffa_ret) != FFA_MSG_SEND_DIRECT_RESP_SMC32) {
+		ERROR("Failed to send message. error: %lx\n", ffa_ret.ret2);
+		return CACTUS_ERROR;
+	}
+
+	/* If anything went bad on the receiver's end. */
+	if (cactus_get_response(ffa_ret) == CACTUS_ERROR) {
+		ERROR("Received error from receiver!\n");
+		return CACTUS_ERROR;
+	}
+
+	if (mem_func != FFA_MEM_DONATE_SMC32) {
+		/*
+		 * Do a memory reclaim only if the mem_func regards to memory
+		 * share or lend operations, as with a donate the owner is
+		 * permanently given up access to the memory region.
+		 */
+		if (ffa_mem_reclaim(handle, 0).ret0 == FFA_ERROR) {
+			ERROR("Failed to reclaim memory!\n");
+			return CACTUS_ERROR;
+		}
+
+		/**
+		 * Read Content that has been written to memory to validate
+		 * access to memory segment has been reestablished, and receiver
+		 * made use of memory region.
+		 */
+		#if (LOG_LEVEL >= LOG_LEVEL_VERBOSE)
+			uint32_t *ptr = (uint32_t *)constituents->address;
+			VERBOSE("Memory contents after receiver SP's use:\n");
+			for (unsigned int i = 0U; i < 5U; i++) {
+				VERBOSE("      %u: %x\n", i, ptr[i]);
+			}
+		#endif
+	}
+
+	return CACTUS_SUCCESS;
+}
