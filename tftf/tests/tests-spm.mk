1de62e4965eb84c03b4932f2a643ec1f4f303c65
Max Shvetsov <maksims.svecovs@arm.com>
2021-02-23 18:29:14 +0000

    TFTF: Test partition info get from normal world
    Test if a normal world can get partition information of SPs. Testing
    for both individual partition information as well as information for
    all deployed SPs.
    
    Signed-off-by: Max Shvetsov <maksims.svecovs@arm.com>
    Change-Id: I4bc061c2658a10e6925eb9f621930edb703b0f10

--- tftf/tests/tests-spm.mk
+++ tftf/tests/tests-spm.mk
@@ -11,6 +11,7 @@ TESTS_SOURCES	+=							\
 		test_ffa_direct_messaging.c				\
 		test_ffa_features.c					\
 		test_ffa_memory_sharing.c				\
+		test_ffa_partition_info_get.c				\
 		test_ffa_rxtx_map.c					\
 		test_ffa_version.c					\
 		test_spm_cpu_features.c					\
