480dd23f76c9bb15a8a8da98f661293836863c32
J-Alves <joao.alves@arm.com>
2021-03-11 14:03:54 +0000

    feat(ff-a): notifications bitmap create and destroy ABIs
    Defined wrappers for FF-A calls:
    - FFA_NOTIFICATION_BITMAP_CREATE;
    - FFA_NOTIFICATION_BITMAP_DESTROY;
    
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Change-Id: Ia3dfbfa083807d696f937d677516d80223f04f58

--- include/runtime_services/ffa_helpers.h
+++ include/runtime_services/ffa_helpers.h
@@ -415,6 +415,9 @@ smc_ret_values ffa_mem_retrieve_req(uint32_t descriptor_length,
 			            uint32_t fragment_length);
 smc_ret_values ffa_mem_relinquish(void);
 smc_ret_values ffa_mem_reclaim(uint64_t handle, uint32_t flags);
+smc_ret_values ffa_notification_bitmap_create(ffa_id_t vm_id,
+					      uint32_t vcpu_count);
+smc_ret_values ffa_notification_bitmap_destroy(ffa_id_t vm_id);
 
 #endif /* __ASSEMBLY__ */
 
