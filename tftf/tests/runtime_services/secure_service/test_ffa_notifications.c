28b01d54927e3962841b3a7fa095002641f100b6
J-Alves <joao.alves@arm.com>
2021-03-29 15:25:19 +0100

    TFTF: Test notifications set and get interfaces
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Change-Id: Ibcfb415735ac7500752f70016c690457b13409b7

--- tftf/tests/runtime_services/secure_service/test_ffa_notifications.c
+++ tftf/tests/runtime_services/secure_service/test_ffa_notifications.c
@@ -347,3 +347,285 @@ test_result_t test_ffa_notifications_bind_unbind_zeroed(void)
 
 	return TEST_RESULT_SUCCESS;
 }
+
+/**
+ * Helper function to test FFA_NOTIFICATION_GET interface.
+ * Receives all arguments to use 'cactus_notification_get_send_cmd', and returns
+ * the received response. Depending on the testing scenario, this will allow
+ * to validate if the returned bitmaps are as expected.
+ *
+ * Returns:
+ * - 'true' if response was obtained;
+ * - 'false' if there was an error sending the request.
+ */
+static bool request_notification_get(
+	ffa_vm_id_t cmd_dest, ffa_vm_id_t receiver, uint32_t vcpu_id,
+	uint32_t flags, smc_ret_values *response)
+{
+	VERBOSE("TFTF requesting SP to get notifications!\n");
+
+	*response  = cactus_notification_get_send_cmd(HYP_ID, cmd_dest,
+						      receiver, vcpu_id,
+						      flags);
+
+	return is_ffa_direct_response(*response);
+}
+
+static bool request_notification_set(
+	ffa_vm_id_t cmd_dest, ffa_vm_id_t receiver, ffa_vm_id_t sender,
+	uint32_t flags, uint64_t notifications, uint32_t exp_resp,
+	int32_t exp_error)
+{
+	smc_ret_values ret;
+
+	VERBOSE("TFTF requesting SP to set notifications\n");
+
+	ret = cactus_notifications_set_send_cmd(HYP_ID, cmd_dest, receiver,
+						sender, flags, notifications);
+
+	return is_expected_cactus_response(ret, exp_resp, exp_error);
+}
+
+/**
+ * Check that SP's response to CACTUS_NOTIFICATION_GET_CMD is as expected.
+ */
+static bool is_notifications_get_as_expected(
+	smc_ret_values *ret, uint64_t exp_from_sp, uint64_t exp_from_vm,
+	ffa_vm_id_t receiver)
+{
+	uint64_t from_sp;
+	uint64_t from_vm;
+	bool success_ret;
+
+	/**
+	 * If receiver ID is SP, this is to evaluate the response to test
+	 * command 'CACTUS_NOTIFICATION_GET_CMD'.
+	 */
+	if (IS_SP_ID(receiver)) {
+		success_ret = (cactus_get_response(*ret) == CACTUS_SUCCESS);
+		from_sp = cactus_notifications_get_from_sp(*ret);
+		from_vm = cactus_notifications_get_from_vm(*ret);
+	} else {
+		/**
+		 * Else, this is to evaluate the return of FF-A call:
+		 * ffa_notification_get.
+		 */
+		success_ret = (ffa_func_id(*ret) == FFA_SUCCESS_SMC32);
+		from_sp = ffa_notifications_get_from_sp(*ret);
+		from_vm = ffa_notifications_get_from_vm(*ret);
+	}
+
+	if (success_ret != true ||
+	    exp_from_sp != from_sp ||
+	    exp_from_vm != from_vm) {
+		VERBOSE("Notifications not as expected:\n"
+			"   from sp: %llx\n"
+			"   from vm: %llx\n",
+			from_sp,
+			from_vm);
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * Helper to get the SP to bind notification, and set it for the SP.
+ */
+static bool request_notification_bind_and_set(ffa_vm_id_t sender,
+	ffa_vm_id_t receiver, uint64_t notifications)
+{
+	smc_ret_values ret;
+
+	if (!request_notification_bind(receiver, receiver, sender,
+				      notifications, 0, CACTUS_SUCCESS, 0)) {
+		return false;
+	}
+
+	ret = ffa_notification_set(sender, receiver, 0, notifications);
+
+	return is_expected_ffa_return(ret, FFA_SUCCESS_SMC32);
+}
+
+/**
+ * Helper to request SP to get the notifications and validate the return.
+ */
+static bool request_notification_get_and_validate(ffa_vm_id_t sender,
+	ffa_vm_id_t receiver, uint64_t notification, uint32_t flags)
+{
+	smc_ret_values ret;
+
+	return request_notification_get(receiver, receiver, 0, flags, &ret) &&
+	       is_notifications_get_as_expected(&ret, 0, notification, receiver);
+}
+
+/**
+ * Test to validate a VM can signal an SP.
+ */
+test_result_t test_ffa_notifications_vm_signals_sp(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	const ffa_vm_id_t sender = 1;
+	const ffa_vm_id_t receiver = SP_ID(1);
+	uint64_t notification = FFA_NOTIFICATION(1) | FFA_NOTIFICATION(60);
+	const uint32_t flags = FFA_NOTIFICATIONS_FLAG_BITMAP_VM;
+	test_result_t result = TEST_RESULT_SUCCESS;
+
+	if (!request_notification_bind_and_set(sender,
+					       receiver,
+					       notification)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if(!request_notification_get_and_validate(sender, receiver, notification,
+						 flags)) {
+		result = TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(receiver, receiver, sender,
+					 notification, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return result;
+}
+
+
+/**
+ * Test to validate an SP can signal an SP.
+ */
+test_result_t test_ffa_notifications_sp_signals_sp(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	const ffa_vm_id_t sender = SP_ID(1);
+	const ffa_vm_id_t receiver = SP_ID(2);
+	uint32_t get_flags = FFA_NOTIFICATIONS_FLAG_BITMAP_SP;
+	smc_ret_values ret;
+
+	/** Request receiver to bind a set of notifications to the sender */
+	if (!request_notification_bind(receiver, receiver, sender,
+				       g_notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_set(sender, receiver, sender, 0,
+				      g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_get(receiver, receiver, 0, get_flags, &ret)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!is_notifications_get_as_expected(&ret, g_notifications, 0,
+					      receiver)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!request_notification_unbind(receiver, receiver, sender,
+					 g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test to validate an SP can signal a VM.
+ */
+test_result_t test_ffa_notifications_sp_signals_vm(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	const ffa_vm_id_t sender = SP_ID(1);
+	const ffa_vm_id_t receiver = 1;
+	uint32_t get_flags = FFA_NOTIFICATIONS_FLAG_BITMAP_SP;
+	smc_ret_values ret;
+
+	/** Ask SPMC to allocate notifications bitmap */
+	if (!notifications_bitmap_create(receiver, 1)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/** Allow sp to send notifications */
+	ret = ffa_notification_bind(sender, receiver, 0, g_notifications);
+
+	if (!is_expected_ffa_return(ret, FFA_SUCCESS_SMC32)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Request SP to set notifications */
+	if (!request_notification_set(sender, receiver, sender, 0,
+				      g_notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_get(receiver, 0, get_flags);
+
+	if (!is_notifications_get_as_expected(&ret, g_notifications, 0,
+					      receiver)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	ret = ffa_notification_unbind(sender, receiver, g_notifications);
+
+	if (!is_expected_ffa_return(ret, FFA_SUCCESS_SMC32)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!notifications_bitmap_destroy(receiver)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/**
+ * Test to validate it is not possible to unbind a pending notification.
+ */
+test_result_t test_ffa_notifications_unbind_pending(void)
+{
+	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	const ffa_vm_id_t receiver = SP_ID(1);
+	const ffa_vm_id_t sender = 1;
+	const uint64_t notifications = FFA_NOTIFICATION(30) |
+				       FFA_NOTIFICATION(35);
+	uint32_t get_flags = FFA_NOTIFICATIONS_FLAG_BITMAP_VM;
+	smc_ret_values ret;
+
+	if (!request_notification_bind(receiver, receiver, sender,
+				       notifications, 0, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Set one of the bound notifications */
+	ret = ffa_notification_set(sender, receiver, 0, FFA_NOTIFICATION(30));
+
+	if (is_ffa_call_error(ret)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Attempt to unbind the pending notification */
+	if (!request_notification_unbind(receiver, receiver, sender,
+					 FFA_NOTIFICATION(30),
+					 CACTUS_ERROR, FFA_ERROR_DENIED)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/**
+	 * Request receiver partition to get pending notifications from VMs.
+	 * Only notification 30 is expected.
+	 */
+	if (!request_notification_get_and_validate(sender, receiver,
+						   FFA_NOTIFICATION(30),
+						   get_flags)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Unbind all notifications, to not interfere with other tests */
+	if (!request_notification_unbind(receiver, receiver, sender,
+					notifications, CACTUS_SUCCESS, 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
