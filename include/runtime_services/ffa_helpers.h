4431beb712748e4c1fe8989a2d7143da77e9edc9
J-Alves <joao.alves@arm.com>
2021-07-08 11:19:33 +0100

    test(ff-a): signaling per-vCPU notifications (SP to VM)
    To consolidate test from previous patch [1], validate signaling of
    per-vCPU notifications from SP to NWd.
    
    [1] I86ba840b4c1d35cce95e2bc197293511db6783a1
    
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Change-Id: I1899badcb2b0ea0ce1fa84e249dc53c6fb73a0e5

--- include/runtime_services/ffa_helpers.h
+++ include/runtime_services/ffa_helpers.h
@@ -68,6 +68,9 @@ static inline ffa_id_t ffa_endpoint_id(smc_ret_values val) {
 
 #define FFA_NOTIFICATIONS_FLAG_PER_VCPU	UINT32_C(0x1 << 0)
 
+/** Flag to delay Schedule Receiver Interrupt. */
+#define FFA_NOTIFICATIONS_FLAG_DELAY_SRI	UINT32_C(0x1 << 1)
+
 #define FFA_NOTIFICATIONS_FLAGS_VCPU_ID(id) UINT32_C((id & 0xFFFF) << 16)
 
 #define FFA_NOTIFICATIONS_FLAG_BITMAP_SP	UINT32_C(0x1 << 0)
