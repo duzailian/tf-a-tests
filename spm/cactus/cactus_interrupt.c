1cc82555febf34a537479431a8bbd32ad795ccc1
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2021-08-05 15:34:07 -0500

    feat: add support for secure interrupt handling and completion
    As per FF-A v1.1 spec, the conduit used by SPM to signal an interrupt
    to S-EL1 Partition is vIRQ and/or ERET.
    
    Depending on the execution state of the vCPU of the target SP,
    FFA_INTERRUPT ABI and/or Interrupt ID can be used as parameters during
    eret to the SP.
    
    This patch adds support for secure interrupt handling in Cactus SP as
    well as adds support for FFA_INTERRUPT ABI in the message loop.
    
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>
    Change-Id: I173b9e0af11b6fd2605145391da68dfda04a14f2

--- spm/cactus/cactus_interrupt.c
+++ spm/cactus/cactus_interrupt.c
@@ -6,14 +6,21 @@
 
 #include <debug.h>
 
+#include <context.h>
 #include <ffa_helpers.h>
 #include <sp_helpers.h>
 #include <spm_helpers.h>
+#include <drivers/arm/sp805.h>
+#include <platform.h>
+#include <platform_def.h>
 
 #include "cactus_test_cmds.h"
+#include "cactus_message_loop.h"
 #include "spm_common.h"
 
 extern ffa_id_t g_ffa_id;
+
+#define DEFERRED_INT_ID 0xFFFF
 
 static void managed_exit_handler(void)
 {
@@ -24,15 +31,78 @@ static void managed_exit_handler(void)
 	cactus_response(g_ffa_id, HYP_ID, MANAGED_EXIT_INTERRUPT_ID);
 }
 
-int cactus_irq_handler(void)
+static void secure_interrupt_handler(uint32_t intid)
 {
+	switch(intid) {
+	case IRQ_TWDOG_INTID:
+		/*
+		 * Interrupt triggered due to Trusted watchdog timer expiry.
+		 * Clear the interrupt and stop the timer.
+		 */
+		NOTICE("Trusted WatchDog timer stopped\n");
+		sp805_twdog_stop();
+		break;
+	default:
+		/*
+		 * Currently the only source of secure interrupt is Trusted
+		 * Watchdog timer.
+		 */
+		ERROR("%s: Interrupt ID %x not handled!\n", __func__,
+			 intid);
+		panic();
+	}
+
+	/* Perform de-activation of secure interrupt. */
+	spm_interrupt_deactivate(intid, intid);
+}
+
+smc_ret_values handle_interrupt(uint32_t int_id)
+{
+	uint32_t para_virtualized_irq;
+
+	/*
+	 * spm_interrupt_get() invocation is necessary to advance the interrupt
+	 * state machine maintained by Hafnium(SPM) for virtual interrupts.
+	 */
+	para_virtualized_irq = spm_interrupt_get();
+
+	/* SP is in BLOCKED state */
+	if (int_id == DEFERRED_INT_ID) {
+		unsigned int mpid = read_mpidr_el1() & MPID_MASK;
+		unsigned int core_pos = platform_get_core_pos(mpid);
+
+		secure_interrupt_handler(para_virtualized_irq);
+
+		/* Perform signal completion.
+		 * TODO: Find the target of current blocked request */
+		return ffa_run(SP_ID(2), core_pos);
+	} else {
+		/*
+		 * When SP is in WAITING state, SPM passes interrupt id as
+		 * part of FFA_INTERRUPT ABI call.
+		 */
+		secure_interrupt_handler(int_id);
+
+		/* Perform signal completion */
+		return ffa_msg_wait();
+	}
+}
+
+int cactus_irq_handler(struct ctx_regs *context)
+{
+	smc_ret_values ffa_ret;
 	uint32_t irq_num;
 
-	irq_num = spm_interrupt_get();
-
-	ERROR("%s: Interrupt ID %u not handled!\n", __func__, irq_num);
-
-	return 0;
+	save_context_ffa(&ffa_ret, context);
+	if (ffa_func_id(ffa_ret) == FFA_INTERRUPT) {
+		irq_num = ffa_interrupt_id(ffa_ret);
+		ffa_ret = handle_interrupt(irq_num);
+		restore_context_ffa(ffa_ret, context);
+	} else {
+		irq_num = spm_interrupt_get();
+		secure_interrupt_handler(irq_num);
+	}
+		return 0;
 }
 
 int cactus_fiq_handler(void)
--- spm/common/spm_helpers.h
+++ spm/common/spm_helpers.h
@@ -7,6 +7,7 @@
 #ifndef SPMC_H
 #define SPMC_H
 
+#include <context.h>
 #include <ffa_helpers.h>
 #include <spm_common.h>
 
@@ -24,5 +25,31 @@ uint32_t spm_interrupt_get(void);
 int64_t spm_interrupt_enable(uint32_t int_id, bool enable, enum interrupt_pin pin);
 int64_t spm_interrupt_deactivate(uint32_t pint_id, uint32_t vint_id);
 void spm_debug_log(char c);
+smc_ret_values handle_interrupt(uint32_t int_id);
 
+static inline void save_context_ffa(smc_ret_values *ffa_ret,
+			struct ctx_regs *context)
+{
+	ffa_ret->ret0 = context->gp_regs[0];
+	ffa_ret->ret1 = context->gp_regs[1];
+	ffa_ret->ret2 = context->gp_regs[2];
+	ffa_ret->ret3 = context->gp_regs[3];
+	ffa_ret->ret4 = context->gp_regs[4];
+	ffa_ret->ret5 = context->gp_regs[5];
+	ffa_ret->ret6 = context->gp_regs[6];
+	ffa_ret->ret7 = context->gp_regs[7];
+}
+
+static inline void restore_context_ffa(smc_ret_values ffa_ret,
+			struct ctx_regs *context)
+{
+	context->gp_regs[0] = ffa_ret.ret0;
+	context->gp_regs[1] = ffa_ret.ret1;
+	context->gp_regs[2] = ffa_ret.ret2;
+	context->gp_regs[3] = ffa_ret.ret3;
+	context->gp_regs[4] = ffa_ret.ret4;
+	context->gp_regs[5] = ffa_ret.ret5;
+	context->gp_regs[6] = ffa_ret.ret6;
+	context->gp_regs[7] = ffa_ret.ret7;
+}
 #endif /* SPMC_H */
