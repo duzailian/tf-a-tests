62bcb114ecd0490c6c1e781ace33ea45bc038744
Mark Dykes <mark.dykes@arm.com>
2021-09-22 18:26:05 -0500

    Adding tests for multi CPU delegation and fail conditions
    This testing patch has the following test scenarios for realm payload:
    
    1. Query the RMI Version on a single CPU.
    2. Multi CPU version query.
    3. Test to delegate and undelegate a buffer using RMI Delegate and
    Undelegate commands.
    4. Multi CPU delegation with granules that are randomly initialized
    Each CPU is assigned its own set of granules to avoid locking issues
    5. Fail scenarios for delegation including double delegation of a
    granule and submission of a misaligned address
    
    Signed-off-by: Mark Dykes <mark.dykes@arm.com>
    Change-Id: Ib09995568f229d3adb269c2f06d123103ce596cc

--- include/runtime_services/realm_payload/realm_payload_test.h
+++ include/runtime_services/realm_payload/realm_payload_test.h
@@ -40,9 +40,15 @@
 #define RMI_ABI_VERSION_GET_MAJOR(_version) ((_version) >> 16)
 #define RMI_ABI_VERSION_GET_MINOR(_version) ((_version) & 0xFFFF)
 
-#define GRANULE_SIZE 			4096
+#define NUM_GRANULES			5
+#define NUM_RANDOM_ITERATIONS		7
+#define GRANULE_SIZE			4096
+
+#define B_DELEGATED			0
+#define B_UNDELEGATED			1
 
 u_register_t realm_version(void);
 u_register_t realm_granule_delegate(uintptr_t);
 u_register_t realm_granule_undelegate(uintptr_t);
 test_result_t realm_multi_cpu_payload_test(void);
+test_result_t realm_multi_cpu_payload_del_undel(void);
