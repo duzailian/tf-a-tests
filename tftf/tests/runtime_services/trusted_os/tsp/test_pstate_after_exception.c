bc661fdaaedf4edb85c4630d20baa9843abe38f6
Daniel Boulby <daniel.boulby@arm.com>
2021-10-26 14:01:23 +0100

    test(el3-runtime): check DIT is retained on exception
    Add a test to check that the PSTATE bits not set in the
    Aarch64.TakeException but set to a default when taking an exception to
    EL3 are maintained after an exception and that changes in TSP do not
    effect the PSTATE in TFTF and vice versa.
    
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: Id4d625c7e9cbb565ac236f844274319cc02c2335

---
+++ tftf/tests/runtime_services/trusted_os/tsp/test_pstate_after_exception.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <test_helpers.h>
+
+/*
+ * Test that the PSTATE bits not set in Aarch64.TakeException but
+ * set to a default when taking an exception to EL3 are maintained
+ * after an exception and that changes in TSP do not effect the PSTATE
+ * in TFTF and vice versa.
+ */
+test_result_t tsp_check_pstate_maintained_on_exception(void)
+{
+	smc_args tsp_svc_params;
+	smc_ret_values ret;
+	u_register_t dit;
+
+	SKIP_TEST_IF_TSP_NOT_PRESENT();
+	SKIP_TEST_IF_DIT_NOT_SUPPORTED();
+
+	write_dit(DIT_BIT);
+
+	/* Standard SMC */
+	tsp_svc_params.fid = TSP_STD_FID(TSP_CHECK_DIT);
+	tsp_svc_params.arg1 = 0;
+	tsp_svc_params.arg2 = 0;
+	ret = tftf_smc(&tsp_svc_params);
+	if (ret.ret1 == 0) {
+		if (ret.ret2 == 0xffff) {
+			tftf_testcase_printf("DIT bit not supported by TSP");
+			return TEST_RESULT_SKIPPED;
+		}
+		tftf_testcase_printf("DIT bit in the TSP is not 0.\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	dit = read_dit();
+	if (dit != DIT_BIT) {
+		tftf_testcase_printf("DIT bit in TFTF was not maintained.\n"
+				     "Expected: 0x%llx, Actual: 0x%lx",
+				     DIT_BIT, dit);
+		return TEST_RESULT_FAIL;
+	}
+
+	tsp_svc_params.fid = TSP_STD_FID(TSP_CHECK_DIT);
+	tsp_svc_params.arg1 = DIT_BIT;
+	tsp_svc_params.arg2 = 0;
+	ret = tftf_smc(&tsp_svc_params);
+	if (ret.ret1 == 0) {
+		tftf_testcase_printf("DIT bit in the TSP was not maintained\n"
+				     "Expected: 0x%llx, Actual: 0x%lx",
+				     DIT_BIT, ret.ret2);
+		return TEST_RESULT_FAIL;
+	}
+
+	dit = read_dit();
+	if (dit != DIT_BIT) {
+		tftf_testcase_printf("DIT bit in TFTF was not maintained.\n"
+				     "Expected: 0x%llx, Actual: 0x%lx",
+				     DIT_BIT, dit);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
