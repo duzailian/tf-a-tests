fcf56ef749fc3bd16676ee85867c5744dfbd5896
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2021-12-24 08:52:52 +0000

    feat(WFxT): add a test for WFxT instructions
    This patch adds the test to verify the WFET and WFIT instructions
    introduced by FEAT_WFxT.
    
    WFET and WFIT instructions assist in generating local-timeout event
    and thereby act as wakeup event for the PE, when the virtual count
    in CNTVCT_EL0 (counter-timer virtual count) register equals or exceed
    the timeout value passed with these instructions.
    
    Accordingly, this testcase verifies whether the timelapsed matches the
    value supplied with WFET and WFIT instructions.
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: I9aea5da869db8520e305e49989cb71f166a582eb

--- include/lib/aarch64/arch_features.h
+++ include/lib/aarch64/arch_features.h
@@ -10,6 +10,7 @@
 #include <stdbool.h>
 
 #include <arch_helpers.h>
+#include <debug.h>
 
 static inline bool is_armv7_gentimer_present(void)
 {
@@ -152,6 +153,14 @@ static inline bool get_feat_afp_present(void)
 {
 	return (((read_id_aa64mmfr1_el1() >> ID_AA64MMFR1_EL1_AFP_SHIFT) &
 		  ID_AA64MMFR1_EL1_AFP_MASK) == ID_AA64MMFR1_EL1_AFP_SUPPORTED);
+}
+
+static inline bool get_feat_wfxt_present(void)
+{
+	uint64_t val = read_id_aa64isar2_el1();
+	INFO("val = %llu\n", val);
+	return (((read_id_aa64isar2_el1() >> ID_AA64ISAR2_WFXT_SHIFT) &
+		ID_AA64ISAR2_WFXT_MASK) == ID_AA64ISAR2_WFXT_SUPPORTED);
 }
 
 #endif /* ARCH_FEATURES_H */
