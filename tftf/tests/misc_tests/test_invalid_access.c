bdddb6a2260cc6a374e1e8a5487cdfb3aac9822a
Olivier Deprez <olivier.deprez@arm.com>
2022-01-11 11:50:36 +0100

    test(tftf): prevent realm region access from nwd
    This change adds a TFTF test attempting an access to a memory region
    marked Realm in the GPT. It is expected this results in a GPF data
    abort being caught on the PE.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I05134bb9a3e37fc5478f602844b7a6ad3b8a3293

--- tftf/tests/misc_tests/test_invalid_access.c
+++ tftf/tests/misc_tests/test_invalid_access.c
@@ -13,7 +13,9 @@
 #endif
 #include <test_helpers.h>
 #include <tftf_lib.h>
-
+#include <runtime_services/realm_payload/realm_payload_test.h>
+#include <lib/aarch64/arch_features.h>
+#include <lib/xlat_tables/xlat_tables_defs.h>
 #include <platform_def.h>
 
 /*
@@ -31,6 +33,10 @@
 
 static volatile bool sync_exception_triggered;
 static volatile bool data_abort_triggered;
+
+static __aligned(PAGE_SIZE) uint64_t share_page[PAGE_SIZE / sizeof(uint64_t)];
+
+static volatile uint32_t data_abort_gpf_triggered;
 
 static bool data_abort_handler(void)
 {
@@ -91,10 +97,91 @@ test_result_t access_el3_memory_from_ns(void)
 
 	return TEST_RESULT_SUCCESS;
 }
+
+static bool data_abort_gpf_handler(void)
+{
+	uint64_t esr_el2 = read_esr_el2();
+
+	VERBOSE("%s esr_el2 %llx elr_el2 %llx\n",
+		__func__, esr_el2, read_elr_el2());
+
+	if ((EC_BITS(esr_el2) == EC_DABORT_CUR_EL) &&
+	    ((ISS_BITS(esr_el2) & 0x3f) == 0x28)) {
+		data_abort_gpf_triggered++;
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * @Test_Aim@ Check a realm region cannot be accessed from normal world.
+ *
+ * This test delegates a TFTF allocated buffer to Realm. It then attempts
+ * a read access to the region from normal world. This results in the PE
+ * triggering a GPF caught by a custom synchronous abort handler.
+ *
+ */
+test_result_t rl_memory_cannot_be_accessed_in_ns(void)
+{
+	test_result_t result = TEST_RESULT_FAIL;
+	u_register_t retmm;
+
+	if (get_armv9_2_feat_rme_support() == 0U) {
+		return TEST_RESULT_SKIPPED;
+	}
+
+	data_abort_gpf_triggered = 0;
+	register_custom_sync_exception_handler(data_abort_gpf_handler);
+
+	/* First read access to the test region must not fail. */
+	*((volatile uint64_t*)share_page);
+
+	if (data_abort_gpf_triggered != 0) {
+		goto out_unregister;
+	}
+
+	/* Delegate the shared page to Realm. */
+	retmm = realm_granule_delegate((u_register_t)&share_page);
+	if (retmm != 0UL) {
+		ERROR("Granule delegate failed!\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	/* This access shall trigger a GPF. */
+	*((volatile uint64_t*)share_page);
+
+	if (data_abort_gpf_triggered != 1) {
+		goto out_undelegate;
+	}
+
+	result = TEST_RESULT_SUCCESS;
+
+out_undelegate:
+	/* Undelegate the shared page. */
+	retmm = realm_granule_undelegate((u_register_t)&share_page);
+	if (retmm != 0UL) {
+		ERROR("Granule undelegate failed!\n");
+	}
+
+out_unregister:
+	unregister_custom_sync_exception_handler();
+
+	return result;
+}
+
 #else
+
 test_result_t access_el3_memory_from_ns(void)
 {
 	tftf_testcase_printf("Test not ported to AArch32\n");
 	return TEST_RESULT_SKIPPED;
 }
+
+test_result_t rl_memory_cannot_be_accessed_in_ns(void)
+{
+	tftf_testcase_printf("Test not ported to AArch32\n");
+	return TEST_RESULT_SKIPPED;
+}
+
 #endif /* __aarch64__ */
