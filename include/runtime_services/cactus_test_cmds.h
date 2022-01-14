42cf8b4357743779fff4d736274bfa476cf3a0d9
Federico Recanati <federico.recanati@arm.com>
2022-01-14 15:48:16 +0100

    test(cactus): add test for NS memory sharing between SPs
    Define a new shared memory region in 48-bit address range
    (configured as NS in the TZC) in cactus-tertiary's manifest.
    Since SPs can share either secure or non-secure memory, propagate the
    security information in the relevant cactus commands:
    CACTUS_REQ_MEM_SEND_CMD and CACTUS_MEM_SEND_CMD.
    
    Change-Id: I10af24c96ff8fc0d13c80a52b0264a1482a5cf56
    Signed-off-by: Federico Recanati <federico.recanati@arm.com>

--- include/runtime_services/cactus_test_cmds.h
+++ include/runtime_services/cactus_test_cmds.h
@@ -196,15 +196,20 @@ static inline ffa_id_t cactus_deadlock_get_next_dest2(smc_ret_values ret)
 
 static inline smc_ret_values cactus_mem_send_cmd(
 	ffa_id_t source, ffa_id_t dest, uint32_t mem_func,
-	ffa_memory_handle_t handle)
+	ffa_memory_handle_t handle, bool non_secure)
 {
 	return cactus_send_cmd(source, dest, CACTUS_MEM_SEND_CMD, mem_func,
-			       handle, 0, 0);
+			       handle, non_secure, 0);
 }
 
 static inline ffa_memory_handle_t cactus_mem_send_get_handle(smc_ret_values ret)
 {
 	return (ffa_memory_handle_t)ret.ret5;
+}
+
+static inline bool cactus_mem_send_get_non_secure(smc_ret_values ret)
+{
+	return (bool)ret.ret6;
 }
 
 /**
@@ -218,10 +223,10 @@ static inline ffa_memory_handle_t cactus_mem_send_get_handle(smc_ret_values ret)
 
 static inline smc_ret_values cactus_req_mem_send_send_cmd(
 	ffa_id_t source, ffa_id_t dest, uint32_t mem_func,
-	ffa_id_t receiver)
+	ffa_id_t receiver, bool non_secure)
 {
 	return cactus_send_cmd(source, dest, CACTUS_REQ_MEM_SEND_CMD, mem_func,
-			       receiver, 0, 0);
+			       receiver, non_secure, 0);
 }
 
 static inline uint32_t cactus_req_mem_send_get_mem_func(smc_ret_values ret)
@@ -232,6 +237,11 @@ static inline uint32_t cactus_req_mem_send_get_mem_func(smc_ret_values ret)
 static inline ffa_id_t cactus_req_mem_send_get_receiver(smc_ret_values ret)
 {
 	return (ffa_id_t)ret.ret5;
+}
+
+static inline bool cactus_req_mem_send_get_non_secure(smc_ret_values ret)
+{
+	return (bool)ret.ret6;
 }
 
 /**
