5368db61e7dee19d7002ea83eeba698d90d5f70b
Manish Pandey <manish.pandey2@arm.com>
2022-01-17 11:05:53 +0000

    feat(invalid_access): test to access secure memory
    Add a test case to access secure memory from NS world.
    Currently introduced for fvp platform only but can be extended to other
    platforms.
    
    Test steps:
    	1. Register a custom sync exception handler(try & catch)
    	2. Access a secure memory, should give data abort.
    	3. Exception handler should check if data abort then continue.
    	4. Unregister custom exception handler.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Ib8b199acb9b96548c889539610ff1b58777d3d1d

---
+++ tftf/tests/misc_tests/test_invalid_access.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch.h>
+#include <arch_helpers.h>
+#include <debug.h>
+#include <tftf_lib.h>
+#include <sync.h>
+
+#include <platform_def.h>
+
+bool data_abort_handler(void)
+{
+	uint64_t elr_el2 = read_elr_el2();
+	uint64_t esr_el2 = read_esr_el2();
+
+	INFO("%s esr_el2 %llx elr_el2 %llx\n", __func__, esr_el2, elr_el2);
+
+	if (EC_BITS(esr_el2) == EC_DABORT_CUR_EL) {
+		VERBOSE("%s Data Abort caught\n", __func__);
+		return true;
+	}
+
+	return false;
+}
+
+test_result_t access_secure_from_NS(void)
+{
+	const uintptr_t test_address = SECURE_ACCESS_TEST_ADDRESS;
+
+	tftf_testcase_printf("Attempt to access secure memory (0x%lx)\n", test_address);
+
+	register_custom_sync_exception_handler(data_abort_handler);
+	*((volatile uint64_t*)test_address);
+	unregister_custom_sync_exception_handler();
+
+	return TEST_RESULT_SUCCESS;
+}
