898b8fd4b62f6432dc6b934d326b4861fa10d4db
Manish Pandey <manish.pandey2@arm.com>
2022-01-17 11:05:53 +0000

    feat(tftf): add tests to access invalid memory
    Add test framework to access memory addresses which are not accessible
    to tftf. The test framework will catch the error and do graceful exit.
    For now only test introduced is to access memory owned by EL3, which can
    be easily extended to add more tests to access realm/secure(in RME
    enabled systems).
    
    Platform needs to provide test address, if not then test will
    be skipped.
    
    Test steps:
    	1. Register a custom sync exception handler(try & catch)
    	2. Access a secure memory, should give data abort.
    	3. Exception handler should check if data abort then continue.
    	4. Unregister custom exception handler.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Ib8b199acb9b96548c889539610ff1b58777d3d1d

---
+++ tftf/tests/misc_tests/test_invalid_access.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch.h>
+#include <arch_helpers.h>
+#include <arch_features.h>
+#include <debug.h>
+#include <sync.h>
+#include <test_helpers.h>
+#include <tftf_lib.h>
+
+#include <platform_def.h>
+
+#ifndef EL3_MEMORY_ACCESS_ADDR
+#define EL3_MEMORY_ACCESS_ADDR		U(0xFFFFFFFF)
+#endif
+
+static volatile bool data_abort_triggered = false;
+
+static bool data_abort_handler(void)
+{
+	unsigned int rme_supported = get_armv9_2_feat_rme_support();
+	uint64_t esr_el2 = read_esr_el2();
+
+	VERBOSE("%s esr_el2 %llx\n", __func__, esr_el2);
+
+	if (EC_BITS(esr_el2) == EC_DABORT_CUR_EL) {
+		if (rme_supported == 0) {
+			/* Synchronous external data abort triggered by trustzone controller */
+			if ((ISS_BITS(esr_el2) & ISS_DFSC_MASK) == DFSC_EXT_DABORT) {
+				VERBOSE("%s TZC Data Abort caught\n", __func__);
+				data_abort_triggered = true;
+				return true;
+			}
+		} else {
+			/* Synchronous data abort triggered by Granule protection */
+			if ((ISS_BITS(esr_el2) & ISS_DFSC_MASK) == DFSC_GPF_DABORT) {
+				VERBOSE("%s GPF Data Abort caught\n", __func__);
+				data_abort_triggered = true;
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+test_result_t access_el3_memory_from_ns(void)
+{
+	const uintptr_t test_address = EL3_MEMORY_ACCESS_ADDR;
+
+	SKIP_TEST_IF_INVALID_ADDRESS(test_address);
+
+	VERBOSE("Attempt to access el3 memory (0x%lx)\n", test_address);
+
+	data_abort_triggered = false;
+	register_custom_sync_exception_handler(data_abort_handler);
+	dsbsy();
+
+	*((volatile uint64_t *)test_address);
+
+	dsbsy();
+	unregister_custom_sync_exception_handler();
+	if (data_abort_triggered == false) {
+		tftf_testcase_printf("Data abort not triggered\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
