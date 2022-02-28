6625d7bd08c5b6e9c7ea337f23b7e681c0e5b9dd
Olivier Deprez <olivier.deprez@arm.com>
2022-02-28 18:57:26 +0100

    test(smmu): secure peripheral access prevented
    Check a secure peripheral upstream to an SMMU cannot perform
    transactions to a realm region.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I2ca7c303e8fef6baf99fe4b64b33102546b9ff30

--- tftf/tests/runtime_services/secure_service/test_spm_smmu.c
+++ tftf/tests/runtime_services/secure_service/test_spm_smmu.c
@@ -10,6 +10,13 @@
 #include <smccc.h>
 #include <spm_test_helpers.h>
 #include <test_helpers.h>
+#include <sp_platform_def.h>
+#include <runtime_services/host_realm_managment/host_realm_rmi.h>
+#include <runtime_services/host_realm_managment/host_realm_rmi.h>
+
+/* TODO: export SMMUv3 test engine macros. */
+#define ENGINE_MEMCPY	(2U)
+#define ENGINE_RAND48	(3U)
 
 static const struct ffa_uuid expected_sp_uuids[] = { {PRIMARY_UUID} };
 
@@ -24,11 +31,26 @@ test_result_t test_smmu_spm(void)
 	/**********************************************************************
 	 * Check SPMC has ffa_version and expected FFA endpoints are deployed.
 	 **********************************************************************/
-	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
 
-	VERBOSE("Sending command to SP %x for initiating DMA transfer\n",
+	VERBOSE("Sending command to SP %x for initiating DMA transfer.\n",
 			SP_ID(1));
-	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1));
+
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_RAND48,
+		PLAT_CACTUS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE);
+
+	/* Expect the SMMU DMA operation to pass. */
+	if (cactus_get_response(ret) != CACTUS_SUCCESS) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Expect the SMMU DMA operation to pass. */
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_MEMCPY,
+		PLAT_CACTUS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE);
 
 	if (cactus_get_response(ret) != CACTUS_SUCCESS) {
 		return TEST_RESULT_FAIL;
@@ -37,3 +59,48 @@ test_result_t test_smmu_spm(void)
 	return TEST_RESULT_SUCCESS;
 }
 
+/**************************************************************************
+ * TODO: test_smmu_spm_invalid_access
+ **************************************************************************/
+test_result_t test_smmu_spm_invalid_access(void)
+{
+	struct ffa_value ret;
+	u_register_t retmm;
+
+	/* Skip this test if RME is not implemented. */
+	if (get_armv9_2_feat_rme_support() == 0U) {
+		return TEST_RESULT_SKIPPED;
+	}
+
+	/**********************************************************************
+	 * Check SPMC has ffa_version and expected FFA endpoints are deployed.
+	 **********************************************************************/
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+	retmm = host_rmi_granule_delegate((u_register_t)PLAT_CACTUS_SMMU_NS_MEMCPY_BASE);
+	if (retmm != 0UL) {
+		ERROR("Granule delegate failed!\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	VERBOSE("Sending command to SP %x for initiating DMA transfer.\n",
+			SP_ID(1));
+
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_RAND48,
+		PLAT_CACTUS_SMMU_NS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE);
+
+	retmm = host_rmi_granule_undelegate((u_register_t)PLAT_CACTUS_SMMU_NS_MEMCPY_BASE);
+	if (retmm != 0UL) {
+		ERROR("Granule undelegate failed!\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Expect the SMMU DMA operation to fail. */
+	if (cactus_get_response(ret) != CACTUS_ERROR) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
