d2c3a57cb0196e13787629a9c5de49d8d6b93e63
Olivier Deprez <olivier.deprez@arm.com>
2022-02-28 18:57:26 +0100

    test(smmu): secure peripheral access prevented
    Check a secure peripheral upstream to an SMMU cannot perform
    transactions to a realm region.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I2ca7c303e8fef6baf99fe4b64b33102546b9ff30

--- tftf/tests/runtime_services/secure_service/test_spm_smmu.c
+++ tftf/tests/runtime_services/secure_service/test_spm_smmu.c
@@ -10,12 +10,21 @@
 #include <smccc.h>
 #include <spm_test_helpers.h>
 #include <test_helpers.h>
+#include <sp_platform_def.h>
+#include <runtime_services/host_realm_managment/host_realm_rmi.h>
+#include <runtime_services/host_realm_managment/host_realm_rmi.h>
+
+#define ENGINE_MEMCPY	(2U)
+#define ENGINE_RAND48	(3U)
 
 static const struct ffa_uuid expected_sp_uuids[] = { {PRIMARY_UUID} };
 
 /**************************************************************************
- * Send a command to SP1 initiate DMA service with the help of a peripheral
- * device upstream of an SMMUv3 IP
+ * Send commands to SP1 initiate DMA service with the help of a peripheral
+ * device upstream of an SMMUv3 IP.
+ * The scenario involves randomizing a secure buffer (first DMA operation),
+ * copying this buffer to another location (second DMA operation),
+ * and checking (by CPU) that both buffer contents match.
  **************************************************************************/
 test_result_t test_smmu_spm(void)
 {
@@ -24,12 +33,35 @@ test_result_t test_smmu_spm(void)
 	/**********************************************************************
 	 * Check SPMC has ffa_version and expected FFA endpoints are deployed.
 	 **********************************************************************/
-	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
 
-	VERBOSE("Sending command to SP %x for initiating DMA transfer\n",
+	VERBOSE("Sending command to SP %x for initiating DMA transfer.\n",
 			SP_ID(1));
-	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1));
 
+	/*
+	 * Randomize first half of a secure buffer from the secure world
+	 * through the SMMU test engine DMA.
+	 */
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_RAND48,
+		PLAT_CACTUS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE / 2);
+
+	/* Expect the SMMU DMA operation to pass. */
+	if (cactus_get_response(ret) != CACTUS_SUCCESS) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/*
+	 * Copy first half to second half of the buffer and
+	 * check both match.
+	 */
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_MEMCPY,
+		PLAT_CACTUS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE);
+
+	/* Expect the SMMU DMA operation to pass. */
 	if (cactus_get_response(ret) != CACTUS_SUCCESS) {
 		return TEST_RESULT_FAIL;
 	}
@@ -37,3 +69,58 @@ test_result_t test_smmu_spm(void)
 	return TEST_RESULT_SUCCESS;
 }
 
+/**************************************************************************
+ * The scenario changes the active PAS for a NS buffer into Realm PAS.
+ * Then it queries a SP to initiate a secure DMA operation on this buffer
+ * through the SMMU.
+ * The operation is expected to fail as a secure DMA transaction to a Realm
+ * region fails GPC on the SMMU path.
+ **************************************************************************/
+test_result_t test_smmu_spm_invalid_access(void)
+{
+	struct ffa_value ret;
+	u_register_t retmm;
+
+	/* Skip this test if RME is not implemented. */
+	if (get_armv9_2_feat_rme_support() == 0U) {
+		return TEST_RESULT_SKIPPED;
+	}
+
+	/**********************************************************************
+	 * Check SPMC has ffa_version and expected FFA endpoints are deployed.
+	 **********************************************************************/
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+	/* Update the NS buffer to Realm PAS. */
+	retmm = host_rmi_granule_delegate((u_register_t)PLAT_CACTUS_SMMU_NS_MEMCPY_BASE);
+	if (retmm != 0UL) {
+		ERROR("Granule delegate failed!\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	VERBOSE("Sending command to SP %x for initiating DMA transfer.\n",
+			SP_ID(1));
+
+	/*
+	 * Attempt randomizing the buffer (now turned into Realm PAS)
+	 * from the secure world through the SMMU test engine DMA.
+	 */
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_RAND48,
+		PLAT_CACTUS_SMMU_NS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE);
+
+	/* Update the buffer back to NS PAS. */
+	retmm = host_rmi_granule_undelegate((u_register_t)PLAT_CACTUS_SMMU_NS_MEMCPY_BASE);
+	if (retmm != 0UL) {
+		ERROR("Granule undelegate failed!\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Expect the SMMU DMA operation to have failed. */
+	if (cactus_get_response(ret) != CACTUS_ERROR) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
