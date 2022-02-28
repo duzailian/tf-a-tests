78272ebd26c44b2817462efd4c806ac41ef1ad8a
Olivier Deprez <olivier.deprez@arm.com>
2022-02-28 18:57:26 +0100

    refactor(smmu): secure side DMA rand and memcopy
    Add operation, source and size parameters to the cactus command
    initiating an operation to the DMA test engine through the SMMU.
    Add RAND48 operation support to the DMA test engine.
    Refactor to a common run_testengine helper exercised by MEMCPY
    and RAND48 SMMU operations.
    Cactus SP dispatches either one of the operation based on the
    operation id passed from TFTF.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I2ca7c303e8fef6baf99fe4b64b33102546b9ff30

--- tftf/tests/runtime_services/secure_service/test_spm_smmu.c
+++ tftf/tests/runtime_services/secure_service/test_spm_smmu.c
@@ -11,29 +11,66 @@
 #include <spm_test_helpers.h>
 #include <test_helpers.h>
 
+#if PLAT_fvp || PLAT_tc
+#include <sp_platform_def.h>
 static const struct ffa_uuid expected_sp_uuids[] = { {PRIMARY_UUID} };
+#endif
+
+#define ENGINE_MEMCPY	(2U)
+#define ENGINE_RAND48	(3U)
 
 /**************************************************************************
- * Send a command to SP1 initiate DMA service with the help of a peripheral
- * device upstream of an SMMUv3 IP
+ * test_smmu_spm
+ *
+ * Send commands to SP1 initiate DMA service with the help of a peripheral
+ * device upstream of an SMMUv3 IP.
+ * The scenario involves randomizing a secure buffer (first DMA operation),
+ * copying this buffer to another location (second DMA operation),
+ * and checking (by CPU) that both buffer contents match.
  **************************************************************************/
 test_result_t test_smmu_spm(void)
 {
+#if PLAT_fvp || PLAT_tc
 	struct ffa_value ret;
 
 	/**********************************************************************
 	 * Check SPMC has ffa_version and expected FFA endpoints are deployed.
 	 **********************************************************************/
-	CHECK_SPMC_TESTING_SETUP(1, 0, expected_sp_uuids);
+	CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
 
-	VERBOSE("Sending command to SP %x for initiating DMA transfer\n",
+	VERBOSE("Sending command to SP %x for initiating DMA transfer.\n",
 			SP_ID(1));
-	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1));
 
+	/*
+	 * Randomize first half of a secure buffer from the secure world
+	 * through the SMMU test engine DMA.
+	 */
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_RAND48,
+		PLAT_CACTUS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE / 2, 0);
+
+	/* Expect the SMMU DMA operation to pass. */
+	if (cactus_get_response(ret) != CACTUS_SUCCESS) {
+		return TEST_RESULT_FAIL;
+	}
+
+	/*
+	 * Copy first half to second half of the buffer and
+	 * check both match.
+	 */
+	ret = cactus_send_dma_cmd(HYP_ID, SP_ID(1),
+		ENGINE_MEMCPY,
+		PLAT_CACTUS_MEMCPY_BASE,
+		PLAT_CACTUS_MEMCPY_RANGE, 0);
+
+	/* Expect the SMMU DMA operation to pass. */
 	if (cactus_get_response(ret) != CACTUS_SUCCESS) {
 		return TEST_RESULT_FAIL;
 	}
 
 	return TEST_RESULT_SUCCESS;
+#else
+	return TEST_RESULT_SKIPPED;
+#endif
 }
-
