548481d9f92bcf27876706199d9529ce5c5c892c
Daniel Boulby <daniel.boulby@arm.com>
2022-03-03 18:29:21 +0000

    test(spm): split generate_json script by sp
    generate_json.sh now will only add the partition information to the
    layout file for the sp given in arg1. This allows the ability for
    sp's to pass flags to the script such as IVY_SHIM.
    
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: I0e8b62806ebfb4c1ed0c94150d2c6af582254126

--- spm/ivy/ivy.mk
+++ spm/ivy/ivy.mk
@@ -89,7 +89,7 @@ $(IVY_DTB) : $(IVY_DTS)
 	${Q}tools/generate_dtb/generate_dtb.sh \
 		ivy ${IVY_DTS} $(BUILD_PLAT) $(IVY_DTB)
 	${Q}tools/generate_json/generate_json.sh \
-		ivy $(BUILD_PLAT)
+		ivy $(BUILD_PLAT) $(IVY_SHIM)
 	@echo
 	@echo "Built $@ successfully"
 	@echo
