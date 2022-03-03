9c06a4b2230b2638aefc436e6a4541dab9bdf3d1
Daniel Boulby <daniel.boulby@arm.com>
2022-03-03 18:29:21 +0000

    test(spm): split generate_json script by sp
    generate_json.sh now will only add the partition information to the
    layout file for the sp given in arg1. This allows the ability for
    sp's to pass flags to the script such as IVY_SHIM.
    
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: I0e8b62806ebfb4c1ed0c94150d2c6af582254126

--- tools/generate_json/generate_json.sh
+++ tools/generate_json/generate_json.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2020-2021, Arm Limited. All rights reserved.
+# Copyright (c) 2020-2022, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 # Generate a JSON file which will be fed to TF-A as SPM_LAYOUT_FILE to package
 # Secure Partitions as part of FIP.
+# Note the script will append the partition to the existing layout file.
+# If you wish to only generate a layout file with this partition first run
+# "make realclean" to remove the exisiting file.
 
-# $1 = Secure Partition (cactus)
+# $1 = Secure Partition
 # $2 = Platform built path
+# $3 = Ivy Shim present
 # Output = $2/sp_layout.json
 
 GENERATED_JSON=$2/sp_layout.json
+primary_dts="$1.dts"
+
+# Remove closing bracket and add comma if sp_layout file already exists
+if [ ! -f "$GENERATED_JSON" ]; then
+	echo -e "{\n" >> "$GENERATED_JSON"
+else
+	if [ "$1" == "ivy" ] && [ "$3" == "0" ]; then
+		primary_dts="ivy-sel0.dts"
+	fi
+
+	if [ $(grep "$primary_dts" "$GENERATED_JSON" | wc -l) -eq "0" ]; then
+		sed -i '$d' "$GENERATED_JSON"
+		sed -i '$ s/$/,/' "$GENERATED_JSON"
+		echo -e "\n" >> "$GENERATED_JSON"
+	else
+		exit 0
+	fi
+fi
 
 # To demonstrate communication between SP's, two cactus S-EL1 instances used.
 # To also test mapping of the RXTX region a third cactus S-EL1 instance is used.
 # cactus-primary, cactus-secondary and cactus-tertiary have same binary but
 # different partition manifests.
 if [ "$1" == "cactus" ]; then
-	echo -e "{\n\t\"$1-primary\" : {\n \
+	echo -e "\t\"$1-primary\" : {\n \
 	\t\"image\": \"$1.bin\",\n \
 	\t\"pm\": \"$1.dts\",\n \
 	\t\"owner\": \"SiP\"\n\t},\n\n\t\"$1-secondary\" : {\n \
@@ -28,13 +50,16 @@ if [ "$1" == "cactus" ]; then
 	\t\"pm\": \"$1-secondary.dts\",\n \
 	\t\"owner\": \"Plat\"\n\t},\n\n\t\"$1-tertiary\" : {\n \
 	\t\"image\": \"$1.bin\",\n \
-	\t\"pm\": \"$1-tertiary.dts\", \n \
-	\t\"owner\": \"Plat\"\n\t},\n\n\t\"ivy\" : {\n \
+	\t\"pm\": \"$1-tertiary.dts\",\n \
+	\t\"owner\": \"Plat\"\n\t}" \
+	>> "$GENERATED_JSON"
+elif [ "$1" == "ivy" ]; then
+	echo -e "\t\"ivy\" : {\n \
 	\t\"image\": \"ivy.bin\",\n \
-	\t\"pm\": \"ivy.dts\", \n \
-	\t\"owner\": \"Plat\"\n \
-	}\n}" \
-	> "$GENERATED_JSON"
+	\t\"pm\": \"$primary_dts\",\n \
+	\t\"owner\": \"Plat\"\n\t}" >> "$GENERATED_JSON"
 else
-	echo -e "\nWarning: Only Cactus is supported as Secure Partition\n"
+	echo -e "\nWarning: Secure Partition not supported\n"
 fi
+
+echo -e "}" >> "$GENERATED_JSON"
