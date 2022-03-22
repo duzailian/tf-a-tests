ca1e7470695d421a5030cbc8b832e32e6a1fe876
nabkah01 <nabil.kahlouche@arm.com>
2022-03-22 12:37:19 +0000

    test(tftf): prevent secure region access from nwd
    This change adds a TFTF test attempting an access to a memory region
    marked Secure in the GPT. It is expected this results in a GPF data
    abort being caught on the PE.
    
    Signed-off-by: Nabil Kahlouche <nabil.kahlouche@arm.com>
    Change-Id: I016775e6a8ba7b398c5770c0a296c09898ebd706

--- tftf/tests/misc_tests/test_invalid_access.c
+++ tftf/tests/misc_tests/test_invalid_access.c
@@ -72,6 +72,7 @@ test_result_t access_el3_memory_from_ns(void)
 	VERBOSE("Attempt to access el3 memory (0x%lx)\n", test_address);
 
 	data_abort_triggered = false;
+	sync_exception_triggered = false;
 	register_custom_sync_exception_handler(data_abort_handler);
 	dsbsy();
 
@@ -162,6 +163,50 @@ out_unregister:
 	return result;
 }
 
+test_result_t s_memory_cannot_be_accessed_in_ns(void)
+{
+	const uintptr_t test_address = SECURE_MEMORY_ACCESS_ADDR;
+
+	/* in non RME platform secure is root, and root test already done in other test case */
+	if (get_armv9_2_feat_rme_support() == 0U) {
+		return TEST_RESULT_SKIPPED;
+	}
+
+	VERBOSE("Attempt to access secure memory (0x%lx)\n", test_address);
+
+	data_abort_triggered = false;
+	sync_exception_triggered = false;
+	register_custom_sync_exception_handler(data_abort_handler);
+	dsbsy();
+
+	int rc = mmap_add_dynamic_region(test_address, test_address, PAGE_SIZE,
+					MT_MEMORY | MT_RW | MT_NS);
+
+	if (rc != 0) {
+		tftf_testcase_printf("%d: mmap_add_dynamic_region() = %d\n", __LINE__, rc);
+		return TEST_RESULT_FAIL;
+	}
+
+	*((volatile uint64_t *)test_address);
+
+	mmap_remove_dynamic_region(test_address, PAGE_SIZE);
+
+	dsbsy();
+	unregister_custom_sync_exception_handler();
+
+	if (sync_exception_triggered == false) {
+		tftf_testcase_printf("No sync exception while accessing (0x%lx)\n", test_address);
+		return TEST_RESULT_SKIPPED;
+	}
+
+	if (data_abort_triggered == false) {
+		tftf_testcase_printf("Sync exception is not data abort\n");
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
 #else
 
 test_result_t access_el3_memory_from_ns(void)
@@ -176,4 +221,9 @@ test_result_t rl_memory_cannot_be_accessed_in_ns(void)
 	return TEST_RESULT_SKIPPED;
 }
 
+test_result_t s_memory_cannot_be_accessed_in_ns(void)
+{
+	tftf_testcase_printf("Test not ported to AArch32\n");
+	return TEST_RESULT_SKIPPED;
+}
 #endif /* __aarch64__ */
