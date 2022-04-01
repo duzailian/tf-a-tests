1626c1714497e81bd4026643c4429506849123c0
Daniel Boulby <daniel.boulby@arm.com>
2022-04-01 12:31:52 +0100

    test(spm): run ffa_tests on ivy
    Move cactus_test_ffa.c to a common directory for SPs so the
    ivy partition can also run the same tests.
    
    We also create a sp_platform_def.h file which contains various
    plaform defines required for the tests. This header is defined
    in both cactus and ivy directories and then fvp and tc0
    subdirectories within those. The appropriate header for the sp
    and platform being built is then included in the build scripts.
    
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: If65d099d43cd930ef730539b5ad1596e686f788a

---
+++ spm/ivy/app/plat/arm/tc0/include/sp_platform_def.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021-2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * This file contains common defines for a secure partition. The correct
+ * platform_def.h header file is selected according to the secure partition
+ * and platform being built using the make scripts.
+ */
+
+#include <platform_def.h>
+
+#ifndef SP_PLATFORM_DEF_H
+#define SP_PLATFORM_DEF_H
+
+#define PLAT_ARM_DEVICE0_BASE		DEVICE0_BASE
+#define PLAT_ARM_DEVICE0_SIZE		DEVICE0_SIZE
+
+#define SP_PL011_UART_BASE		PL011_UART2_BASE
+#define SP_PL011_UART_CLK_IN_HZ		PL011_UART2_CLK_IN_HZ
+
+#define PLAT_SP_RX_BASE			ULL(0x7300000)
+
+#endif /* SP_PLATFORM_DEF_H */
