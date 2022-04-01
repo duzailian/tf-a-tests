b26bc793a461c5c7d91fbeb512720f7d4030ba47
Daniel Boulby <daniel.boulby@arm.com>
2022-04-01 12:31:52 +0100

    test(spm): run ffa_tests on ivy
    Move cactus_test_ffa.c to a common directory for SPs so the
    ivy partition can also run the same tests.
    
    We also create a sp_platform_def.h file which contains various
    plaform defines required for the tests. This header is defined
    in both cactus and ivy directories and then fvp and tc0
    subdirectories within those. The appropriate header for the sp
    and platform being built is then included in the build scripts.
    
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: If65d099d43cd930ef730539b5ad1596e686f788a

--- spm/cactus/plat/arm/tc0/include/sp_platform_def.h
+++ spm/cactus/plat/arm/tc0/include/sp_platform_def.h
@@ -4,24 +4,30 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/*
+ * This file contains common defines for a secure partition. The correct
+ * platform_def.h header file is selected according to the secure partition
+ * and platform being built using the make scripts.
+ */
+
 #include <platform_def.h>
 
-#ifndef CACTUS_PLATFORM_DEF_H
-#define CACTUS_PLATFORM_DEF_H
+#ifndef SP_PLATFORM_DEF_H
+#define SP_PLATFORM_DEF_H
 
 #define PLAT_ARM_DEVICE0_BASE		TC0_DEVICE0_BASE
 #define PLAT_ARM_DEVICE0_SIZE		TC0_DEVICE0_SIZE
 
-#define CACTUS_PL011_UART_BASE		PL011_UART1_BASE
-#define CACTUS_PL011_UART_CLK_IN_HZ	PL011_UART1_CLK_IN_HZ
+#define SP_PL011_UART_BASE		PL011_UART1_BASE
+#define SP_PL011_UART_CLK_IN_HZ		PL011_UART1_CLK_IN_HZ
 
-#define PLAT_CACTUS_RX_BASE		ULL(0xfe300000)
-#define PLAT_CACTUS_CORE_COUNT		(8)
+#define PLAT_SP_RX_BASE			ULL(0xfe300000)
+#define PLAT_SP_CORE_COUNT		(8)
 
 /* Scratch memory used for SMMUv3 driver testing purposes in Cactus SP */
 /* SMMUv3 tests are disabled for TC platform */
-#define PLAT_CACTUS_MEMCPY_BASE		ULL(0xfe400000)
-#define PLAT_CACTUS_MEMCPY_RANGE	ULL(0x8000)
+#define PLAT_SP_MEMCPY_BASE		ULL(0xfe400000)
+#define PLAT_SP_MEMCPY_RANGE		ULL(0x8000)
 
 /* Base address of user and PRIV frames in SMMUv3TestEngine */
 /* SMMUv3 tests are disabled for TC platform */
@@ -34,4 +40,4 @@
 #define CACTUS_SP3_MEM_SHARE_BASE 0xfe502000
 #define CACTUS_SP3_NS_MEM_SHARE_BASE 0x880080001000ULL
 
-#endif /* CACTUS_PLATFORM_DEF_H */
+#endif /* SP_PLATFORM_DEF_H */
