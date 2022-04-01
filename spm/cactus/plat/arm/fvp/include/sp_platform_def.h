64b4e06d2f7f37feb9385fe077b29a2c5c670cfa
Daniel Boulby <daniel.boulby@arm.com>
2022-04-01 13:54:03 +0100

    test(spm): remove cactus references in common code
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>
    Change-Id: I4f7e2e121ae8f8ebfcf5fcd6c7d09f82d3c0fa53

--- spm/cactus/plat/arm/fvp/include/sp_platform_def.h
+++ spm/cactus/plat/arm/fvp/include/sp_platform_def.h
@@ -1,26 +1,26 @@
 /*
- * Copyright (c) 2020-2021, Arm Limited. All rights reserved.
+ * Copyright (c) 2020-2022, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <platform_def.h>
 
-#ifndef CACTUS_PLATFORM_DEF_H
-#define CACTUS_PLATFORM_DEF_H
+#ifndef SP_PLATFORM_DEF_H
+#define SP_PLATFORM_DEF_H
 
 #define PLAT_ARM_DEVICE0_BASE		DEVICE0_BASE
 #define PLAT_ARM_DEVICE0_SIZE		DEVICE0_SIZE
 
-#define CACTUS_PL011_UART_BASE		PL011_UART2_BASE
-#define CACTUS_PL011_UART_CLK_IN_HZ	PL011_UART2_CLK_IN_HZ
+#define SP_PL011_UART_BASE		PL011_UART2_BASE
+#define SP_PL011_UART_CLK_IN_HZ		PL011_UART2_CLK_IN_HZ
 
-#define PLAT_CACTUS_RX_BASE		ULL(0x7300000)
-#define PLAT_CACTUS_CORE_COUNT		(8U)
+#define PLAT_SP_RX_BASE			ULL(0x7300000)
+#define PLAT_SP_CORE_COUNT		(8U)
 
 /* Scratch memory used for SMMUv3 driver testing purposes in Cactus SP */
-#define PLAT_CACTUS_MEMCPY_BASE		ULL(0x7400000)
-#define PLAT_CACTUS_MEMCPY_RANGE	ULL(0x8000)
+#define PLAT_SP_MEMCPY_BASE		ULL(0x7400000)
+#define PLAT_SP_MEMCPY_RANGE		ULL(0x8000)
 
 /* Base address of user and PRIV frames in SMMUv3TestEngine */
 #define USR_BASE_FRAME			ULL(0x2BFE0000)
@@ -32,4 +32,4 @@
 #define CACTUS_SP3_MEM_SHARE_BASE 0x7502000
 #define CACTUS_SP3_NS_MEM_SHARE_BASE 0x880080001000ULL
 
-#endif /* CACTUS_PLATFORM_DEF_H */
+#endif /* SP_PLATFORM_DEF_H */
--- spm/common/sp_def.h
+++ spm/common/sp_def.h
@@ -15,26 +15,32 @@
  */
 
 /* Up to 2 MiB at an arbitrary address that doesn't overlap the devices. */
-#define CACTUS_IMAGE_BASE		ULL(0x1000)
-#define CACTUS_IMAGE_SIZE		ULL(0x200000)
+#define SP_IMAGE_BASE		ULL(0x1000)
+#define SP_IMAGE_SIZE		ULL(0x200000)
 
 /* Memory reserved for stacks */
-#define CACTUS_STACKS_SIZE		ULL(0x1000)
+#define SP_STACKS_SIZE		ULL(0x1000)
 
 /*
  * RX/TX buffer used by VM's in SPM for memory sharing
  * Each VM allocated 2 pages, one for RX and one for TX buffer.
  */
-#define CACTUS_RX_BASE			PLAT_CACTUS_RX_BASE
-#define CACTUS_TX_BASE			CACTUS_RX_BASE + PAGE_SIZE
-#define CACTUS_RX_TX_SIZE		PAGE_SIZE * 2
+#define SP_RX_BASE			PLAT_SP_RX_BASE
+#define SP_TX_BASE			SP_RX_BASE + PAGE_SIZE
+#define SP_RX_TX_SIZE			PAGE_SIZE * 2
 
 /*
  * RX/TX buffer helpers.
  */
-#define get_sp_rx_start(sp_id) (CACTUS_RX_BASE + (((sp_id & 0x7FFFU) - 1U) * CACTUS_RX_TX_SIZE))
-#define get_sp_rx_end(sp_id) (CACTUS_RX_BASE + (((sp_id & 0x7FFFU) - 1U) * CACTUS_RX_TX_SIZE) + PAGE_SIZE)
-#define get_sp_tx_start(sp_id) (CACTUS_TX_BASE + (((sp_id & 0x7FFFU) - 1U) * CACTUS_RX_TX_SIZE))
-#define get_sp_tx_end(sp_id) (CACTUS_TX_BASE + (((sp_id & 0x7FFFU) - 1U) * CACTUS_RX_TX_SIZE) + PAGE_SIZE)
+#define get_sp_rx_start(sp_id) (SP_RX_BASE \
+				+ (((sp_id & 0x7FFFU) - 1U) * SP_RX_TX_SIZE))
+#define get_sp_rx_end(sp_id) (SP_RX_BASE \
+			      + (((sp_id & 0x7FFFU) - 1U) * SP_RX_TX_SIZE) \
+			      + PAGE_SIZE)
+#define get_sp_tx_start(sp_id) (SP_TX_BASE + \
+				(((sp_id & 0x7FFFU) - 1U) * SP_RX_TX_SIZE))
+#define get_sp_tx_end(sp_id) (SP_TX_BASE \
+			      + (((sp_id & 0x7FFFU) - 1U) * SP_RX_TX_SIZE) \
+			      + PAGE_SIZE)
 
 #endif /* CACTUS_DEF_H */
