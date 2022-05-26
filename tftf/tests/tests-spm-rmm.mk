596a5c398e449c602fc66f62c17143a11f7e4b14
nabkah01 <nabil.kahlouche@arm.com>
2022-05-26 18:39:02 +0100

    test(tftf): test Secure interrupt can preempt Realm EL1
    - Send a direct message request command to first Cactus SP to start the
      trusted watchdog timer.
    - Create and execute a busy loop to sleep the PE in the realm world for
      REALM_TIME_SLEEP ms.
    - Trusted watchdog timer expires during this time which leads to secure
      interrupt being triggered while cpu is executing in realm world.
    - Realm EL1 exits to host, but because the FIQ is still pending,
      the Host will be pre-empted to EL3.
    - Once the SP handles the interrupt, it returns execution back to normal
      world
    - Normal world parses REC's exit reason(FIQ in this case) and resumes
      REC by re-entering the realm world
    - We make sure the time elapsed in the sleep routine is not less than
      the requested value.
    
    Signed-off-by: Nabil Kahlouche <nabil.kahlouche@arm.com>
    Change-Id: I6f4cfd334777000d33924bb1239b77182a3dcea6

---
+++ tftf/tests/tests-spm-rmm.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2018-2022, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_SOURCES	+=							\
+	$(addprefix tftf/tests/runtime_services/host_realm_managment/,	\
+		host_realm_rmi.c					\
+		host_realm_helper.c				\
+		host_shared_data.c					\
+	)
+TESTS_SOURCES	+=							\
+	$(addprefix tftf/tests/runtime_services/secure_service/,	\
+		${ARCH}/ffa_arch_helpers.S				\
+		ffa_helpers.c						\
+		spm_common.c						\
+	)
+TESTS_SOURCES	+=							\
+	$(addprefix tftf/tests/runtime_services/secure_realm_service/,	\
+		test_spm_rmm.c						\
+	)
+TESTS_SOURCES	+=							\
+	$(addprefix lib/heap/,						\
+		page_alloc.c						\
+	)
