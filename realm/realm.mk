3edbfb01f0fd13f72140aafa2ee679a3d45829fc
nabkah01 <nabil.kahlouche@arm.com>
2022-06-13 19:11:10 +0100

    test(tftf): test FPU state registers context is preserved in RL/SE/NS
    Test that FPU/SIMD state are preserved during a randomly context switch
    between secure/non-secure/realm(R-EL1)worlds.
    FPU/SIMD state consist of the 32 SIMD vectors, FPCR and FPSR registers,
    the test runs for 1000 iterations with random combination of:
    SECURE_FILL_FPU, SECURE_READ_FPU, REALM_FILL_FPU, REALM_READ_FPU,
    NONSECURE_FILL_FPU, NONSECURE_READ_FPU commands,to test all possible situations
    of synchronous context switch between worlds, while the content of those registers
    is being used.
    
    Signed-off-by: Nabil Kahlouche <nabil.kahlouche@arm.com>
    Change-Id: I6da5fd334777000111924bb1239b77123a3dcea6

--- realm/realm.mk
+++ realm/realm.mk
@@ -15,6 +15,7 @@ REALM_INCLUDES :=							\
 	-Iinclude/lib/${ARCH}						\
 	-Iinclude/lib/utils						\
 	-Iinclude/lib/xlat_tables					\
+	-Iinclude/lib/extensions					\
 	-Iinclude/runtime_services					\
 	-Iinclude/runtime_services/host_realm_managment			\
 	-Irealm								\
@@ -37,7 +38,9 @@ REALM_SOURCES += lib/${ARCH}/cache_helpers.S				\
 	lib/smc/${ARCH}/smc.c						\
 	lib/exceptions/${ARCH}/sync.c					\
 	lib/locks/${ARCH}/spinlock.S					\
-	lib/delay/delay.c
+	lib/delay/delay.c						\
+	lib/extensions/fpu/aarch64/fpu_halpers.S			\
+	lib/extensions/fpu/fpu.c					\
 
 # TODO: Remove dependency on TFTF files.
 REALM_SOURCES	+=							\
--- realm/realm_payload_main.c
+++ realm/realm_payload_main.c
@@ -10,15 +10,16 @@
 #include <debug.h>
 #include <host_realm_helper.h>
 #include <host_shared_data.h>
+#include <lib/extensions/fpu.h>
 #include "realm_def.h"
 #include <realm_rsi.h>
 #include <tftf_lib.h>
 
-/**
- *   @brief    - C entry function for realm payload
- *   @param    - void
- *   @return   - void (returns to Host)
- **/
+#define SIMD_REALM_VALUE	0x33U
+#define FPCR_REALM_VALUE	0x75F9500U
+#define FPSR_REALM_VALUE	0x88000097U
+
+
 static void realm_sleep_cmd(void)
 {
 	uint64_t sleep = realm_shared_data_get_host_val(1);
@@ -45,7 +46,43 @@ static void realm_get_version_cmd(void)
 }
 
 /*
- *   @brief    - C entry function for Realm payload
+ * Fill FPU state(SIMD vectors, FPCR, FPSR) in realm world with a template values,
+ * SIMD_REALM_VALUE, FPCR_REALM_VALUE, FPSR_REALM_VALUE to be distinguished
+ * from the value in the normal and secure worlds.
+ */
+static void realm_req_fpu_fill_cmd(void)
+{
+	fpu_reg_state_t fpu_state_send;
+	fpu_state_set(&fpu_state_send, SIMD_REALM_VALUE, FPCR_REALM_VALUE, FPSR_REALM_VALUE);
+	fill_fpu_state_registers(&fpu_state_send);
+}
+
+/*
+ * compare FPU reg state from secure world side with the previously loaded values.
+ */
+static bool realm_req_fpu_cmp_cmd(void)
+{
+	fpu_reg_state_t fpu_state_send, fpu_state_receive;
+
+	fpu_state_set(&fpu_state_send, SIMD_REALM_VALUE, FPCR_REALM_VALUE, FPSR_REALM_VALUE);
+	fpu_state_set(&fpu_state_receive, 0, 0, 0);
+	/* read FPU/SIMD state registers values.*/
+	read_fpu_state_registers(&fpu_state_receive);
+	/* compare.*/
+	if (memcmp((uint8_t *)&fpu_state_send,
+			(uint8_t *)&fpu_state_receive,
+			sizeof(fpu_reg_state_t)) != 0) {
+			ERROR("REALM_PAYLOAD: realm_req_fpu_cmp_cmd faild\n");
+			return false;
+	}
+	else {
+		return true;
+	}
+
+}
+
+/**
+ *   @brief    - C entry function for realm payload
  *   @param    - void
  *   @return   - void (returns to Host)
  */
@@ -65,6 +102,13 @@ void realm_payload_main(void)
 		case REALM_GET_RSI_VERSION:
 			realm_get_version_cmd();
 			test_succeed = true;
+			break;
+		case REALM_REQ_FPU_FILL_CMD:
+			realm_req_fpu_fill_cmd();
+			test_succeed = true;
+			break;
+		case REALM_REQ_FPU_CMP_CMD:
+			test_succeed = realm_req_fpu_cmp_cmd();
 			break;
 		default:
 			INFO("REALM_PAYLOAD: %s invalid cmd=%hhu", __func__, cmd);
