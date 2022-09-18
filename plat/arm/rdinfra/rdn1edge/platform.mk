62551e3416d0e756d6aff087134c145576d4098d
Shriram K <shriram.k@arm.com>
2022-09-18 22:19:54 +0530

    feat(plat/sgi): introduce platform variant build option
    Neoverse reference design platforms can have two or more variants that
    differ in core count, cluster count or other peripherals. To allow reuse
    of platform code across all the variants of a platform, introduce build
    option CSS_SGI_PLATFORM_VARIANT for Arm Neoverse reference design
    platforms. The range of allowed values for the build option is platform
    specific.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I4559527987ee87f0f75a84f905746b1e430a2a53

--- plat/arm/rdinfra/rdn1edge/platform.mk
+++ plat/arm/rdinfra/rdn1edge/platform.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019, Arm Limited. All rights reserved.
+# Copyright (c) 2019-2022, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -11,3 +11,8 @@ PLAT_INCLUDES		+=	-Iplat/arm/rdinfra/rdn1edge/include/
 PLAT_SOURCES		+=	plat/arm/rdinfra/rdn1edge/topology.c
 
 PLAT_TESTS_SKIP_LIST	:=	plat/arm/rdinfra/rdn1edge/tests_to_skip.txt
+
+ifneq ($(CSS_SGI_PLATFORM_VARIANT),0)
+  $(error "CSS_SGI_PLATFORM_VARIANT for RD-N1-Edge should always be 0, \
+    currently set to ${CSS_SGI_PLATFORM_VARIANT}.")
+endif
--- plat/arm/sgi/sgi575/platform.mk
+++ plat/arm/sgi/sgi575/platform.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018, Arm Limited. All rights reserved.
+# Copyright (c) 2018-2022, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -11,3 +11,8 @@ PLAT_INCLUDES		+=	-Iplat/arm/sgi/sgi575/include/
 PLAT_SOURCES		+=	plat/arm/sgi/sgi575/sgi575_topology.c
 
 PLAT_TESTS_SKIP_LIST	:=	plat/arm/sgi/sgi575/tests_to_skip.txt
+
+ifneq ($(CSS_SGI_PLATFORM_VARIANT),0)
+  $(error "CSS_SGI_PLATFORM_VARIANT for SGI575 should always be 0, \
+    currently set to ${CSS_SGI_PLATFORM_VARIANT}.")
+endif
