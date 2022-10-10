a025a8cf2cf465091fc058e01af3055408c53c32
nabkah01 <nabil.kahlouche@arm.com>
2022-10-10 12:36:46 +0100

    feat: tftf realm extension
    This patch add Realm payload management capabilities to TFTF
    to act as a NS Host, it include, creation and destruction of a Realm,
    mapping of protected data and creation of all needed RTT levels
    ,sharing of NS memory buffer from Host to Realm by mapping of
    unprotected IPA, create REC and auxiliary granules, exit Realm
    using RSI_HOST_CALL ABI.
    
    Older realm_payload name is used now for only R-EL1 test cases,
    RMI and SPM test cases have been moved to new file tests-rmi-spm.
    
    New TFTF_MAX_IMAGE_SIZE argument add to FVP platform.mk,
    as an offset from where R-EL1 payload memory resources starts.
    
    Signed-off-by: Nabil Kahlouche <nabil.kahlouche@arm.com>
    Change-Id: Ida4cfd334795879d55924bb33b9b77182a3dcef7

---
+++ include/runtime_services/host_realm_managment/realm_def.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef REALM_DEF_H
+#define REALM_DEF_H
+
+#include <xlat_tables_defs.h>
+
+/* 1mb for Realm payload as a default value*/
+#define REALM_MAX_LOAD_IMG_SIZE		U(0x100000)
+#define REALM_STACK_SIZE		0x1000U
+#define DATA_PATTERN_1			0x12345678U
+#define DATA_PATTERN_2			0x11223344U
+#define REALM_SUCCESS			0U
+#define REALM_ERROR			1U
+
+/*
+ * Realm test image is position independent and can be loaded at any address.
+ * Realm images have been tested using 4KB translation granule, however,
+ * it is possible to configure MMU with any of supported translation granule
+ * size 4KB, 16KB and 64KB.
+ *
+ * Also the image size must be equal to 1MB when 4KB translation granule used.
+ * The image size requirement could be more in the case of 16KB or 64KB
+ * translation granule.
+ */
+
+#if (PAGE_SIZE == PAGE_SIZE_4KB)
+#define PAGE_ALIGNMENT			PAGE_SIZE_4KB
+#define TCR_TG0				TCR_TG0_4K
+#else
+#error "Undefined value for PAGE_SIZE"
+#endif
+
+#define VAL_SWITCH_TO_HOST_SUCCESS	5U
+#define VAL_SWITCH_TO_HOST_ERROR	6U
+#define VAL_INVALID_HVC			0xFFFFFU
+
+#endif /* REALM_DEF_H */
