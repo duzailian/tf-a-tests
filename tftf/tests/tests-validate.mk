87548130bf6d9cba641de70a893860b13dbde06e
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2022-12-05 16:09:00 +0000

    refactor: add new test suite 'tests-validate'
    The test-suite 'tests-validate' has been added, which can
    run all test cases except stress tests.
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: I42a479365df06ffcd277ace05d51658ab106ce10

---
+++ tftf/tests/tests-validate.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2022, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_MAKEFILE := $(addprefix tftf/tests/,      \
+        tests-arm-state-switch.mk               \
+        tests-boot-req.mk                       \
+        tests-cpu-extensions.mk                 \
+        tests-el3-power-state.mk                \
+        tests-performance.mk                    \
+        tests-pmu-leakage.mk                    \
+        tests-psci.mk                           \
+        tests-runtime-instrumentation.mk        \
+        tests-sdei.mk                           \
+        tests-single-fault.mk                   \
+        tests-smc.mk                            \
+        tests-spm.mk                            \
+        tests-template.mk                       \
+        tests-tftf-validation.mk                \
+        tests-trng.mk                           \
+        tests-tsp.mk                            \
+        tests-uncontainable.mk                  \
+        tests-debugfs.mk                        \
+        tests-rmi-spm.mk                        \
+        tests-realm-payload.mk                  \
+)
+
+include ${TESTS_MAKEFILE}
