8c25ecc67875c24abc643c9c416a2765f12e3e7e
Juan Pablo Conde <juanpablo.conde@arm.com>
2022-12-07 13:56:40 -0600

    feat(rng_trap): modify tests for FEAT_RNG_TRAP
    With the introduction of a handler for the trap generated upon an
    attempt to read RNDR and RNDRRS registers, the old test will not
    work anymore and needs to be changed so it does not longer expect
    the system to panic but for EL3 return a valid random number after
    handling the trap.
    
    In addition, the test configs are unified as there is no need to
    keep them separate since a system panic is not expected anymore.
    
    Signed-off-by: Juan Pablo Conde <juanpablo.conde@arm.com>
    Change-Id: I3ea4d09bbae58a906ea0be3b5448f5d3090571e7

---
+++ tftf/tests/extensions/rng_trap/test_rndrrs_trap_rng.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch_helpers.h>
+#include <arch_features.h>
+#include <test_helpers.h>
+#include <tftf.h>
+#include <tftf_lib.h>
+
+#define MAX_ITERATIONS_RNG 		5
+
+/*
+ * This test ensures that a RNDR read returns an FVP-generated
+ * pseudo-random number.
+ */
+test_result_t test_rndrrs_trap_rng(void)
+{
+#if defined __aarch64__
+	u_register_t rng1;
+	volatile u_register_t rng2;
+	unsigned int j;
+
+	/* Make sure FEAT_RNG_TRAP is supported. */
+	SKIP_TEST_IF_RNG_TRAP_NOT_SUPPORTED();
+
+	/* Attempt to read RNDRRS. */
+	VERBOSE("Attempting to read register RNDRRS...\n");
+	__asm__ volatile ("mrs %0, rndrrs\n" : "=r" (rng1));
+	VERBOSE("First random number: %lu\n", rng1);
+
+	j = 0;
+	do
+	{
+		/* Attempt to read RNDRRS */
+		VERBOSE("Attempting to read register RNDRRS...\n");
+		__asm__ volatile ("mrs %0, rndrrs\n"
+					: "=r" (rng2));
+		VERBOSE("Second random number: %lu\n", rng2);
+	} while ((++j < MAX_ITERATIONS_RNG) && (rng1 == rng2));
+
+	if (rng1 == rng2)
+	{
+		VERBOSE("Last number read: %lu\n", rng2);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+
+#else
+	/* Skip test if AArch32 */
+	SKIP_TEST_IF_AARCH32();
+#endif
+}
