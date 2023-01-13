bed1325c695e062e6bc74116c997084175f7fb7c
Manish Pandey <manish.pandey2@arm.com>
2023-01-13 23:22:26 +0000

    feat: introduce test to generate SError
    Introduce capability to generate SError in TFTF.
    One of the usecases of this capability is to test EL3 feature of
    trapping lower ELs SError.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I4bc78edf21f9329f1ed76b0ae27db223a83a9670

---
+++ tftf/tests/misc_tests/test_inject_serror.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+#include <arch_helpers.h>
+#include <mmio.h>
+#include <tftf_lib.h>
+#include <xlat_tables_v2.h>
+
+/*
+ * Purpose of this test to ensure the crash dump for lower ELs in EL3 works fine.
+ *
+ * This test never returns, it ends up with a crash in EL3.
+ *
+ * This test maps an non-existent memory as Device memory and access it. Memory
+ * is mapped as device memory to go past MMU and cauase on error on bus and
+ * trap as an SError.
+ * This test is used in conjunction with HANDLE_EA_EL3_FIRST_NS feature
+ * (trapping EA in lower ELs to EL3) in TF-A.
+ * SError caused by this error will be trapped in EL3 and eventually cause a
+ * panic along with printing Crash Dump for lower EL.
+ */
+test_result_t test_inject_serror(void)
+{
+	const uintptr_t test_address = 0x7FFFF000;
+
+	int rc = mmap_add_dynamic_region(test_address, test_address, PAGE_SIZE,
+						MT_DEVICE | MT_RW | MT_NS);
+	if (rc != 0) {
+		tftf_testcase_printf("%d: mmap_add_dynamic_region() = %d\n", __LINE__, rc);
+		return TEST_RESULT_FAIL;
+	}
+
+	/* Try to write to invalid address */
+	mmio_write_32(test_address, 0x1);
+
+	return TEST_RESULT_SUCCESS;
+}
