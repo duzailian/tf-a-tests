43fbd6abff5ea6bd26b330434bfbe224a8f5f907
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2023-01-16 17:58:47 +0000

    feat(sme): add basic SME2 tests
    FEAT_SME2 introduces an architectural register ZT0 to support
    lookup table feature. This patch ensures that EL-3 has
    properly enabled the SME2 for use at lower exception levels,
    thereby avoiding the traps excecution at lower exception levels,
    when instructions access ZT0 register to EL-3.
    
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
    Change-Id: I46d51184b74c1e82c88344530601f2a3c1aee8ea

--- lib/extensions/sme/aarch64/sme.c
+++ lib/extensions/sme/aarch64/sme.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2021-2023, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,7 +9,9 @@
 
 #include <arch.h>
 #include <arch_helpers.h>
+#include <debug.h>
 #include <lib/extensions/sme.h>
+
 
 #ifdef __aarch64__
 
@@ -19,21 +21,20 @@
  * Return
  *   true if SME supported, false if not.
  */
-bool feat_sme_supported(void)
+bool is_feat_sme_supported(void)
 {
 	uint64_t features;
 
 	features = read_id_aa64pfr1_el1() >> ID_AA64PFR1_EL1_SME_SHIFT;
-	return (features & ID_AA64PFR1_EL1_SME_MASK) != 0U;
+	return (features & ID_AA64PFR1_EL1_SME_MASK) >= ID_AA64PFR1_EL1_SME_SUPPORTED;
 }
 
 /*
- * feat_sme_fa64_supported
- *  Check if FEAT_SME_FA64 is supported.
- * Return
- *  True if supported, false if not.
+ * is_feat_sme_fa64_supported
+ *   Check if FEAT_SME_FA64 is supported.
+ * Return: True if supported, false if not.
  */
-bool feat_sme_fa64_supported(void)
+bool is_feat_sme_fa64_supported(void)
 {
 	uint64_t features;
 
@@ -52,7 +53,7 @@ int sme_enable(void)
 	u_register_t reg;
 
 	/* Make sure SME is supported. */
-	if (!feat_sme_supported()) {
+	if (!is_feat_sme_supported()) {
 		return -1;
 	}
 
@@ -75,42 +76,67 @@ int sme_enable(void)
 
 /*
  * sme_smstart
- *   This function enables streaming mode and optinally enables ZA array access
- *   at the same time.
+ *   This function enables streaming mode and ZA array storage access
+ * independently or together based on the type of instruction variant.
  * Parameters
- *   enable_za: If set, ZA access is enabled.  If cleared, ZA bit is untouched.
+ *   smstart_type: If SMSTART, streaming mode and ZA access is enabled
+ *                 If SMSTART_SM, streaming mode enabled.
+ *                 If SMSTART_ZA enables SME ZA storage and, ZT0 storage access.
  */
-void sme_smstart(bool enable_za)
+void sme_smstart(smestart_instruction_type_t smstart_type)
 {
-	u_register_t svcr = SVCR_SM_BIT;
+	u_register_t svcr = 0ULL;
 
-	if (enable_za) {
-		svcr |= SVCR_ZA_BIT;
+	switch(smstart_type) {
+		case SMSTART:
+			svcr = ( SVCR_SM_BIT | SVCR_ZA_BIT );
+	   		break;
+
+	   	case SMSTART_SM:
+			svcr = SVCR_SM_BIT ;
+	   		break;
+
+	   	case SMSTART_ZA:
+			svcr = SVCR_ZA_BIT ;
+	   		break;
+
+		default:
+			ERROR("Illegal SMSTART Instruction Variant \n");
+			break;
 	}
-
 	write_svcr(read_svcr() | svcr);
 }
 
 /*
  * sme_smstop
- *   This function disables streaming mode OR disables ZA array access but not
- *   both.  It might seem strange but this is the functionality of the SMSTOP
- *   assembly instruction.
+ *   This function exits streaming mode and disables ZA array storage access
+ * independently or together based on the type of instruction variant.
  * Parameters
- *   disable_za: If set, ZA access is disabled but streaming mode is not
- *               affected.  If clear, streaming mode is exited and ZA bit is
- *               left alone.
+ *   smstop_type: If SMSTOP, exits streaming mode and ZA access is disabled
+ *                If SMSTOP_SM, exits streaming mode.
+ *                If SMSTOP_ZA disables SME ZA storage and, ZT0 storage access.
  */
-void sme_smstop(bool disable_za)
+void sme_smstop(smestop_instruction_type_t smstop_type)
 {
-	u_register_t svcr;
+	u_register_t svcr = 0ULL;
 
-	if (disable_za) {
-		svcr = ~SVCR_ZA_BIT;
-	} else {
-		svcr = ~SVCR_SM_BIT;
+	switch(smstop_type) {
+		case SMSTOP:
+			svcr = (~SVCR_SM_BIT) & (~SVCR_ZA_BIT);
+	   		break;
+
+	   	case SMSTOP_SM:
+			svcr = ~SVCR_SM_BIT ;
+	   		break;
+
+	   	case SMSTOP_ZA:
+			svcr = ~SVCR_ZA_BIT ;
+	   		break;
+
+		default:
+			ERROR("Illegal SMSTOP Instruction Variant \n");
+			break;
 	}
-
 	write_svcr(read_svcr() & svcr);
 }
 
