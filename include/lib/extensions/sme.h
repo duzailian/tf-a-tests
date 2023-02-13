97ca8f11f4255d3c0687f47fb92bd68542d256f6
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2023-02-13 12:15:11 +0000

    feat(sme): update sme/mortlach tests
    FEAT_SME is an optional architectural extension from v9.2.
    Previously due to the lack of suport in toolchain, testing
    SME instructions were overlooked and minimal tests were added.
    
    This patch addresses them, with additional tests to test
    the SME instructions. In order to avoid toolchain requirements
    we manually encode the instructions for accessing ZA array.
    
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
    Change-Id: Ia9edd2711d548757b96495498bf9d47b9db68a09

--- include/lib/extensions/sme.h
+++ include/lib/extensions/sme.h
@@ -7,8 +7,11 @@
 #ifndef SME_H
 #define SME_H
 
-#define SME_SMCR_LEN_MAX U(0x1FF)
+#define MAX_VL                  (512)
+#define MAX_VL_B                (MAX_VL / 8)
+#define SME_SMCR_LEN_MAX	U(0x1FF)
 
+/* SME feature related prototypes */
 bool feat_sme_supported(void);
 bool feat_sme_fa64_supported(void);
 int sme_enable(void);
@@ -18,5 +21,7 @@ void sme_smstop(bool disable_za);
 /* Assembly function prototypes */
 uint64_t sme_rdvl_1(void);
 void sme_try_illegal_instruction(void);
+void sme_vector_to_ZA(const uint64_t *input_vector);
+void sme_ZA_to_vector(const uint64_t *output_vector);
 
 #endif /* SME_H */
