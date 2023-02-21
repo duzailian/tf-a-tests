781c2b99a73c067871b69e42871c7cacfc2c2fd0
Manish Pandey <manish.pandey2@arm.com>
2023-02-21 13:05:07 +0000

    refactor(ras): rename sdei related functions/events
    Function to report sdei event was named on the basis of serror which has
    caused that error. SError can be used in many cases and SDEI is one of
    its usecases.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I451008a7e5bfa34d8c4e8fb3c0c59b90c4bb7e5f

--- tftf/tests/misc_tests/test_single_fault.c
+++ tftf/tests/misc_tests/test_single_fault.c
@@ -11,13 +11,13 @@
 
 #ifdef __aarch64__
 
-uint64_t serror_received;
+uint64_t sdei_event_received;
 
 extern void inject_unrecoverable_ras_error(void);
 
-int serror_handler(int ev, uint64_t arg)
+int sdei_handler(int ev, uint64_t arg)
 {
-	serror_received = 1;
+	sdei_event_received = 1;
 	tftf_testcase_printf("SError SDEI event received.\n");
 
 	return 0;
