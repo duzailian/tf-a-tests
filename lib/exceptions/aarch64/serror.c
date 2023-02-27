4691af3dfd8031f80d1dd5a20c3730d0c6f295ae
Manish Pandey <manish.pandey2@arm.com>
2023-02-27 13:23:06 +0000

    feat: introduce SError exception handler
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: Idbe37d3690e3a8e08fa3b0dff496d18d3022a8fc

---
+++ lib/exceptions/aarch64/serror.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdbool.h>
+
+#include <arch_helpers.h>
+#include <debug.h>
+#include <serror.h>
+
+static exception_handler_t custom_serror_handler;
+
+void register_custom_serror_handler(exception_handler_t handler)
+{
+	custom_serror_handler = handler;
+}
+
+void unregister_custom_serror_handler(void)
+{
+	custom_serror_handler = NULL;
+}
+
+bool tftf_serror_handler(void)
+{
+	bool resume = false;
+
+	if (custom_serror_handler == NULL) {
+		return false;
+	}
+
+	resume = custom_serror_handler();
+
+	return resume;
+}
