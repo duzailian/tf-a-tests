95e43b6b1e07b88a67b97b498e03580d804aac87
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-03-13 19:37:46 +0000

    feat(rme): add PMU Realm tests
    This patch adds Realm PMU payload tests with
    PMU interrupt handling.
    
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>
    Change-Id: I86ef96252e04c57db385e129227cc0d7dcd1fec2

--- tftf/tests/tests-invalid-access.mk
+++ tftf/tests/tests-invalid-access.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (c) 2022, Arm Limited. All rights reserved.
+# Copyright (c) 2022-2023, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 TFTF_INCLUDES +=							\
 	-Iinclude/runtime_services/host_realm_managment
+
 
 TESTS_SOURCES	+=	tftf/tests/misc_tests/test_invalid_access.c
 
