4682469673ecb19494861830f163e5a056128395
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-03-20 19:01:27 +0000

    feat(rme): add PMU interrupt test
    This patch adds PMU interrupt test for Realm payload
    test suite.
    
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>
    Change-Id: I4ed36ab11efc9e51670c3c1eb91053d97c0aefc0

---
+++ include/runtime_services/host_realm_managment/host_realm_pmu.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+#ifndef HOST_REALM_PMU_H
+#define HOST_REALM_PMU_H
+
+/* PMU physical interrupt */
+#define PMU_PPI		23UL
+
+/* PMU virtual interrupt */
+#define PMU_VIRQ	PMU_PPI
+
+#endif /* HOST_REALM_PMU_H */
