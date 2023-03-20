cd3c031bff556a4ee9bff3733e2eaed0dde5f863
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-03-20 19:01:27 +0000

    feat(rme): add PMU interrupt test
    This patch adds PMU interrupt test for Realm payload
    test suite.
    
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>
    Change-Id: I4ed36ab11efc9e51670c3c1eb91053d97c0aefc0

--- realm/realm_interrupt.c
+++ realm/realm_interrupt.c
@@ -1,13 +1,24 @@
 /*
- * Copyright (c) 2022, Arm Limited. All rights reserved.
+ * Copyright (c) 2022-2023, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <debug.h>
+#include <arch_helpers.h>
+#include <host_realm_pmu.h>
 
-/* Dummy interrupt handler as for now */
+/* Realm interrupt handler */
 void realm_interrupt_handler(void)
 {
-	INFO("%s\n", __func__);
+	/* Read INTID and acknowledge interrupt */
+	unsigned long iar1_el1 = read_icv_iar1_el1();
+
+	/* Deactivate interrupt */
+	write_icv_eoir1_el1(iar1_el1);
+
+	/* Clear PMU interrupt */
+	if (iar1_el1 == PMU_VIRQ) {
+		write_pmintenclr_el1(read_pmintenset_el1());
+		isb();
+	}
 }
