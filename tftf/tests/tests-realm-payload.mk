2e430107d9e89f41a8aa5ba71ef6ceeb3bd63b35
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2023-04-05 15:30:18 +0100

    feat(rme): add SVE Realm tests
    Verifies Realm with SVE support. Below tests are added
    - Check whether RMI features reports proper SVE VL
    - Create SVE Realm and check rdvl result
    - Create SVE Realm with invalid VL and check if it fails
    - Create SVE Realm and test ID registers
    - Create non SVE Realm and test ID registers
    - Create SVE Realm and probe all supported VLs
    - Check RMM preserves NS ZCR_EL2 register
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: I98a20f34ce72c7c1a353ed13678870168fa27c48

--- tftf/tests/tests-realm-payload.mk
+++ tftf/tests/tests-realm-payload.mk
@@ -11,6 +11,7 @@ TESTS_SOURCES	+=							\
 	$(addprefix tftf/tests/runtime_services/realm_payload/,		\
 		host_realm_payload_tests.c				\
 		host_realm_spm.c					\
+		host_realm_payload_sve_tests.c				\
 	)
 
 TESTS_SOURCES	+=							\
