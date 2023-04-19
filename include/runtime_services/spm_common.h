f4aa9726026bb4df258102e12e14a4bb48b64b73
Shruti Gupta <shruti.gupta@arm.com>
2023-04-19 17:00:38 +0100

    refactor(tftf): move FPU save/restore routine to common lib
    - Move FPU routines to common lib
    - Additionally store FPSR/FPCR
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: I88f0a9f716aafdd634c4eae5b885f839bb3deb00

--- include/runtime_services/spm_common.h
+++ include/runtime_services/spm_common.h
@@ -97,27 +97,15 @@ bool is_expected_cactus_response(struct ffa_value ret, uint32_t expected_resp,
 void dump_ffa_value(struct ffa_value ret);
 
 /*
- * Max. vector length:
- * SIMD: 128 bits = 16 bytes
- */
-#define SIMD_VECTOR_LEN_BYTES		16
-
-#define SIMD_NUM_VECTORS		32
-
-typedef uint8_t simd_vector_t[SIMD_VECTOR_LEN_BYTES];
-
-/*
  * Fills SIMD/SVE registers with the content of the container v.
  * Number of vectors is assumed to be SIMD/SVE_NUM_VECTORS.
  */
-void fill_simd_vector_regs(const simd_vector_t v[SIMD_NUM_VECTORS]);
 void fill_sve_vector_regs(const sve_vector_t v[SVE_NUM_VECTORS]);
 
 /*
  * Reads contents of SIMD/SVE registers into the provided container v.
  * Number of vectors is assumed to be SIMD/SVE_NUM_VECTORS.
  */
-void read_simd_vector_regs(simd_vector_t v[SIMD_NUM_VECTORS]);
 void read_sve_vector_regs(sve_vector_t v[SVE_NUM_VECTORS]);
 
 bool check_spmc_execution_level(void);
