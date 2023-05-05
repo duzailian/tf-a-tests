duzailian/tf-a-tests

commit ce372779a0b9958ed34de61a63a7ce058f43fb2a

feat(build): add support for new binutils versions
Users of GNU ld (BFD) from binutils 2.39+ will observe multiple instaces
of a new warning when linking the tftf.elf in the form:

ld.bfd: warning: tftf.elf has a LOAD segment with RWX permissions

These new warnings are enbaled by default to secure elf binaries:
 - https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=ba951afb99912da01a6e8434126b8fac7aa75107
 - https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=0d38576a34ec64a1b4500c9277a8e9d0f07e6774

Linux kernel fixed it as listed here:
https://lore.kernel.org/all/20220810222442.2296651-1-ndesaulniers@google.com/

Following this, we have resolved the same in TF-A as well:
https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/19401

Replicating the same for TFTF as well, by adding
--no-warn-rwx-segments since this a ld.bfd related.

Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
Change-Id: I191e6c62ca6e437ea8c696d823b44d6c6da48ab6

Makefile
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018-2022, Arm Limited. All rights reserved.
+# Copyright (c) 2018-2023, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -228,6 +228,10 @@ COMMON_CFLAGS		+=	-ffunction-sections -fdata-sections
 COMMON_CFLAGS 		+=	${CFLAGS} ${INCLUDES}
 
 COMMON_LDFLAGS		+=	--fatal-warnings -O1 --gc-sections --build-id=none
+
+# With ld.bfd version 2.39 and newer new warnings are added. Skip those since we
+# are not loaded by a elf loader.
+COMMON_LDFLAGS		+=	--no-warn-rwx-segments
 
 CC			:=	${CROSS_COMPILE}gcc
 CPP			:=	${CROSS_COMPILE}cpp

commit bbc6ae4a448c3ff0513b934f2e02131ee2deb1a5

fix(build): resolve build issue for olcer toolchain
The patch ce37277 introduced support for new toolchain but
the LD option supplied is not recognized by older toolchains.
This patch fixes the problem by ensuring that the LD option
is supported before being added to LDFLAGS.

Signed-off-by: Soby Mathew <soby.mathew@arm.com>
Change-Id: I43b2c407af49992f2951c071c54decc5a05ec6ea

Makefile
@@ -231,7 +231,7 @@ COMMON_LDFLAGS		+=	--fatal-warnings -O1 --gc-sections --build-id=none
 
 # With ld.bfd version 2.39 and newer new warnings are added. Skip those since we
 # are not loaded by a elf loader.
-COMMON_LDFLAGS		+=	--no-warn-rwx-segments
+COMMON_LDFLAGS		+=	$(call ld_option, --no-warn-rwx-segments)
 
 CC			:=	${CROSS_COMPILE}gcc
 CPP			:=	${CROSS_COMPILE}cpp

make_helpers/build_macros.mk
@@ -28,3 +28,9 @@ $(if $(word $(2), $($(1))),\
   $(call CREATE_SEQ,$(1),$(2))\
 )
 endef
+
+# Convenience function to check for a given linker option. An call to
+# $(call ld_option, --no-XYZ) will return --no-XYZ if supported by the linker
+define ld_option
+	$(shell if $(LD) $(1) -v >/dev/null 2>&1; then echo $(1); fi )
+endef

tftf/tests/tests-standard.xml
@@ -30,21 +30,6 @@
 
 <testsuites>
 
-  &tests-tftf-validation;
-  &tests-boot-req;
-  &tests-psci;
-  &tests-sdei;
-  &tests-rt-instr;
-  &tests-trng;
-  &tests-tsp;
-  &tests-el3-pstate;
-  &tests-state-switch;
-  &tests-cpu-extensions;
-  &tests-performance;
-  &tests-smc;
-  &tests-spm;
-  &tests-pmu-leakage;
-  &tests-debugfs;
   &tests-rmi-spm;
   &tests-realm-payload;
 