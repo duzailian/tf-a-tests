1c9e076effd9c2f5b81195c4bfb4ba29430f332a
Karl Meakin <karl.meakin@arm.com>
2023-05-11 11:20:47 +0100

    [WIP] test(memory sharing): add test for hypervisor retrieve request
    Change-Id: If27d8d5b515c5e468977ed4f5f4ddeb86e44c5df
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>

--- tftf/tests/runtime_services/secure_service/test_ffa_memory_sharing.c
+++ tftf/tests/runtime_services/secure_service/test_ffa_memory_sharing.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022, Arm Limited. All rights reserved.
+ * Copyright (c) 2020-2023, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -448,4 +448,64 @@ test_result_t test_mem_share_to_sp_clear_memory(void)
 	}
 
 	return TEST_RESULT_SUCCESS;
+}
+
+test_result_t test_foo(void) {
+        struct ffa_memory_region_constituent constituents[] = {
+            {(void *)share_page, 1, 1}};
+        const uint32_t constituents_count =
+            sizeof(constituents) / sizeof(struct ffa_memory_region_constituent);
+        struct mailbox_buffers mb;
+        ffa_memory_handle_t handle;
+        struct ffa_value ret;
+
+        ffa_id_t hypervisor = HYP_ID;
+        ffa_id_t sp = SP_ID(1);
+
+        CHECK_SPMC_TESTING_SETUP(1, 1, expected_sp_uuids);
+
+        GET_TFTF_MAILBOX(mb);
+
+        // 1. Share
+        handle = memory_init_and_send(
+            (struct ffa_memory_region *)mb.send, MAILBOX_SIZE, hypervisor, sp,
+            constituents, constituents_count, FFA_MEM_SHARE_SMC32, &ret);
+
+        if (handle == FFA_MEMORY_HANDLE_INVALID) {
+                ERROR("Memory share failed: %d\n", ffa_error_code(ret));
+                return TEST_RESULT_FAIL;
+        }
+
+        VERBOSE("Memory has been shared!\n");
+
+        // 2. Retrieve
+        ffa_memory_retrieve_request_init(mb.send, handle, hypervisor, 0, 0, 0,
+                                         0, 0, 0, 0, 0);
+        ((struct ffa_memory_region *)mb.send)->receiver_count = 0;
+        ret = ffa_mem_retrieve_req(sizeof(struct ffa_memory_region),
+                                   sizeof(struct ffa_memory_region));
+
+        if (ffa_func_id(ret) != FFA_MEM_RETRIEVE_RESP) {
+                ERROR("Memory retrieve failed: (FID = %lu, error = %d)\n",
+                      ret.fid, ffa_error_code(ret));
+                return false;
+        }
+
+        // 2.5. Verify
+        struct ffa_memory_region *region = mb.send;
+        assert(region->sender == hypervisor);
+        assert(region->attributes == 0);
+        assert(region->flags == 0);
+        assert(region->handle == handle);
+        assert(region->tag == 0);
+        assert(region->receiver_count == 0);
+
+        // 3. Reclaim
+        ret = ffa_mem_reclaim(handle, 0);
+        if (is_ffa_call_error(ret)) {
+                ERROR("Memory reclaim failed: %d\n", ffa_error_code(ret));
+                return TEST_RESULT_FAIL;
+        }
+
+        return TEST_RESULT_SUCCESS;
 }
