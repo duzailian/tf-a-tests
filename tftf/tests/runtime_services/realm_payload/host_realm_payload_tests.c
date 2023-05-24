8b69c99d64ffad15d2238b04ef914b416992c556
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-05-24 12:42:40 +0100

    feat(rme): add extra tests for FEAT_LPA2 on RMM
    This patch adds two new tests for RMM:
    
      * Test realm creation with no LPA2 and -1 RTT starting level
      * Test realm creation with no LPA2 and S2SZ > 48 bits
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I05312814f0dc867104618c5f34d819e9b0d76355

--- tftf/tests/runtime_services/realm_payload/host_realm_payload_tests.c
+++ tftf/tests/runtime_services/realm_payload/host_realm_payload_tests.c
@@ -36,7 +36,8 @@ test_result_t host_test_realm_create_enter(void)
 			(u_register_t)PAGE_POOL_BASE,
 			(u_register_t)(PAGE_POOL_MAX_SIZE +
 			NS_REALM_SHARED_MEM_SIZE),
-			(u_register_t)PAGE_POOL_MAX_SIZE, 0UL)) {
+			(u_register_t)PAGE_POOL_MAX_SIZE,
+			RMI_FEATURE_REGISTER_0_LPA2)) {
 		return TEST_RESULT_FAIL;
 	}
 	if (!host_create_shared_mem(NS_REALM_SHARED_MEM_BASE,
@@ -124,7 +125,8 @@ static test_result_t host_test_realm_pmuv3(uint8_t cmd)
 	host_set_pmu_state();
 
 	feature_flag = RMI_FEATURE_REGISTER_0_PMU_EN |
-			INPLACE(FEATURE_PMU_NUM_CTRS, (unsigned long long)(-1));
+			INPLACE(FEATURE_PMU_NUM_CTRS, (unsigned long long)(-1)) |
+			RMI_FEATURE_REGISTER_0_LPA2;
 
 	if (!host_create_realm_payload((u_register_t)REALM_IMAGE_BASE,
 			(u_register_t)PAGE_POOL_BASE,
