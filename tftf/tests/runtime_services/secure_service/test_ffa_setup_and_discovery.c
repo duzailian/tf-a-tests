95cf83f8245bdac5c9f4584f023787dc0052cc44
Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
2023-06-06 09:03:53 +0100

    build(clang): introduce clang toolchain support
    Only the compiler is switched to clang. The assembler and the
    linker are provided  by the GCC toolchain.
    
    clang is used to build TFTF when the base name of the path assigned
    to  "CC" variable contains the string 'clang'.
    
    'CROSS_COMPILE' flg is still required and must point to the
    appropriate GCC toolchain.
    
    *Note:
    Compiling TF-A tests with clang toolchain has following
    issues and as a result few sections of the code have
    been commented or guarded accordingly:
    
    1. error: instruction requires: wfxt
       fix : build with ARM_ARCH_MINOR=7.
    
    2. error: initializer element is not a compile-time constant
       test_ffa_setup_and_discovery.c:33:11[.uuid = sp_uuids[0]]
       sp_test_ffa.c:38:11:.uuid = sp_uuids[0]
    
       fix: gcc overlooks this error but clang doesn't. Hence
       initializer elements are explicitly specified.
    
    This is an introductory patch to cover the "tftf" target only.
    
    Other tf-a tests targets need to be ported and will be
    implemented in separate patches. In summary, with this patch,
    we will be able to compile TFTF code covering tftf target
    with the following command:
    
    **********************************************************
    * Build TFTF Aarch64 ( Default Build ) with Clang support
    * tested with clang version: 14.0.0
    **********************************************************
    make CC=${CLANG_PATH}/clang \
    PLAT=fvp \
    CROSS_COMPILE=aarch64-none-elf- \
    ARM_ARCH_MINOR=7 \
    V=0 tftf
    ********************************************************
    
    Signed-off-by: Jayanth Dodderi Chidanand <jayanthdodderi.chidanand@arm.com>
    Change-Id: I409d11b596dfd50a3d25598679808dd9fcfc51e5

--- tftf/tests/runtime_services/secure_service/test_ffa_setup_and_discovery.c
+++ tftf/tests/runtime_services/secure_service/test_ffa_setup_and_discovery.c
@@ -30,7 +30,7 @@ static const struct ffa_partition_info ffa_expected_partition_info[] = {
 		.properties = FFA_PARTITION_AARCH64_EXEC |
 			      FFA_PARTITION_DIRECT_REQ_RECV |
 			      FFA_PARTITION_NOTIFICATION,
-		.uuid = sp_uuids[0]
+		.uuid = {PRIMARY_UUID}
 	},
 	/* Secondary partition info */
 	{
@@ -39,7 +39,7 @@ static const struct ffa_partition_info ffa_expected_partition_info[] = {
 		.properties = FFA_PARTITION_AARCH64_EXEC |
 			      FFA_PARTITION_DIRECT_REQ_RECV |
 			      FFA_PARTITION_NOTIFICATION,
-		.uuid = sp_uuids[1]
+		.uuid = {SECONDARY_UUID}
 	},
 	/* Tertiary partition info */
 	{
@@ -48,7 +48,7 @@ static const struct ffa_partition_info ffa_expected_partition_info[] = {
 		.properties = FFA_PARTITION_AARCH64_EXEC |
 			      FFA_PARTITION_DIRECT_REQ_RECV |
 			      FFA_PARTITION_NOTIFICATION,
-		.uuid = sp_uuids[2]
+		.uuid = {TERTIARY_UUID}
 	},
 	/* Ivy partition info */
 	{
@@ -56,7 +56,7 @@ static const struct ffa_partition_info ffa_expected_partition_info[] = {
 		.exec_context = IVY_EXEC_CTX_COUNT,
 		.properties = FFA_PARTITION_AARCH64_EXEC |
 			      FFA_PARTITION_DIRECT_REQ_RECV,
-		.uuid = sp_uuids[3]
+		.uuid = {IVY_UUID}
 	}
 };
 
