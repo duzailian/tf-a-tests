b4e08adab93b084796e4992e41a0301a43879ede
Shruti Gupta <shruti.gupta@arm.com>
2023-07-14 13:24:58 +0100

    test(bti): enable stage 1 translation
    * Enable Stage 1 transalation for Realm payload.
    * Enable BTI for Realm payload.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: Ifca79e8f560c04b7324d4b002707b15482d25776

--- realm/realm.mk
+++ realm/realm.mk
@@ -58,6 +58,7 @@ REALM_SOURCES +=	lib/extensions/pauth/aarch64/pauth.c            \
 			lib/extensions/pauth/aarch64/pauth_helpers.S
 
 REALM_INCLUDES +=	-Iinclude/lib/extensions
+PLAT_XLAT_TABLES_DYNAMIC := 1
 
 REALM_DEFINES:=
 $(eval $(call add_define,REALM_DEFINES,ARM_ARCH_MAJOR))
@@ -66,3 +67,4 @@ $(eval $(call add_define,REALM_DEFINES,ENABLE_BTI))
 $(eval $(call add_define,REALM_DEFINES,ENABLE_PAUTH))
 $(eval $(call add_define,REALM_DEFINES,LOG_LEVEL))
 $(eval $(call add_define,REALM_DEFINES,IMAGE_REALM))
+$(eval $(call add_define,REALM_DEFINES,PLAT_XLAT_TABLES_DYNAMIC))
