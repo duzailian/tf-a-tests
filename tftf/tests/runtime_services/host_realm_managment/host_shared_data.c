14387ce0a605088f06c452f311bade6795adbe40
Shruti Gupta <shruti.gupta@arm.com>
2023-08-21 11:51:52 +0100

    test(rmm): add Realm support for multiple rec and CPU
    - Add support for multiple rec and CPU in Realm.
    - Add support for PSCI Complete SMC call.
    - Update shared data structure between host and realm.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: I5d0b1b28244000c7ed981736ab08516e5a37e795

--- tftf/tests/runtime_services/host_realm_managment/host_shared_data.c
+++ tftf/tests/runtime_services/host_realm_managment/host_shared_data.c
@@ -6,18 +6,24 @@
 
 #include <string.h>
 
+#include <assert.h>
 #include <host_realm_mem_layout.h>
 #include <host_shared_data.h>
 
-static host_shared_data_t *host_shared_data =
-		((host_shared_data_t *)(NS_REALM_SHARED_MEM_BASE));
-static host_shared_data_t *guest_shared_data;
+static host_shared_data_t *host_shared_data[MAX_REC_COUNT];
+static host_shared_data_t *guest_shared_data[MAX_REC_COUNT];
 /*
  * Return shared buffer pointer mapped as host_shared_data_t structure
  */
-host_shared_data_t *host_get_shared_structure(void)
+host_shared_data_t *host_get_shared_structure(unsigned int rec_num)
 {
-	return host_shared_data;
+	assert(rec_num < MAX_REC_COUNT);
+	return host_shared_data[rec_num];
+}
+
+void host_set_shared_structure(host_shared_data_t *guest_shared_data)
+{
+	*host_shared_data = guest_shared_data;
 }
 
 /*
@@ -25,83 +31,92 @@ host_shared_data_t *host_get_shared_structure(void)
  */
 void realm_set_shared_structure(host_shared_data_t *ptr)
 {
-	guest_shared_data = ptr;
+	*guest_shared_data = ptr;
 }
 
 /*
  * Get guest mapped shared buffer pointer
  */
-host_shared_data_t *realm_get_shared_structure(void)
+host_shared_data_t *realm_get_shared_structure(unsigned int rec_num)
 {
-	return guest_shared_data;
+	assert(rec_num < MAX_REC_COUNT);
+	return guest_shared_data[rec_num];
 }
 
 /*
  * Set data to be shared from Host to realm
  */
-void realm_shared_data_set_host_val(uint8_t index, u_register_t val)
+void realm_shared_data_set_host_val(unsigned int rec_num, uint8_t index, u_register_t val)
 {
-	host_shared_data->host_param_val[(index >= MAX_DATA_SIZE) ?
-		(MAX_DATA_SIZE - 1) : index] = val;
+	assert(rec_num < MAX_REC_COUNT);
+	assert(index < MAX_DATA_SIZE);
+	host_shared_data[rec_num]->host_param_val[index] = val;
 }
 
 /*
  * Set data to be shared from realm to Host
  */
-void realm_shared_data_set_realm_val(uint8_t index, u_register_t val)
+void realm_shared_data_set_realm_val(unsigned int rec_num, uint8_t index, u_register_t val)
 {
-	host_shared_data->realm_out_val[(index >= MAX_DATA_SIZE) ?
-		(MAX_DATA_SIZE - 1) : index] = val;
+	assert(rec_num < MAX_REC_COUNT);
+	assert(index < MAX_DATA_SIZE);
+	host_shared_data[rec_num]->realm_out_val[index] = val;
 }
 
 /*
  * Return Host's data at index
  */
-u_register_t realm_shared_data_get_host_val(uint8_t index)
+u_register_t realm_shared_data_get_host_val(unsigned int rec_num, uint8_t index)
 {
-	return guest_shared_data->host_param_val[(index >= MAX_DATA_SIZE) ?
-		(MAX_DATA_SIZE - 1) : index];
+	assert(rec_num < MAX_REC_COUNT);
+	assert(index < MAX_DATA_SIZE);
+	return guest_shared_data[rec_num]->host_param_val[index];
 }
 
 /*
  * Return Realm's data at index
  */
-u_register_t realm_shared_data_get_realm_val(uint8_t index)
+u_register_t realm_shared_data_get_realm_val(unsigned int rec_num, uint8_t index)
 {
-	return host_shared_data->realm_out_val[(index >= MAX_DATA_SIZE) ?
-		(MAX_DATA_SIZE - 1) : index];
+	assert(rec_num < MAX_REC_COUNT);
+	assert(index < MAX_DATA_SIZE);
+	return host_shared_data[rec_num]->realm_out_val[index];
 }
 
 /*
  * Clear shared realm data
  */
-void realm_shared_data_clear_realm_val(void)
+void realm_shared_data_clear_realm_val(unsigned int rec_num)
 {
-	(void)memset((char *)host_shared_data->realm_out_val, 0,
-	MAX_DATA_SIZE * sizeof(host_shared_data->realm_out_val[0]));
+	assert(rec_num < MAX_REC_COUNT);
+	(void)memset((char *)host_shared_data[rec_num]->realm_out_val, 0,
+	MAX_DATA_SIZE * sizeof(host_shared_data[0U]->realm_out_val));
 }
 
 /*
  * Clear shared Host data
  */
-void realm_shared_data_clear_host_val(void)
+void realm_shared_data_clear_host_val(unsigned int rec_num)
 {
-	(void)memset((char *)host_shared_data->host_param_val, 0,
-	MAX_DATA_SIZE * sizeof(host_shared_data->host_param_val[0]));
+	assert(rec_num < MAX_REC_COUNT);
+	(void)memset((char *)host_shared_data[rec_num]->host_param_val, 0,
+	MAX_DATA_SIZE * sizeof(host_shared_data[0U]->host_param_val));
 }
 
 /*
  * Get command sent from Host to realm
  */
-uint8_t realm_shared_data_get_realm_cmd(void)
+uint8_t realm_shared_data_get_realm_cmd(unsigned int rec_num)
 {
-	return guest_shared_data->realm_cmd;
+	assert(rec_num < MAX_REC_COUNT);
+	return guest_shared_data[rec_num]->realm_cmd;
 }
 
 /*
  * Set command to be send from Host to realm
  */
-void realm_shared_data_set_realm_cmd(uint8_t cmd)
+void realm_shared_data_set_realm_cmd(uint8_t cmd, unsigned int rec_num)
 {
-	host_shared_data->realm_cmd = cmd;
+	assert(rec_num < MAX_REC_COUNT);
+	host_shared_data[rec_num]->realm_cmd = cmd;
 }
