822bfb642f8ef719afa96de85a0184181502ef18
Shruti Gupta <shruti.gupta@arm.com>
2023-08-22 12:51:11 +0100

    test(rmm): add testcase for multiple rec on multiple cpu
    Test creates and enter 8 recs on different CPUs.
    Exercises CPU_ON, CPU_OFF and PSCI_AFFINITY_INFO
    from realm.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: Iecc58ea79bfde28f307d1df99680d707e57a1d80

--- realm/realm_multiple_rec.c
+++ realm/realm_multiple_rec.c
@@ -23,7 +23,6 @@
 static volatile int is_secondary_cpu_booted;
 static spinlock_t counter_lock;
 
-
 static void secondary_cpu(u_register_t cxt_id)
 {
 	realm_printf("Realm: running on CPU = 0x%lx cxt_id= 0x%lx\n",
@@ -55,5 +54,54 @@ bool test_realm_multiple_rec_psci_denied_cmd(void)
 	}
 
 	ret = realm_cpu_on(2U, (uintptr_t)secondary_cpu, 0x102);
+	return true;
+}
+
+bool test_realm_multiple_rec_multiple_cpu_cmd(void)
+{
+	unsigned int i = 1U, rec_count;
+	u_register_t ret;
+
+	realm_printf("Realm: running on CPU = 0x%lx\n", read_mpidr_el1() & MPID_MASK);
+	init_spinlock(&counter_lock);
+
+	rec_count = realm_shared_data_get_host_val(0U, HOST_ARG1_INDEX);
+
+	/* Check CPU_ON is supported */
+	ret = realm_psci_features(SMC_PSCI_CPU_ON);
+	if (ret != PSCI_E_SUCCESS) {
+		realm_printf("SMC_PSCI_CPU_ON not supported\n");
+	}
+
+	for (unsigned int j = 1U; j < rec_count; j++) {
+		ret = realm_cpu_on(j, (uintptr_t)secondary_cpu, CXT_ID_MAGIC + j);
+		if (ret != PSCI_E_SUCCESS) {
+			realm_printf("SMC_PSCI_CPU_ON failed %d.\n", j);
+			return false;
+		}
+	}
+
+	/* wait for all CPUs to come up */
+	if (is_secondary_cpu_booted != rec_count-1) {
+		waitms(500);
+	}
+
+	/* wait for all CPUs to turn off */
+	while (i < rec_count) {
+		ret = realm_psci_affinity_info(i, MPIDR_AFFLVL0);
+		if (ret != PSCI_STATE_OFF) {
+			/* wait and query again */
+			realm_printf(" CPU %d is not off\n", i);
+			waitms(200);
+			continue;
+		}
+		i++;
+	}
+	/*Try to on CPU 2 again*/
+	ret = realm_cpu_on(2U, (uintptr_t)secondary_cpu, CXT_ID_MAGIC + 2U);
+	if (ret != PSCI_E_SUCCESS && ret != PSCI_E_ALREADY_ON) {
+		realm_printf("SMC_PSCI_CPU_ON failed %d.\n", i);
+		return false;
+	}
 	return true;
 }
