4df303154bcc401f5430f7b2d85d7d3f1b7d80b2
Shruti Gupta <shruti.gupta@arm.com>
2023-08-22 12:51:11 +0100

    test(rmm): add testcase for multiple rec
    Test creates and enter 8 recs on different CPUs.
    Exercises CPU_ON, CPU_OFF and PSCI_AFFINITY_INFO
    from realm.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: Iecc58ea79bfde28f307d1df99680d707e57a1d80

--- realm/realm_payload_main.c
+++ realm/realm_payload_main.c
@@ -13,12 +13,17 @@
 #include <host_realm_helper.h>
 #include <host_shared_data.h>
 #include <pauth.h>
+#include <psci.h>
 #include "realm_def.h"
 #include <realm_rsi.h>
 #include <realm_tests.h>
 #include <tftf_lib.h>
 
 static fpu_reg_state_t fpu_temp_rl;
+static volatile int is_secondary_cpu_booted;
+static spinlock_t counter_lock;
+extern void realm_entrypoint(void);
+
 /*
  * This function reads sleep time in ms from shared buffer and spins PE
  * in a loop for that time period.
@@ -53,6 +58,66 @@ static void realm_get_rsi_version(void)
 	RSI_ABI_VERSION_GET_MINOR(RSI_ABI_VERSION_VAL));
 }
 
+static void secondary_cpu(void)
+{
+	smc_args args = { SMC_PSCI_CPU_OFF };
+
+	spin_lock(&counter_lock);
+	is_secondary_cpu_booted++;
+	spin_unlock(&counter_lock);
+	tftf_smc(&args);
+	return;
+}
+
+bool test_realm_multiple_rec_cmd(void)
+{
+	smc_args args;
+	smc_ret_values ret_vals;
+
+	realm_printf("Realm running on CPU = 0x%lx\n", read_mpidr_el1() & MPID_MASK);
+	if ((read_mpidr_el1() & MPID_MASK) != 0U) {
+		secondary_cpu();
+		/* Does not return */
+		return false;
+	}
+	init_spinlock(&counter_lock);
+	for (unsigned int i = 1U; i < MAX_REC_COUNT; i++) {
+		args.fid = SMC_PSCI_CPU_ON;
+		args.arg1 = i;
+		args.arg2 = (uintptr_t)realm_entrypoint;
+		args.arg3 = 0x100 + i;
+
+		ret_vals = tftf_smc(&args);
+		if (ret_vals.ret0 != PSCI_E_SUCCESS && ret_vals.ret0 != PSCI_E_ALREADY_ON) {
+			realm_printf("SMC_PSCI_CPU_ON failed %d. \n", i);
+			return false;
+		}
+	}
+
+	/* wait for all CPUs to come up. */
+	waitms(200);
+
+	if (is_secondary_cpu_booted != MAX_REC_COUNT-1) {
+		return false;
+	}
+
+	/* wait for all CPUs to turn off */
+	for (unsigned int i = 1U; i < MAX_REC_COUNT; i++) {
+		args.fid = SMC_PSCI_AFFINITY_INFO;
+		args.arg1 = i;
+		args.arg2 = 0U;
+		ret_vals = tftf_smc(&args);
+		if (ret_vals.ret0 != PSCI_STATE_OFF) {
+			/* wait and query again */
+			realm_printf(" Wait CPU %d not off\n", i);
+			waitms(50);
+			i--;
+			continue;
+		}
+	}
+	return true;
+}
+
 /*
  * This is the entry function for Realm payload, it first requests the shared buffer
  * IPA address from Host using HOST_CALL/RSI, it reads the command to be executed,
@@ -73,6 +138,9 @@ void realm_payload_main(void)
 		case REALM_SLEEP_CMD:
 			realm_sleep_cmd();
 			test_succeed = true;
+			break;
+		case REALM_MULTIPLE_REC_CMD:
+			test_succeed = test_realm_multiple_rec_cmd();
 			break;
 		case REALM_PAUTH_SET_CMD:
 			test_succeed = test_realm_pauth_set_cmd();
