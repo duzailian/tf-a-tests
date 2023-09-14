e166894030e4c625f8043d46d36f8cb3cf92e08a
Karl Meakin <karl.meakin@arm.com>
2023-09-14 16:25:15 +0100

    test(memory share): hypervisor retrieve request with multiple receivers
    Change-Id: I51d92d74f64bfebfa3d49ff6d5066050087b44ef
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>

--- tftf/tests/runtime_services/secure_service/spm_common.c
+++ tftf/tests/runtime_services/secure_service/spm_common.c
@@ -208,31 +208,38 @@ unsigned int get_ffa_feature_test_target(
 
 bool memory_retrieve(struct mailbox_buffers *mb,
 		     struct ffa_memory_region **retrieved, uint64_t handle,
-		     ffa_id_t sender, ffa_id_t receiver,
-		     ffa_memory_region_flags_t flags,
-		     uint32_t mem_func)
+		     ffa_id_t sender, ffa_id_t receiver_id,
+		     ffa_memory_region_flags_t flags, uint32_t mem_func)
 {
 	struct ffa_value ret;
 	uint32_t fragment_size;
 	uint32_t total_size;
 	uint32_t descriptor_size;
-	const enum ffa_instruction_access inst_access =
-				(mem_func == FFA_MEM_SHARE_SMC32)
-					? FFA_INSTRUCTION_ACCESS_NOT_SPECIFIED
-					: FFA_INSTRUCTION_ACCESS_NX;
+	ffa_memory_access_permissions_t permissions = {
+		.instruction_access =
+			(mem_func == FFA_MEM_SHARE_SMC32)
+				? FFA_INSTRUCTION_ACCESS_NOT_SPECIFIED
+				: FFA_INSTRUCTION_ACCESS_NX,
+		.data_access = FFA_DATA_ACCESS_RW,
+	};
 
 	if (retrieved == NULL || mb == NULL) {
 		ERROR("Invalid parameters!\n");
 		return false;
 	}
 
+	struct ffa_memory_access receiver = {
+		.receiver_permissions = {.permissions = permissions,
+					 .receiver = receiver_id,
+					 .flags = 0},
+		.composite_memory_region_offset = 0,
+		.reserved_0 = 0,
+	};
+
 	descriptor_size = ffa_memory_retrieve_request_init(
-	    mb->send, handle, sender, receiver, 0, flags,
-	    FFA_DATA_ACCESS_RW,
-	    inst_access,
-	    FFA_MEMORY_NORMAL_MEM,
-	    FFA_MEMORY_CACHE_WRITE_BACK,
-	    FFA_MEMORY_INNER_SHAREABLE);
+		mb->send, handle, sender, &receiver, 1, 0, flags,
+		FFA_MEMORY_NORMAL_MEM, FFA_MEMORY_CACHE_WRITE_BACK,
+		FFA_MEMORY_INNER_SHAREABLE);
 
 	ret = ffa_mem_retrieve_req(descriptor_size, descriptor_size);
 
@@ -279,7 +286,8 @@ bool memory_retrieve(struct mailbox_buffers *mb,
 
 bool hypervisor_retrieve(struct mailbox_buffers *mb,
 			 struct ffa_memory_region **retrieved, uint64_t handle,
-			 ffa_id_t receiver)
+			 struct ffa_memory_access *receivers,
+			 uint32_t receiver_count)
 {
 	struct ffa_value ret;
 	uint32_t fragment_size;
@@ -290,8 +298,11 @@ bool hypervisor_retrieve(struct mailbox_buffers *mb,
 		return false;
 	}
 
-	ffa_memory_retrieve_request_init(mb->send, handle, 0, receiver, 0, 0, 0,
-					 0, 0, 0, 0);
+	// all fields must be zero to indicate a hypervisor retrieve
+	ffa_memory_retrieve_request_init(
+		mb->send, handle, 0, receivers, receiver_count, 0, 0,
+		FFA_MEMORY_NOT_SPECIFIED_MEM, FFA_MEMORY_CACHE_RESERVED,
+		FFA_MEMORY_SHARE_NON_SHAREABLE);
 	((struct ffa_memory_region *)mb->send)->receiver_count = 0;
 
 	ret = ffa_mem_retrieve_req(sizeof(struct ffa_memory_region),
@@ -408,7 +419,8 @@ ffa_memory_handle_t memory_send(
  */
 ffa_memory_handle_t memory_init_and_send(
 	struct ffa_memory_region *memory_region, size_t memory_region_max_size,
-	ffa_id_t sender, ffa_id_t receiver,
+	ffa_id_t sender, struct ffa_memory_access receivers[],
+	uint32_t receiver_count,
 	const struct ffa_memory_region_constituent *constituents,
 	uint32_t constituents_count, uint32_t mem_func, struct ffa_value *ret)
 {
@@ -416,23 +428,14 @@ ffa_memory_handle_t memory_init_and_send(
 	uint32_t total_length;
 	uint32_t fragment_length;
 
-	enum ffa_data_access data_access = (mem_func == FFA_MEM_DONATE_SMC32) ?
-						FFA_DATA_ACCESS_NOT_SPECIFIED :
-						FFA_DATA_ACCESS_RW;
+	enum ffa_memory_type type =
+		(receiver_count == 1 && mem_func != FFA_MEM_SHARE_SMC32)
+			? FFA_MEMORY_NOT_SPECIFIED_MEM
+			: FFA_MEMORY_NORMAL_MEM;
 
-	/*
-	 * Initialize memory region structure for the respective memory send
-	 * operation. Note that memory type shall only be specified for memory
-	 * share, for memory lend and memory donate these shall remain
-	 * unspecified.
-	 */
 	remaining_constituent_count = ffa_memory_region_init(
-		memory_region, memory_region_max_size, sender, receiver, constituents,
-		constituents_count, 0, 0, data_access,
-		FFA_INSTRUCTION_ACCESS_NOT_SPECIFIED,
-		mem_func == FFA_MEM_SHARE_SMC32
-			? FFA_MEMORY_NORMAL_MEM
-			: FFA_MEMORY_NOT_SPECIFIED_MEM,
+		memory_region, memory_region_max_size, sender, receivers,
+		receiver_count, constituents, constituents_count, 0, 0, type,
 		FFA_MEMORY_CACHE_WRITE_BACK, FFA_MEMORY_INNER_SHAREABLE,
 		&total_length, &fragment_length);
 
@@ -446,7 +449,7 @@ ffa_memory_handle_t memory_init_and_send(
 	}
 
 	return memory_send(memory_region, mem_func, fragment_length,
-			       total_length, ret);
+			   total_length, ret);
 }
 
 static bool ffa_uuid_equal(const struct ffa_uuid uuid1,
@@ -671,4 +674,22 @@ bool enable_trusted_wdog_interrupt(ffa_id_t source, ffa_id_t dest)
 bool disable_trusted_wdog_interrupt(ffa_id_t source, ffa_id_t dest)
 {
 	return configure_trusted_wdog_interrupt(source, dest, false);
+}
+
+struct ffa_memory_access make_reciever(uint32_t mem_func, ffa_id_t receiver)
+{
+	ffa_memory_access_permissions_t permissions = {
+		.instruction_access = FFA_INSTRUCTION_ACCESS_NOT_SPECIFIED,
+		.data_access = (mem_func == FFA_MEM_DONATE_SMC32)
+				       ? FFA_DATA_ACCESS_NOT_SPECIFIED
+				       : FFA_DATA_ACCESS_RW,
+	};
+
+	return (struct ffa_memory_access){
+		.reserved_0 = 0,
+		.composite_memory_region_offset = 0,
+		.receiver_permissions = {.receiver = receiver,
+					 .permissions = permissions,
+					 .flags = 0},
+	};
 }
