7efb4c10eff892a1c0c4f10d0214ac0d4d3cc3c3
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-09-19 16:07:09 +0100

    feat(rmi): add support for FEAT_LPA2 to the Realm Extension tests
    This patch adds support to the current Realm Extension tests to
    enable and use 52 bit address size with 4KB granularity when
    FEAT_LPA2 is present.
    
    In addition, this patch also adds four new tests for RMM:
    
      * Test realm creation with no FEAT_LPA2 and -1 RTT starting level
      * Test realm creation with no FEAT_LPA2 and S2SZ > 48 bits
      * Test realm creation with FEAT_LPA2 enabled but not supported
      * Test realm creation wiht FEAT_LPA2 disabled but supported
    
    In order to implement the above tests, this patch introduces changes
    to support passing the starting RTT level, S2SZ and FEAT_LPA2 enable
    flag during realm creation.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I0a930735a44772e5e76d6608c969759f27129917

---
+++ tftf/tests/runtime_services/realm_payload/host_realm_lpa2_tests.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2023, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <debug.h>
+#include <test_helpers.h>
+
+#include <host_realm_helper.h>
+#include <host_realm_mem_layout.h>
+
+static struct realm realm;
+
+/*
+ * @Test_Aim@ Test realm creation with no LPA2 and -1 RTT starting level
+ */
+test_result_t host_test_realm_no_lpa2_invalid_sl(void)
+{
+	u_register_t rec_flag[1] = {RMI_RUNNABLE};
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	/*
+	 * Set RMI_FEATURE_REGISTER_0_S2SZ to RMM_OVERWRITE_S2SL
+	 * to use it as flag to overwrite the stage 2 starting level.
+	 */
+	if (!host_create_activate_realm_payload(&realm, (u_register_t)REALM_IMAGE_BASE,
+			(u_register_t)PAGE_POOL_BASE,
+			(u_register_t)PAGE_POOL_MAX_SIZE,
+			0UL, RTT_MIN_LEVEL_LPA2, rec_flag, 1U)) {
+		return TEST_RESULT_SUCCESS;
+	}
+
+	(void)host_destroy_realm(&realm);
+	return TEST_RESULT_FAIL;
+}
+
+/*
+ * @Test_Aim@ Test realm creation with no LPA2 and S2SZ > 48 bits
+ */
+test_result_t host_test_realm_no_lpa2_invalid_s2sz(void)
+{
+	u_register_t rec_flag[1] = {RMI_RUNNABLE};
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	if (!host_create_activate_realm_payload(&realm, (u_register_t)REALM_IMAGE_BASE,
+			(u_register_t)PAGE_POOL_BASE,
+			(u_register_t)PAGE_POOL_MAX_SIZE,
+			INPLACE(RMI_FEATURE_REGISTER_0_S2SZ, 50UL),
+			RTT_MIN_LEVEL, rec_flag, 1U)) {
+		return TEST_RESULT_SUCCESS;
+	}
+
+	(void)host_destroy_realm(&realm);
+	return TEST_RESULT_FAIL;
+}
+
+/*
+ * @Test_Aim@ Test realm payload creation and destruction with
+ * FEAT_LPA2 disabled but supported.
+ */
+test_result_t host_test_realm_lpa2_supported_and_disabled(void)
+{
+	u_register_t rec_flag[1] = {RMI_RUNNABLE};
+	struct realm realm;
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	if (is_feat_52b_on_4k_2_supported() == false) {
+		return TEST_RESULT_SKIPPED;
+	}
+
+	if (!host_create_activate_realm_payload(&realm, (u_register_t)REALM_IMAGE_BASE,
+			(u_register_t)PAGE_POOL_BASE,
+			(u_register_t)PAGE_POOL_MAX_SIZE,
+			INPLACE(RMI_FEATURE_REGISTER_0_S2SZ, 48UL),
+			RTT_MIN_LEVEL, rec_flag, 1U)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!host_destroy_realm(&realm)) {
+		ERROR("%s(): failed to destroy realm\n", __func__);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test realm payload creation with  FEAT_LPA2 not supported
+ * but enabled.
+ */
+test_result_t host_test_realm_lpa2_enabled_and_unsupported(void)
+{
+	u_register_t rec_flag[1] = {RMI_RUNNABLE};
+	struct realm realm;
+	u_register_t feature_flag = INPLACE(RMI_FEATURE_REGISTER_0_S2SZ, 48UL);
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	if (is_feat_52b_on_4k_2_supported() == true) {
+		return TEST_RESULT_SKIPPED;
+	} else {
+		feature_flag |= RMI_FEATURE_REGISTER_0_LPA2;
+	}
+
+	if (!host_create_activate_realm_payload(&realm, (u_register_t)REALM_IMAGE_BASE,
+			(u_register_t)PAGE_POOL_BASE,
+			(u_register_t)PAGE_POOL_MAX_SIZE,
+			feature_flag, RTT_MIN_LEVEL, rec_flag, 1U)) {
+		return TEST_RESULT_SUCCESS;
+	}
+
+	(void)host_destroy_realm(&realm);
+	return TEST_RESULT_FAIL;
+}
+
