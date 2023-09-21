d00e206cf4d705125b45c5770371d71bda5a87c9
Shruti Gupta <shruti.gupta@arm.com>
2023-09-21 11:12:44 +0100

    feat(rmm-eac4): update RSI_VERSION, RMI_VERSION
    This patch updates API of RSI_VERSION and RMI_VERSION as per RMM
    Specification 1.0-eac4.
    RSI_ABI_VERSION_MAJOR is updated from 13 to 14.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: If4eb14d93f657388e2fe64ceefee002403cc4ae8

--- realm/realm_rsi.c
+++ realm/realm_rsi.c
@@ -14,12 +14,16 @@
 static struct rsi_host_call host_cal __aligned(sizeof(struct rsi_host_call));
 
 /* This function return RSI_ABI_VERSION */
-u_register_t rsi_get_version(void)
+u_register_t rsi_get_version(u_register_t req_ver, u_register_t *impl_ver)
 {
 	smc_ret_values res = {};
 
 	res = tftf_smc(&(smc_args)
-		{RSI_ABI_VERSION, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL});
+		{RSI_ABI_VERSION, req_ver, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL});
+
+	if (res.ret0 == RSI_SUCCESS) {
+		*impl_ver = res.ret1;
+	}
 
 	return res.ret0;
 }
