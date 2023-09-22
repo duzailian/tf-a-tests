831e1c0511ea7158189b512c7854b325d5e07b78
Harrison Mutai <harrison.mutai@arm.com>
2023-09-22 17:17:35 +0100

    feat(handoff): add basic firmware handoff tests
    Add tests to sanity check information shared between BL31 and NS world
    using the firmware handoff framework.
    
    Change-Id: I9d00292db7732157d0815e6159438c0db08551ad
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

---
+++ include/lib/handoff/transfer_list.h
@@ -0,0 +1,41 @@
+#ifndef __TRANSFER_LIST_H
+#define __TRANSFER_LIST_H
+
+
+struct transfer_list_header {
+	uint32_t	signature;
+	uint8_t		checksum;
+	uint8_t		version;
+	uint8_t		hdr_size;
+	uint8_t		alignment;	// max alignment of TE data
+	uint32_t	size;		// TL header + all TEs
+	uint32_t	max_size;
+	/*
+	 * Commented out element used to visualize dynamic part of the
+	 * data structure.
+	 *
+	 * Note that struct transfer_list_entry also is dynamic in size
+	 * so the elements can't be indexed directly but instead must be
+	 * traversed in order
+	 *
+	 * struct transfer_list_entry entries[];
+	 */
+};
+
+
+struct transfer_list_entry {
+	uint16_t	tag_id;
+	uint8_t		reserved0;	// place holder
+	uint8_t		hdr_size;
+	uint32_t	data_size;
+	/*
+	 * Commented out element used to visualize dynamic part of the
+	 * data structure.
+	 *
+	 * Note that padding is added at the end of @data to make to reach
+	 * a 8-byte boundary.
+	 *
+	 * uint8_t	data[ROUNDUP(data_size, 8)];
+	 */
+};
+#endif /*__TRANSFER_LIST_H*/
