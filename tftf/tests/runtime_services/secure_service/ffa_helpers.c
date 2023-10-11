2ad47906df470d5536c1de8d4192048cf27789ff
Karl Meakin <karl.meakin@arm.com>
2023-10-11 18:48:01 +0100

    refactor(ffa_helpers): Make `ffa_memory_access_permisisons_t` and `ffa_memory_attributes_t` structs with bit-fields.
    Instead of doing the bit-manipulation manually, these types can be declared as
    structs where the fields are given explicit bitwidths. The compiler will
    generate the appropriate bit-manipulation automatically.
    
    Change-Id: I0d4754466b1e70d1b36cb039bfe9e7701ebbd60f
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>

--- tftf/tests/runtime_services/secure_service/ffa_helpers.c
+++ tftf/tests/runtime_services/secure_service/ffa_helpers.c
@@ -279,17 +279,16 @@ uint32_t ffa_memory_retrieve_request_init(
 	enum ffa_memory_type type, enum ffa_memory_cacheability cacheability,
 	enum ffa_memory_shareability shareability)
 {
-	ffa_memory_access_permissions_t permissions = 0;
-	ffa_memory_attributes_t attributes = 0;
+	ffa_memory_access_permissions_t permissions = {
+		.data_access = data_access,
+		.instruction_access = instruction_access,
+	};
 
-	/* Set memory region's permissions. */
-	ffa_set_data_access_attr(&permissions, data_access);
-	ffa_set_instruction_access_attr(&permissions, instruction_access);
-
-	/* Set memory region's page attributes. */
-	ffa_set_memory_type_attr(&attributes, type);
-	ffa_set_memory_cacheability_attr(&attributes, cacheability);
-	ffa_set_memory_shareability_attr(&attributes, shareability);
+	ffa_memory_attributes_t attributes = {
+		.type = type,
+		.cacheability = cacheability,
+		.shareability = shareability,
+	};
 
 	ffa_memory_region_init_header(memory_region, sender, attributes, flags,
 					handle, tag, receiver, permissions);
