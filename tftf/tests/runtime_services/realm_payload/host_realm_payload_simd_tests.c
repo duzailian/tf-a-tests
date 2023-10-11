175c239b6b7b6e712eb903ab15a20853c7617b9f
Olivier Deprez <olivier.deprez@arm.com>
2023-10-11 14:09:46 +0200

    test(spm): add host_realm_swd_check_simd
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I210acdff9e584cb392a2f1919dce04971fd2fc7a

--- tftf/tests/runtime_services/realm_payload/host_realm_payload_simd_tests.c
+++ tftf/tests/runtime_services/realm_payload/host_realm_payload_simd_tests.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Arm Limited. All rights reserved.
+ * Copyright (c) 2023-2024, Arm Limited. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -18,53 +18,14 @@
 #include <host_realm_simd.h>
 #include <host_shared_data.h>
 
-#define NS_SVE_OP_ARRAYSIZE		1024U
-#define SVE_TEST_ITERATIONS		50U
-
-/* Min test iteration count for 'host_and_realm_check_simd' test */
-#define TEST_ITERATIONS_MIN	(16U)
-
-/* Number of FPU configs: none */
-#define NUM_FPU_CONFIGS		(0U)
-
-/* Number of SVE configs: SVE_VL, SVE hint */
-#define NUM_SVE_CONFIGS		(2U)
-
-/* Number of SME configs: SVE_SVL, FEAT_FA64, Streaming mode */
-#define NUM_SME_CONFIGS		(3U)
-
-#define NS_NORMAL_SVE			0x1U
-#define NS_STREAMING_SVE		0x2U
-
-typedef enum security_state {
-	NONSECURE_WORLD = 0U,
-	REALM_WORLD,
-	SECURITY_STATE_MAX
-} security_state_t;
-
-typedef enum {
-	TEST_FPU = 0U,
-	TEST_SVE,
-	TEST_SME,
-} simd_test_t;
+#include "host_realm_simd_common.h"
 
 static int ns_sve_op_1[NS_SVE_OP_ARRAYSIZE];
 static int ns_sve_op_2[NS_SVE_OP_ARRAYSIZE];
 
+/* TODO: also defined in host_realm_simd_common.c */
 static sve_z_regs_t ns_sve_z_regs_write;
 static sve_z_regs_t ns_sve_z_regs_read;
-
-static sve_p_regs_t ns_sve_p_regs_write;
-static sve_p_regs_t ns_sve_p_regs_read;
-
-static sve_ffr_regs_t ns_sve_ffr_regs_write;
-static sve_ffr_regs_t ns_sve_ffr_regs_read;
-
-static fpu_q_reg_t ns_fpu_q_regs_write[FPU_Q_COUNT];
-static fpu_q_reg_t ns_fpu_q_regs_read[FPU_Q_COUNT];
-
-static fpu_cs_regs_t ns_fpu_cs_regs_write;
-static fpu_cs_regs_t ns_fpu_cs_regs_read;
 
 static struct realm realm;
 
@@ -85,36 +46,6 @@ static struct realm realm;
 			return TEST_RESULT_SKIPPED;				\
 		}								\
 	} while (false)
-
-static test_result_t host_create_sve_realm_payload(bool sve_en, uint8_t sve_vq)
-{
-	u_register_t feature_flag;
-	u_register_t rec_flag[1] = {RMI_RUNNABLE};
-
-	if (sve_en) {
-		feature_flag = RMI_FEATURE_REGISTER_0_SVE_EN |
-				INPLACE(FEATURE_SVE_VL, sve_vq);
-	} else {
-		feature_flag = 0UL;
-	}
-
-	/* Initialise Realm payload */
-	if (!host_create_activate_realm_payload(&realm,
-				       (u_register_t)REALM_IMAGE_BASE,
-				       (u_register_t)PAGE_POOL_BASE,
-				       (u_register_t)PAGE_POOL_MAX_SIZE,
-				       feature_flag, rec_flag, 1U)) {
-		return TEST_RESULT_FAIL;
-	}
-
-	/* Create shared memory between Host and Realm */
-	if (!host_create_shared_mem(&realm, NS_REALM_SHARED_MEM_BASE,
-				    NS_REALM_SHARED_MEM_SIZE)) {
-		return TEST_RESULT_FAIL;
-	}
-
-	return TEST_RESULT_SUCCESS;
-}
 
 /*
  * RMI should report SVE VL in RMI features and it must be the same value as the
@@ -165,7 +96,7 @@ test_result_t host_sve_realm_cmd_rdvl(void)
 
 	sve_vq = EXTRACT(RMI_FEATURE_REGISTER_0_SVE_VL, rmi_feat_reg0);
 
-	rc = host_create_sve_realm_payload(true, sve_vq);
+	rc = host_create_sve_realm_payload(&realm, true, sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		ERROR("Failed to create Realm with SVE\n");
 		return TEST_RESULT_FAIL;
@@ -216,7 +147,7 @@ test_result_t host_sve_realm_test_invalid_vl(void)
 	 * Pass a sve_vq that is greater than the value supported by RMM
 	 * and check whether creating Realm fails
 	 */
-	rc = host_create_sve_realm_payload(true, (sve_vq + 1));
+	rc = host_create_sve_realm_payload(&realm, true, (sve_vq + 1));
 	if (rc == TEST_RESULT_SUCCESS) {
 		ERROR("Error: Realm created with invalid SVE VL %u\n", (sve_vq + 1));
 		host_destroy_realm(&realm);
@@ -242,7 +173,7 @@ static test_result_t _host_sve_realm_check_id_registers(bool sve_en)
 		sve_vq = EXTRACT(RMI_FEATURE_REGISTER_0_SVE_VL, rmi_feat_reg0);
 	}
 
-	rc = host_create_sve_realm_payload(sve_en, sve_vq);
+	rc = host_create_sve_realm_payload(&realm, sve_en, sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -323,7 +254,7 @@ test_result_t host_sve_realm_cmd_probe_vl(void)
 
 	sve_vq = EXTRACT(RMI_FEATURE_REGISTER_0_SVE_VL, rmi_feat_reg0);
 
-	rc = host_create_sve_realm_payload(true, sve_vq);
+	rc = host_create_sve_realm_payload(&realm, true, sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -378,7 +309,7 @@ test_result_t host_sve_realm_check_config_register(void)
 
 	vq = EXTRACT(RMI_FEATURE_REGISTER_0_SVE_VL, rmi_feat_reg0);
 
-	rc = host_create_sve_realm_payload(true, vq);
+	rc = host_create_sve_realm_payload(&realm, true, vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -455,7 +386,7 @@ static test_result_t run_sve_vectors_operations(bool realm_sve_en,
 	unsigned int i;
 	int val;
 
-	rc = host_create_sve_realm_payload(realm_sve_en, realm_sve_vq);
+	rc = host_create_sve_realm_payload(&realm, realm_sve_en, realm_sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -617,7 +548,7 @@ test_result_t host_sve_realm_check_vectors_leaked(void)
 	sve_config_vq(SVE_VQ_ARCH_MIN);
 
 	/* 3. Create Realm with max VQ (higher than NS SVE VQ) */
-	rc = host_create_sve_realm_payload(true, sve_vq);
+	rc = host_create_sve_realm_payload(&realm, true, sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -668,7 +599,7 @@ test_result_t host_non_sve_realm_check_undef_abort(void)
 	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
 	SKIP_TEST_IF_SVE_NOT_SUPPORTED();
 
-	rc = host_create_sve_realm_payload(false, 0);
+	rc = host_create_sve_realm_payload(&realm, false, 0);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -687,347 +618,6 @@ test_result_t host_non_sve_realm_check_undef_abort(void)
 	}
 
 	return rc;
-}
-
-/* Generate random values and write it to SVE Z, P and FFR registers */
-static void ns_sve_write_rand(void)
-{
-	bool has_ffr = true;
-
-	if (is_feat_sme_supported() && sme_smstat_sm() &&
-	    !sme_feat_fa64_enabled()) {
-		has_ffr = false;
-	}
-
-	sve_z_regs_write_rand(&ns_sve_z_regs_write);
-	sve_p_regs_write_rand(&ns_sve_p_regs_write);
-	if (has_ffr) {
-		sve_ffr_regs_write_rand(&ns_sve_ffr_regs_write);
-	}
-}
-
-/* Read SVE Z, P and FFR registers and compare it with the last written values */
-static test_result_t ns_sve_read_and_compare(void)
-{
-	test_result_t rc = TEST_RESULT_SUCCESS;
-	uint64_t bitmap;
-	bool has_ffr = true;
-
-	if (is_feat_sme_supported() && sme_smstat_sm() &&
-	    !sme_feat_fa64_enabled()) {
-		has_ffr = false;
-	}
-
-	/* Clear old state */
-	memset((void *)&ns_sve_z_regs_read, 0, sizeof(ns_sve_z_regs_read));
-	memset((void *)&ns_sve_p_regs_read, 0, sizeof(ns_sve_p_regs_read));
-	memset((void *)&ns_sve_ffr_regs_read, 0, sizeof(ns_sve_ffr_regs_read));
-
-	/* Read Z, P, FFR registers to compare it with the last written values */
-	sve_z_regs_read(&ns_sve_z_regs_read);
-	sve_p_regs_read(&ns_sve_p_regs_read);
-	if (has_ffr) {
-		sve_ffr_regs_read(&ns_sve_ffr_regs_read);
-	}
-
-	bitmap = sve_z_regs_compare(&ns_sve_z_regs_write, &ns_sve_z_regs_read);
-	if (bitmap != 0UL) {
-		ERROR("SVE Z regs compare failed (bitmap: 0x%016llx)\n",
-		      bitmap);
-		rc = TEST_RESULT_FAIL;
-	}
-
-	bitmap = sve_p_regs_compare(&ns_sve_p_regs_write, &ns_sve_p_regs_read);
-	if (bitmap != 0UL) {
-		ERROR("SVE P regs compare failed (bitmap: 0x%016llx)\n",
-		      bitmap);
-		rc = TEST_RESULT_FAIL;
-	}
-
-	if (has_ffr) {
-		bitmap = sve_ffr_regs_compare(&ns_sve_ffr_regs_write,
-					      &ns_sve_ffr_regs_read);
-		if (bitmap != 0) {
-			ERROR("SVE FFR regs compare failed "
-			      "(bitmap: 0x%016llx)\n", bitmap);
-			rc = TEST_RESULT_FAIL;
-		}
-	}
-
-	return rc;
-}
-
-/*
- * Generate random values and write it to Streaming SVE Z, P and FFR registers.
- */
-static void ns_sme_write_rand(void)
-{
-	/*
-	 * TODO: more SME specific registers like ZA, ZT0 can be included later.
-	 */
-
-	/* Fill SVE registers in normal or streaming SVE mode */
-	ns_sve_write_rand();
-}
-
-/*
- * Read streaming SVE Z, P and FFR registers and compare it with the last
- * written values
- */
-static test_result_t ns_sme_read_and_compare(void)
-{
-	/*
-	 * TODO: more SME specific registers like ZA, ZT0 can be included later.
-	 */
-
-	/* Compares SVE registers in normal or streaming SVE mode */
-	return ns_sve_read_and_compare();
-}
-
-static char *simd_type_to_str(simd_test_t type)
-{
-	if (type == TEST_FPU) {
-		return "FPU";
-	} else if (type == TEST_SVE) {
-		return "SVE";
-	} else if (type == TEST_SME) {
-		return "SME";
-	} else {
-		return "UNKNOWN";
-	}
-}
-
-static void ns_simd_print_cmd_config(bool cmd, simd_test_t type)
-{
-	char __unused *tstr = simd_type_to_str(type);
-	char __unused *cstr = cmd ? "write rand" : "read and compare";
-
-	if (type == TEST_SME) {
-		if (sme_smstat_sm()) {
-			INFO("TFTF: NS [%s] %s. Config: smcr: 0x%llx, SM: on\n",
-			     tstr, cstr, (uint64_t)read_smcr_el2());
-		} else {
-			INFO("TFTF: NS [%s] %s. Config: smcr: 0x%llx, "
-			     "zcr: 0x%llx sve_hint: %d SM: off\n", tstr, cstr,
-			     (uint64_t)read_smcr_el2(),
-			     (uint64_t)sve_read_zcr_elx(),
-			     tftf_smc_get_sve_hint());
-		}
-	} else if (type == TEST_SVE) {
-		INFO("TFTF: NS [%s] %s. Config: zcr: 0x%llx, sve_hint: %d\n",
-		     tstr, cstr, (uint64_t)sve_read_zcr_elx(),
-		     tftf_smc_get_sve_hint());
-	} else {
-		INFO("TFTF: NS [%s] %s\n", tstr, cstr);
-	}
-}
-
-/*
- * Randomly select TEST_SME or TEST_FPU. For TEST_SME, randomly select below
- * configurations:
- * - enable/disable streaming mode
- *   For streaming mode:
- *   - enable or disable FA64
- *   - select random streaming vector length
- *   For normal SVE mode:
- *   - select random normal SVE vector length
- */
-static simd_test_t ns_sme_select_random_config(void)
-{
-	simd_test_t type;
-	static unsigned int counter;
-
-	/* Use a static counter to mostly select TEST_SME case. */
-	if ((counter % 8U) != 0) {
-		/* Use counter to toggle between Streaming mode on or off */
-		if (is_armv8_2_sve_present() && ((counter % 2U) != 0)) {
-			sme_smstop(SMSTOP_SM);
-			sve_config_vq(SVE_GET_RANDOM_VQ);
-
-			if ((counter % 3U) != 0) {
-				tftf_smc_set_sve_hint(true);
-			} else {
-				tftf_smc_set_sve_hint(false);
-			}
-		} else {
-			sme_smstart(SMSTART_SM);
-			sme_config_svq(SME_GET_RANDOM_SVQ);
-
-			if ((counter % 3U) != 0) {
-				sme_enable_fa64();
-			} else {
-				sme_disable_fa64();
-			}
-		}
-		type = TEST_SME;
-	} else {
-		type = TEST_FPU;
-	}
-	counter++;
-
-	return type;
-}
-
-/*
- * Randomly select TEST_SVE or TEST_FPU. For TEST_SVE, configure zcr_el2 with
- * random vector length and randomly enable or disable SMC SVE hint bit.
- */
-static simd_test_t ns_sve_select_random_config(void)
-{
-	simd_test_t type;
-	static unsigned int counter;
-
-	/* Use a static counter to mostly select TEST_SVE case. */
-	if ((counter % 4U) != 0) {
-		sve_config_vq(SVE_GET_RANDOM_VQ);
-
-		if ((counter % 2U) != 0) {
-			tftf_smc_set_sve_hint(true);
-		} else {
-			tftf_smc_set_sve_hint(false);
-		}
-
-		type = TEST_SVE;
-	} else {
-		type = TEST_FPU;
-	}
-	counter++;
-
-	return type;
-}
-
-/*
- * Configure NS world SIMD. Randomly choose to test SVE or FPU registers if
- * system supports SVE.
- *
- * Returns either TEST_FPU or TEST_SVE or TEST_SME
- */
-static simd_test_t ns_simd_select_random_config(void)
-{
-	simd_test_t type;
-
-	/* cleanup old config for SME */
-	if (is_feat_sme_supported()) {
-		sme_smstop(SMSTOP_SM);
-		sme_enable_fa64();
-	}
-
-	/* Cleanup old config for SVE */
-	if (is_armv8_2_sve_present()) {
-		tftf_smc_set_sve_hint(false);
-	}
-
-	if (is_armv8_2_sve_present() && is_feat_sme_supported()) {
-		if (rand() % 2) {
-			type = ns_sme_select_random_config();
-		} else {
-			type = ns_sve_select_random_config();
-		}
-	} else if (is_feat_sme_supported()) {
-		type = ns_sme_select_random_config();
-	} else if (is_armv8_2_sve_present()) {
-		type = ns_sve_select_random_config();
-	} else {
-		type = TEST_FPU;
-	}
-
-	return type;
-}
-
-/* Select random NS SIMD config and write random values to its registers */
-static simd_test_t ns_simd_write_rand(void)
-{
-	simd_test_t type;
-
-	type = ns_simd_select_random_config();
-
-	ns_simd_print_cmd_config(true, type);
-
-	if (type == TEST_SME) {
-		ns_sme_write_rand();
-	} else if (type == TEST_SVE) {
-		ns_sve_write_rand();
-	} else {
-		fpu_q_regs_write_rand(ns_fpu_q_regs_write);
-	}
-
-	/* fpcr, fpsr common to all configs */
-	fpu_cs_regs_write_rand(&ns_fpu_cs_regs_write);
-
-	return type;
-}
-
-/* Read and compare the NS SIMD registers with the last written values */
-static test_result_t ns_simd_read_and_compare(simd_test_t type)
-{
-	test_result_t rc = TEST_RESULT_SUCCESS;
-
-	ns_simd_print_cmd_config(false, type);
-
-	if (type == TEST_SME) {
-		rc = ns_sme_read_and_compare();
-	} else if (type == TEST_SVE) {
-		rc = ns_sve_read_and_compare();
-	} else {
-		fpu_q_regs_read(ns_fpu_q_regs_read);
-		if (fpu_q_regs_compare(ns_fpu_q_regs_write,
-				       ns_fpu_q_regs_read)) {
-			ERROR("FPU Q registers compare failed\n");
-			rc = TEST_RESULT_FAIL;
-		}
-	}
-
-	/* fpcr, fpsr common to all configs */
-	fpu_cs_regs_read(&ns_fpu_cs_regs_read);
-	if (fpu_cs_regs_compare(&ns_fpu_cs_regs_write, &ns_fpu_cs_regs_read)) {
-		ERROR("FPCR/FPSR registers compare failed\n");
-		rc = TEST_RESULT_FAIL;
-	}
-
-	return rc;
-}
-
-/* Select random Realm SIMD config and write random values to its registers */
-static simd_test_t rl_simd_write_rand(bool rl_sve_en)
-{
-	enum realm_cmd rl_fill_cmd;
-	simd_test_t type;
-	bool __unused rc;
-
-	/* Select random commands to test. SVE or FPU registers in Realm */
-	if (rl_sve_en && (rand() % 2)) {
-		type = TEST_SVE;
-	} else {
-		type = TEST_FPU;
-	}
-
-	INFO("TFTF: RL [%s] write random\n", simd_type_to_str(type));
-	if (type == TEST_SVE) {
-		rl_fill_cmd = REALM_SVE_FILL_REGS;
-	} else {
-		rl_fill_cmd = REALM_REQ_FPU_FILL_CMD;
-	}
-
-	rc = host_enter_realm_execute(&realm, rl_fill_cmd, RMI_EXIT_HOST_CALL, 0U);
-	assert(rc);
-
-	return type;
-}
-
-/* Read and compare the Realm SIMD registers with the last written values */
-static bool rl_simd_read_and_compare(simd_test_t type)
-{
-	enum realm_cmd rl_cmp_cmd;
-
-	INFO("TFTF: RL [%s] read and compare\n", simd_type_to_str(type));
-	if (type == TEST_SVE) {
-		rl_cmp_cmd = REALM_SVE_CMP_REGS;
-	} else {
-		rl_cmp_cmd = REALM_REQ_FPU_CMP_CMD;
-	}
-
-	return host_enter_realm_execute(&realm, rl_cmp_cmd, RMI_EXIT_HOST_CALL,
-					0U);
 }
 
 /*
@@ -1068,7 +658,7 @@ test_result_t host_and_realm_check_simd(void)
 
 	/* Create Realm with SVE enabled if RMI features supports it */
 	INFO("TFTF: create realm sve_en/sve_vq: %d/%d\n", sve_en, sve_vq);
-	rc = host_create_sve_realm_payload(sve_en, sve_vq);
+	rc = host_create_sve_realm_payload(&realm, sve_en, sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -1083,7 +673,7 @@ test_result_t host_and_realm_check_simd(void)
 	 * Randomly select and configure Realm simd context to test. Enter realm
 	 * and fill simd context with random values.
 	 */
-	rl_simd_type = rl_simd_write_rand(sve_en);
+	rl_simd_type = rl_simd_write_rand(&realm, sve_en);
 	sec_state = REALM_WORLD;
 
 	/*
@@ -1141,7 +731,7 @@ test_result_t host_and_realm_check_simd(void)
 			 * Enter Realm and read the simd context and compare it
 			 * with last written context.
 			 */
-			if (!rl_simd_read_and_compare(rl_simd_type)) {
+			if (!rl_simd_read_and_compare(&realm, rl_simd_type)) {
 				ERROR("%s failed %d\n", __func__, __LINE__);
 				rc = TEST_RESULT_FAIL;
 				goto rm_realm;
@@ -1152,7 +742,7 @@ test_result_t host_and_realm_check_simd(void)
 			 * test. Enter realm and fill simd context with random
 			 * values for the next compare.
 			 */
-			rl_simd_type = rl_simd_write_rand(sve_en);
+			rl_simd_type = rl_simd_write_rand(&realm, sve_en);
 			break;
 		default:
 			break;
@@ -1193,7 +783,7 @@ test_result_t host_realm_check_sme_id_registers(void)
 
 	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
 
-	rc = host_create_sve_realm_payload(false, 0);
+	rc = host_create_sve_realm_payload(&realm, false, 0);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -1236,7 +826,7 @@ test_result_t host_realm_check_sme_undef_abort(void)
 	SKIP_TEST_IF_SME_NOT_SUPPORTED();
 	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
 
-	rc = host_create_sve_realm_payload(false, 0);
+	rc = host_create_sve_realm_payload(&realm, false, 0);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
@@ -1285,7 +875,7 @@ test_result_t host_realm_check_sme_configs(void)
 		sve_vq = 0;
 	}
 
-	rc = host_create_sve_realm_payload(sve_en, sve_vq);
+	rc = host_create_sve_realm_payload(&realm, sve_en, sve_vq);
 	if (rc != TEST_RESULT_SUCCESS) {
 		return rc;
 	}
