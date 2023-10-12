80d8a663977f13ad3cdbc2795422eeb61498b728
Shruti Gupta <shruti.gupta@arm.com>
2023-10-12 21:45:12 +0100

    feat(rmm-eac4): update RSI_VERSION, RMI_VERSION
    This patch updates API of RSI_VERSION and RMI_VERSION as per RMM
    Specification 1.0-eac4.
    RSI_ABI_VERSION_MAJOR is updated from 13 to 14.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: If4eb14d93f657388e2fe64ceefee002403cc4ae8

--- realm/include/realm_rsi.h
+++ realm/include/realm_rsi.h
@@ -26,7 +26,7 @@
  * The major version number of the RSI implementation.  Increase this whenever
  * the binary format or semantics of the SMC calls change.
  */
-#define RSI_ABI_VERSION_MAJOR		12U
+#define RSI_ABI_VERSION_MAJOR		14U
 
 /*
  * The minor version number of the RSI implementation.  Increase this when
@@ -102,7 +102,7 @@ struct rsi_host_call {
 #define RSI_REALM_CONFIG	SMC_RSI_FID(6U)
 
 /* This function return RSI_ABI_VERSION */
-u_register_t rsi_get_version(void);
+u_register_t rsi_get_version(u_register_t req_ver);
 
 /* This function will call the Host to request IPA of the NS shared buffer */
 u_register_t rsi_get_ns_buffer(void);
