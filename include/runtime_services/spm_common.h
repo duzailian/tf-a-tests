a770a06ee6d58f383e2b5f3543583815047bfa33
Karl Meakin <karl.meakin@arm.com>
2023-10-13 20:03:16 +0100

    test(memory share): fragmented hypervisor retrieve request
    Change-Id: I6a47735415a8fed1d15322499f1d08cc3e1fd6a2
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>

--- include/runtime_services/spm_common.h
+++ include/runtime_services/spm_common.h
@@ -110,13 +110,11 @@ unsigned int get_ffa_feature_test_target(const struct ffa_features_test **test_t
 bool memory_retrieve(struct mailbox_buffers *mb,
 		     struct ffa_memory_region **retrieved, uint64_t handle,
 		     ffa_id_t sender, struct ffa_memory_access *receivers,
-		     uint32_t receiver_count, ffa_memory_region_flags_t flags,
-		     uint32_t mem_func);
+		     uint32_t receiver_count, ffa_memory_region_flags_t flags);
 
-bool hypervisor_retrieve(struct mailbox_buffers *mb,
-			 struct ffa_memory_region **retrieved, uint64_t handle,
-			 struct ffa_memory_access receivers[],
-			 uint32_t receiver_count);
+bool hypervisor_retrieve(struct mailbox_buffers *mb, uint64_t handle,
+			 struct ffa_memory_access *receivers,
+			 uint32_t receiver_count, void *out, uint32_t out_size);
 
 /**
  * Helper to conduct a memory relinquish. The caller is usually the receiver,
@@ -125,21 +123,23 @@ bool hypervisor_retrieve(struct mailbox_buffers *mb,
 bool memory_relinquish(struct ffa_mem_relinquish *m, uint64_t handle,
 		       ffa_id_t id);
 
-ffa_memory_handle_t memory_send(
-	struct ffa_memory_region *memory_region, uint32_t mem_func,
-	uint32_t fragment_length, uint32_t total_length, struct ffa_value *ret);
+struct ffa_value memory_send(struct mailbox_buffers *mb, uint32_t mem_func,
+			     struct ffa_memory_region_constituent *constituents,
+			     uint32_t constituent_count,
+			     uint32_t remaining_constituent_count,
+			     uint32_t fragment_length, uint32_t total_length);
 
-ffa_memory_handle_t memory_init_and_send(
-	struct ffa_memory_region *memory_region, size_t memory_region_max_size,
-	ffa_id_t sender, struct ffa_memory_access receivers[],
-	uint32_t receiver_count,
-	const struct ffa_memory_region_constituent *constituents,
-	uint32_t constituents_count, uint32_t mem_func, struct ffa_value *ret);
+struct ffa_value memory_init_and_send(struct mailbox_buffers *mb,
+		size_t memory_region_max_size,
+		ffa_id_t sender, struct ffa_memory_access receivers[],
+		uint32_t receiver_count,
+		struct ffa_memory_region_constituent *constituents,
+		uint32_t constituents_count, uint32_t mem_func);
 
 bool ffa_partition_info_helper(struct mailbox_buffers *mb,
-			const struct ffa_uuid uuid,
-			const struct ffa_partition_info *expected,
-			const uint16_t expected_size);
+			       const struct ffa_uuid uuid,
+			       const struct ffa_partition_info *expected,
+			       const uint16_t expected_size);
 bool enable_trusted_wdog_interrupt(ffa_id_t source, ffa_id_t dest);
 bool disable_trusted_wdog_interrupt(ffa_id_t source, ffa_id_t dest);
 
