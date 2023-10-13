5ece7f3e2ca4208c6db5cc08988dafee3c6fc597
Karl Meakin <karl.meakin@arm.com>
2023-10-13 20:03:16 +0100

    test(memory share): fragmented hypervisor retrieve request
    Checks that the hypervisor retrieve request from section 17.4.3 of FF-A
    v1.2 REL0 is supported, when the request does not fit in a single
    fragment.
    
    Change-Id: I6a47735415a8fed1d15322499f1d08cc3e1fd6a2
    Signed-off-by: Karl Meakin <karl.meakin@arm.com>

--- spm/cactus/cactus_tests/cactus_test_memory_sharing.c
+++ spm/cactus/cactus_tests/cactus_test_memory_sharing.c
@@ -4,16 +4,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <sp_def.h>
 #include "cactus_message_loop.h"
 #include "cactus_test_cmds.h"
+#include "sp_tests.h"
 #include <debug.h>
 #include <ffa_helpers.h>
-#include <sp_helpers.h>
-#include "sp_tests.h"
-#include <xlat_tables_defs.h>
 #include <lib/xlat_tables/xlat_tables_v2.h>
+#include <sp_def.h>
+#include <sp_helpers.h>
 #include <sync.h>
+#include <xlat_tables_defs.h>
 
 static volatile uint32_t data_abort_gpf_triggered;
 
@@ -89,7 +89,7 @@ CACTUS_CMD_HANDLER(mem_send_cmd, CACTUS_MEM_SEND_CMD)
 		0);
 
 	expect(memory_retrieve(mb, &m, handle, source, &receiver, 1,
-			       retrv_flags, mem_func),
+			       retrv_flags),
 	       true);
 
 	composite = ffa_memory_region_get_composite(m, 0);
@@ -206,7 +206,7 @@ CACTUS_CMD_HANDLER(req_mem_send_cmd, CACTUS_REQ_MEM_SEND_CMD)
 		source, receiver, mem_func, (uint64_t)share_page_addr);
 
 	const struct ffa_memory_region_constituent constituents[] = {
-		{share_page_addr, 1, 0}
+		{share_page_addr, 1, 0},
 	};
 
 	const uint32_t constituents_count = (sizeof(constituents) /
@@ -232,14 +232,10 @@ CACTUS_CMD_HANDLER(req_mem_send_cmd, CACTUS_REQ_MEM_SEND_CMD)
 					 CACTUS_ERROR_TEST);
 	}
 
-	handle = memory_init_and_send((struct ffa_memory_region *)mb->send,
-				      PAGE_SIZE, vm_id, &receiver, 1,
-				      constituents, constituents_count,
-				      mem_func, &ffa_ret);
+	handle = memory_init_and_send(mb->send, PAGE_SIZE, vm_id, &receiver, 1,
+				       constituents, constituents_count,
+				       mem_func, &ffa_ret);
 
-	/*
-	 * If returned an invalid handle, we should break the test.
-	 */
 	if (handle == FFA_MEMORY_HANDLE_INVALID) {
 		VERBOSE("Received an invalid FF-A memory Handle!\n");
 		return cactus_error_resp(vm_id, source,
