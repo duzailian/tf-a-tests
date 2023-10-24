0e9ff75c8cd8b6e5e357166c75f93349eb45ba7e
mardyk01 <mark.dykes@arm.com>
2023-10-24 16:23:23 -0500

    test(smc_fuzzing): Adding TSP testing
    Adding TSP based SMC instructions to fuzzing infrastructure.
    
    Change-Id: Ic47879dd6294be5afe44a37da9a630cf635f920a
    Signed-off-by: mardyk01 <mark.dykes@arm.com>

--- smc_fuzz/src/runtestfunction.c
+++ smc_fuzz/src/runtestfunction.c
@@ -72,4 +72,68 @@ void runtestfunction(char *funcstr)
 		}
 		printf("running %s\n", funcstr);
 	}
+	if (strcmp(funcstr, "tsp_add_op") == CMP_SUCCESS) {
+		uint64_t fn_identifier = TSP_FAST_FID(TSP_ADD);
+		uint64_t addarg1 = 4;
+		uint64_t addarg2 = 6;
+		smc_args tsp_svc_params = {fn_identifier, addarg1, addarg2};
+		smc_ret_values tsp_result;
+
+		tsp_result = tftf_smc(&tsp_svc_params);
+
+		if (tsp_result.ret0) {
+			tftf_testcase_printf("TSP operation 0x%x failed, error:0x%x\n",
+					(unsigned int) fn_identifier,
+					(unsigned int) tsp_result.ret0);
+		}
+		printf("running %s\n", funcstr);
+	}
+	if (strcmp(funcstr, "tsp_sub_op") == CMP_SUCCESS) {
+		uint64_t fn_identifier = TSP_FAST_FID(TSP_SUB);
+		uint64_t subarg1 = 4;
+		uint64_t subarg2 = 6;
+		smc_args tsp_svc_params = {fn_identifier, subarg1, subarg2};
+		smc_ret_values tsp_result;
+
+		tsp_result = tftf_smc(&tsp_svc_params);
+
+		if (tsp_result.ret0) {
+			tftf_testcase_printf("TSP operation 0x%x failed, error:0x%x\n",
+					(unsigned int) fn_identifier,
+					(unsigned int) tsp_result.ret0);
+		}
+		printf("running %s\n", funcstr);
+	}
+	if (strcmp(funcstr, "tsp_mul_op") == CMP_SUCCESS) {
+		uint64_t fn_identifier = TSP_FAST_FID(TSP_MUL);
+		uint64_t mularg1 = 4;
+		uint64_t mularg2 = 6;
+		smc_args tsp_svc_params = {fn_identifier, mularg1, mularg2};
+		smc_ret_values tsp_result;
+
+		tsp_result = tftf_smc(&tsp_svc_params);
+
+		if (tsp_result.ret0) {
+			tftf_testcase_printf("TSP operation 0x%x failed, error:0x%x\n",
+					(unsigned int) fn_identifier,
+					(unsigned int) tsp_result.ret0);
+		}
+		printf("running %s\n", funcstr);
+	}
+	if (strcmp(funcstr, "tsp_div_op") == CMP_SUCCESS) {
+		uint64_t fn_identifier = TSP_FAST_FID(TSP_DIV);
+		uint64_t divarg1 = 4;
+		uint64_t divarg2 = 6;
+		smc_args tsp_svc_params = {fn_identifier, divarg1, divarg2};
+		smc_ret_values tsp_result;
+
+		tsp_result = tftf_smc(&tsp_svc_params);
+
+		if (tsp_result.ret0) {
+			tftf_testcase_printf("TSP operation 0x%x failed, error:0x%x\n",
+					(unsigned int) fn_identifier,
+					(unsigned int) tsp_result.ret0);
+		}
+		printf("running %s\n", funcstr);
+	}
 }
