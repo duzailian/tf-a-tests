e1d955d58b47314f27eb9d6abe8ff141c83298c5
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-12-12 16:55:35 +0000

    feat(rme): add extra tests for FEAT_LPA2 on RMM
    This patch adds two new tests for RMM:
    
      * Test realm creation with no LPA2 and -1 RTT starting level
      * Test realm creation with no LPA2 and S2SZ > 48 bits
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I0a930735a44772e5e76d6608c969759f27129917

--- tftf/tests/runtime_services/realm_payload/host_realm_payload_simd_tests.c
+++ tftf/tests/runtime_services/realm_payload/host_realm_payload_simd_tests.c
@@ -88,14 +88,16 @@ static struct realm realm;
 
 static test_result_t host_create_sve_realm_payload(bool sve_en, uint8_t sve_vq)
 {
-	u_register_t feature_flag;
+	u_register_t feature_flag = 0UL;
 	u_register_t rec_flag[1] = {RMI_RUNNABLE};
 
+	if (is_feat_52b_on_4k_2_supported() == true) {
+		feature_flag = RMI_FEATURE_REGISTER_0_LPA2;
+	}
+
 	if (sve_en) {
-		feature_flag = RMI_FEATURE_REGISTER_0_SVE_EN |
+		feature_flag |= RMI_FEATURE_REGISTER_0_SVE_EN |
 				INPLACE(FEATURE_SVE_VL, sve_vq);
-	} else {
-		feature_flag = 0UL;
 	}
 
 	/* Initialise Realm payload */
