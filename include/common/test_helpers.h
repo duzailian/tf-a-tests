1bbec021ee6d903cf12426aea135c2c61879c9ff
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2024-01-04 15:22:52 -0600

    feat(mpam): add MPAM system registers access test
    Added a test which reads MPAM system registers to ensure that
    EL3 is giving permission to Non-secure EL2 to access these registers.
    
    Change-Id: Ie701a19c9682df6d6aeaec086954e275afff1843
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>

--- include/common/test_helpers.h
+++ include/common/test_helpers.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2023, Arm Limited. All rights reserved.
+ * Copyright (c) 2018-2024, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -223,6 +223,14 @@ typedef test_result_t (*test_function_arg_t)(void *arg);
 			return TEST_RESULT_SKIPPED;				\
 		}								\
 	} while (false)
+
+#define SKIP_TEST_IF_MPAM_NOT_SUPPORTED()                                     \
+      do {                                                                    \
+              if(!is_feat_mpam_supported()){                                  \
+                      tftf_testcase_printf("ARMv8.4-mpam not supported\n");   \
+                      return TEST_RESULT_SKIPPED;                             \
+              }                                                               \
+      } while (false)
 
 #ifdef __aarch64__
 #define SKIP_TEST_IF_PA_SIZE_LESS_THAN(n)					\
