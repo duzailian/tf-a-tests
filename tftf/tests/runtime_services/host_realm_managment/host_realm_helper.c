f73e371e97b7a914b7c3ecd0dc54e6177e7e7ed7
Shruti Gupta <shruti.gupta@arm.com>
2024-02-06 14:26:36 +0000

    test(realm): add ability to overwrite s2sz for creating realm
    Add ability to pass s2sz param while creating realms
    
    Change-Id: I33afd5903cf4de546aba1c17c988b32a6e1b05a5
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>

--- tftf/tests/runtime_services/host_realm_managment/host_realm_helper.c
+++ tftf/tests/runtime_services/host_realm_managment/host_realm_helper.c
@@ -149,6 +149,16 @@ bool host_create_realm_payload(struct realm *realm_ptr,
 		return false;
 	}
 
+	if (EXTRACT(RMI_FEATURE_REGISTER_0_S2SZ, feature_flag) >
+			EXTRACT(RMI_FEATURE_REGISTER_0_S2SZ, realm_ptr->rmm_feat_reg0)) {
+		ERROR("%s() failed\n", "Invalid s2sz");
+		return false;
+	} else if (EXTRACT(RMI_FEATURE_REGISTER_0_S2SZ, feature_flag) != 0U) {
+		realm_ptr->rmm_feat_reg0 &= ~MASK(RMI_FEATURE_REGISTER_0_S2SZ);
+		realm_ptr->rmm_feat_reg0 |= INPLACE(RMI_FEATURE_REGISTER_0_S2SZ,
+				EXTRACT(RMI_FEATURE_REGISTER_0_S2SZ, feature_flag));
+	}
+
 	/* Disable PMU if not required */
 	if ((feature_flag & RMI_FEATURE_REGISTER_0_PMU_EN) == 0UL) {
 		realm_ptr->rmm_feat_reg0 &= ~RMI_FEATURE_REGISTER_0_PMU_EN;
