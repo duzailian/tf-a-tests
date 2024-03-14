cf928841bd6ec37c570e03f0e90d6d0275257576
Chuyue Luo <Chuyue.Luo@arm.com>
2024-03-14 17:05:24 +0000

    test: add tests for self-hosted debug
    This patch adds tests for self-hosted debug.
    
    One test verifies that the number of breakpoints/watchpoints
    available in Realm is the same as what was requested by the Host.
    
    The other test verifies that entering Realm does not corrupt
    the values written to self-hosted debug registers by NS world, and
    vice versa (i.e. that the registers are saved and restored correctly).
    
    Signed-off-by: Chuyue Luo <Chuyue.Luo@arm.com>
    Change-Id: I0d743c29215dd69302bab617a95c9fa507faec54

--- realm/include/realm_tests.h
+++ realm/include/realm_tests.h
@@ -31,6 +31,9 @@ bool test_realm_sctlr2_ease(void);
 bool test_realm_attestation(void);
 bool test_realm_attestation_fault(void);
 bool test_realm_mpam_undef_abort(void);
+bool test_realm_debug_num_bps_wps(void);
+void test_realm_debug_fill_regs(void);
+bool test_realm_debug_cmp_regs(void);
 
 #endif /* REALM_TESTS_H */
 
