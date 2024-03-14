fac051006b9866fd16b85ac4f67ea02713e41e6e
Chuyue Luo <Chuyue.Luo@arm.com>
2024-03-14 17:05:24 +0000

    test: add tests for self-hosted debug
    This patch adds tests for self-hosted debug.
    
    One test verifies that the number of breakpoints/watchpoints
    available in Realm is the same as what was requested by the Host.
    
    The other test verifies that entering Realm does not corrupt
    the values written to self-hosted debug registers by NS world, and
    vice versa (i.e. that the registers are saved and restored correctly).
    
    Signed-off-by: Chuyue Luo <Chuyue.Luo@arm.com>
    Change-Id: I0d743c29215dd69302bab617a95c9fa507faec54

---
+++ tftf/tests/runtime_services/realm_payload/host_realm_payload_debug_tests.c
@@ -0,0 +1,381 @@
+/*
+ * Copyright (c) 2024-2025, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdlib.h>
+#include <limits.h>
+
+#include <arch_features.h>
+#include <arch_helpers.h>
+#include <debug.h>
+#include <lib/self_hosted_debug/self_hosted_debug_helpers.h>
+#include <test_helpers.h>
+
+#include <host_realm_helper.h>
+#include <host_realm_mem_layout.h>
+#include <host_shared_data.h>
+
+typedef struct {
+	long unsigned int (*read_dbgbcr_el1)(void);
+	long unsigned int (*read_dbgbvr_el1)(void);
+	long unsigned int (*read_dbgwcr_el1)(void);
+	long unsigned int (*read_dbgwvr_el1)(void);
+	void (*write_dbgbcr_el1)(long unsigned int);
+	void (*write_dbgbvr_el1)(long unsigned int);
+	void (*write_dbgwcr_el1)(long unsigned int);
+	void (*write_dbgwvr_el1)(long unsigned int);
+}ctx_fnptr_t;
+
+#define NS_DEBUG_FNPTRS(n)									\
+	{read_dbgbcr##n##_el1, read_dbgbvr##n##_el1, read_dbgwcr##n##_el1, read_dbgwvr##n##_el1, \
+	write_dbgbcr##n##_el1, write_dbgbvr##n##_el1, write_dbgwcr##n##_el1, write_dbgwvr##n##_el1}
+
+
+ctx_fnptr_t debug_regs[MAX_BPS] = {
+	NS_DEBUG_FNPTRS(0),
+	NS_DEBUG_FNPTRS(1),
+	NS_DEBUG_FNPTRS(2),
+	NS_DEBUG_FNPTRS(3),
+	NS_DEBUG_FNPTRS(4),
+	NS_DEBUG_FNPTRS(5),
+	NS_DEBUG_FNPTRS(6),
+	NS_DEBUG_FNPTRS(7),
+	NS_DEBUG_FNPTRS(8),
+	NS_DEBUG_FNPTRS(9),
+	NS_DEBUG_FNPTRS(10),
+	NS_DEBUG_FNPTRS(11),
+	NS_DEBUG_FNPTRS(12),
+	NS_DEBUG_FNPTRS(13),
+	NS_DEBUG_FNPTRS(14),
+	NS_DEBUG_FNPTRS(15)
+};
+
+/*
+ * Set DBGBCRn_EL1 fields HMC, PMC, SSC and SSCE such that breakpoint n will
+ * generate Breakpoint exceptions only in NS Security state.
+ */
+static u_register_t ns_set_dbgbcr_el1(u_register_t current_val)
+{
+	u_register_t new_val = set_dbgbcr_el1(current_val) |
+			       INPLACE(DBGBCR_EL1_SSC, 1UL) |
+			       INPLACE(DBGBCR_EL1_PMC, 1UL);
+
+	new_val &= ~(DBGBCR_EL1_HMC_BIT | DBGBCR_EL1_SSCE_BIT);
+
+	return new_val;
+}
+
+/*
+ * Verify that the value of DBGBCRn_EL1 is the same as what was written
+ * previously.
+ */
+static bool ns_check_dbgbcr_el1(u_register_t reg)
+{
+	u_register_t expected_dbgbcr_el1 = ns_set_dbgbcr_el1(0UL);
+
+	if (reg != expected_dbgbcr_el1) {
+		return false;
+	}
+
+	return true;
+}
+
+/*
+ * Set DBGWCRn_EL1 fields HMC, PAC, SSC and SSCE such that watchpoint n will
+ * generate Watchpoint exceptions only in NS Security state.
+ */
+static u_register_t ns_set_dbgwcr_el1(u_register_t current_val)
+{
+	u_register_t new_val = set_dbgwcr_el1(current_val) |
+			       INPLACE(DBGWCR_EL1_SSC, 1UL) |
+			       INPLACE(DBGWCR_EL1_PAC, 1UL);
+
+	new_val &= ~(DBGWCR_EL1_HMC_BIT | DBGWCR_EL1_SSCE_BIT);
+
+	return new_val;
+}
+
+/*
+ * Verify that the value of DBGWCRn_EL1 is the same as what was written
+ * previously.
+ */
+static bool ns_check_dbgwcr_el1(u_register_t reg)
+{
+	u_register_t expected_dbgwcr_el1 = ns_set_dbgwcr_el1(0UL);
+
+	if (reg != expected_dbgwcr_el1) {
+		return false;
+	}
+
+	return true;
+}
+
+/*
+ * Verify that Realm has the same number of BPs/WPs as what the NS Host has
+ * requested.
+ *
+ * Note that currently RMM assumes the NS Host will request the same number of
+ * BPs/WPs as is available in the hardware. This test can be updated to check
+ * different numbers of BPs/WPs once RMM is able to handle the NS Host
+ * requesting fewer BPs/WPs than the hardware supports.
+ */
+test_result_t host_test_realm_num_bps_wps(void)
+{
+	unsigned long num_bps, num_wps;
+	u_register_t feature_flag;
+	u_register_t rec_flag[1] = {RMI_RUNNABLE};
+	struct realm realm;
+	bool realm_rc;
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	num_bps = EXTRACT(ID_AA64DFR0_BRPs, read_id_aa64dfr0_el1());
+	num_wps = EXTRACT(ID_AA64DFR0_WRPs, read_id_aa64dfr0_el1());
+
+	/* If BPs is 0xF, read DFR1 to find no of BPs supported by HW */
+	if (num_bps == MAX_BPS - 1UL) {
+		num_bps = EXTRACT(ID_AA64DFR1_BRPs, read_id_aa64dfr1_el1());
+		if (num_bps == 0UL) {
+			num_bps = MAX_BPS - 1UL;
+		}
+	}
+
+	/* If WPs is 0xF, read DFR1 to find no of BPs supported by HW */
+	num_wps = EXTRACT(ID_AA64DFR0_WRPs, read_id_aa64dfr0_el1());
+
+	if (num_wps == MAX_WPS - 1UL) {
+		num_wps = EXTRACT(ID_AA64DFR1_WRPs, read_id_aa64dfr1_el1());
+		if(num_wps == 0UL) {
+			num_wps = MAX_WPS - 1UL;
+		}
+	}
+
+	feature_flag = INPLACE(RMI_FEATURE_REGISTER_0_NUM_BPS, num_bps) |
+		       INPLACE(RMI_FEATURE_REGISTER_0_NUM_WPS, num_wps);
+
+
+	if (!host_create_activate_realm_payload(&realm,
+					(u_register_t)REALM_IMAGE_BASE,
+					feature_flag, RTT_MIN_LEVEL, rec_flag, 1U, 0UL)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!host_create_shared_mem(&realm)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	host_shared_data_set_host_val(&realm, 0, 0, HOST_ARG1_INDEX, num_bps);
+	host_shared_data_set_host_val(&realm, 0, 0, HOST_ARG2_INDEX, num_wps);
+
+	realm_rc = host_enter_realm_execute(&realm,
+					    REALM_DEBUG_CHECK_NUM_BPS_WPS,
+					    RMI_EXIT_HOST_CALL,
+					    0);
+
+	if (!realm_rc || !host_destroy_realm(&realm)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * Write random values to DBGBVRn_EL1 registers and set the values of
+ * DBGBCRn_EL1 registers.
+ */
+static void ns_set_bps(int num_bps, u_register_t expected_bp_addr[])
+{
+	unsigned int rand_addr;
+	for(int i = 0; i <= num_bps; i++) {
+		u_register_t value = debug_regs[i].read_dbgbcr_el1();
+		printf("i = %d, read dbgbcr register = %lx\n", i, value);
+		value = ns_set_dbgbcr_el1(debug_regs[i].read_dbgbcr_el1());
+		printf("i = %d, updated dbgbcr register = %lx\n", i, value);
+		debug_regs[i].write_dbgbcr_el1(value);
+		printf("i = %d, write to dbgbcr register = %lx\n", i, debug_regs[i].read_dbgbcr_el1());
+		rand_addr = get_random_address();
+		printf("i = %d, random address generated = %x\n", i, rand_addr);
+		printf("i = %d, read dbgbvr register = %lx\n", i, debug_regs[i].read_dbgbvr_el1());
+		debug_regs[i].write_dbgbvr_el1(INPLACE(DBGBVR_EL1_VA, rand_addr));
+		printf("i = %d, updated dbgbvr = %lx\n", i, debug_regs[i].read_dbgbvr_el1());
+		expected_bp_addr[i] = rand_addr;
+		printf("i = %d, expected_bp_addr[%d] = %lx\n", i, i, expected_bp_addr[i]);
+		printf("\n");
+
+	}
+
+}
+
+/*
+ * Write random values to DBGWVRn_EL1 registers and set the values of
+ * DBGWCRn_EL1 registers.
+ */
+static void ns_set_wps(int num_wps, u_register_t expected_wp_addr[])
+{
+	unsigned int rand_addr;
+	for(int i = 0; i <= num_wps; i++) {
+		u_register_t value = debug_regs[i].read_dbgwcr_el1();
+		printf("i = %d, value = %lx\n", i, value);
+		value = ns_set_dbgwcr_el1(debug_regs[i].read_dbgwcr_el1());
+		printf("i = %d, updated value = %lx\n", i, value);
+		debug_regs[i].write_dbgwcr_el1(value);
+		printf("i = %d, updated dbgbcr = %lx\n", i, debug_regs[i].read_dbgbcr_el1());
+		rand_addr = get_random_address();
+		printf("i = %d, random address generated = %x\n", i, rand_addr);
+		printf("i = %d, read dbgwvr register = %lx\n", i, debug_regs[i].read_dbgwvr_el1());
+		debug_regs[i].write_dbgwvr_el1(INPLACE(DBGWVR_EL1_VA, rand_addr));
+		printf("i = %d, updated dbgbvr = %lx\n", i, debug_regs[i].read_dbgbvr_el1());
+		expected_wp_addr[i] = rand_addr;
+		printf("i = %d, expected_wp_addr[%d] = %lx\n", i, i, expected_wp_addr[i]);
+		printf("\n");
+	}
+}
+
+/*
+ * Verify that the values of DBGBCRn_EL1 and DBGBVRn_EL1 registers are the same
+ * as what was written previously.
+ */
+static test_result_t ns_check_bps(int num_bps, u_register_t expected_bp_addr[])
+{
+	test_result_t rc = TEST_RESULT_FAIL;
+
+	if (num_bps != 0UL) {
+		for(int i = 0; i <= num_bps; i++) {
+			if (!ns_check_dbgbcr_el1(debug_regs[i].read_dbgbcr_el1()) && \
+				(EXTRACT(DBGBVR_EL1_VA, debug_regs[i].read_dbgbvr_el1()) != \
+				expected_bp_addr[i])) {
+				rc = TEST_RESULT_FAIL;
+				break;
+			}
+			printf("i = %d, debug_regs[i].read_dbgbvr_el1() = %lx\n", i, debug_regs[i].read_dbgbvr_el1());
+			printf("i = %d, expected_bp_addr[i] = %lx\n", i, expected_bp_addr[i]);
+			printf("\n");
+		}
+		rc = TEST_RESULT_SUCCESS;
+	}
+	return rc;
+}
+
+/*
+ * Verify that the values of DBGWCRn_EL1 and DBGWVRn_EL1 registers are the same
+ * as what was written previously.
+ */
+static test_result_t ns_check_wps(int num_wps, u_register_t expected_wp_addr[])
+{
+	test_result_t rc = TEST_RESULT_FAIL;
+
+	if (num_wps != 0UL) {
+		for(int i = 0; i <= num_wps; i++) {
+			if (!ns_check_dbgwcr_el1(debug_regs[i].read_dbgwcr_el1()) && \
+				(EXTRACT(DBGWVR_EL1_VA, debug_regs[i].read_dbgwvr_el1()) != \
+				expected_wp_addr[i])) {
+				rc = TEST_RESULT_FAIL;
+				break;
+			}
+		}
+		rc = TEST_RESULT_SUCCESS;
+	}
+	return rc;
+}
+
+/*
+ * Write to the self-hosted debug registers in NS world. Enter Realm and write
+ * random values to the self-hosted debug registers. Check that switching
+ * between NS world/Realm does not corrupt the values in the self-hosted debug
+ * registers.
+ */
+test_result_t host_realm_test_debug_save_restore(void)
+{
+	unsigned long num_bps, num_wps;
+	u_register_t feature_flag;
+	u_register_t rec_flag[1] = {RMI_RUNNABLE};
+	struct realm realm;
+	u_register_t expected_bp_addr[MAX_BPS] = { 0 };
+	u_register_t expected_wp_addr[MAX_WPS] = { 0 };
+	test_result_t test_rc = TEST_RESULT_SUCCESS;
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	/* If BPs is 0xF, read DFR1 to find no of BPs supported by HW */
+	num_bps = EXTRACT(ID_AA64DFR0_BRPs, read_id_aa64dfr0_el1());
+	if (num_bps == (MAX_BPS - 1UL)) {
+		num_bps = EXTRACT(ID_AA64DFR1_BRPs, read_id_aa64dfr1_el1());
+		if (num_bps == 0UL) {
+			num_bps = MAX_BPS - 1UL;
+		}
+	}
+
+	/* If WPs is 0xF, read DFR1 to find no of BPs supported by HW */
+	num_wps = EXTRACT(ID_AA64DFR0_WRPs, read_id_aa64dfr0_el1());
+
+	if (num_wps == MAX_WPS - 1UL) {
+		num_wps = EXTRACT(ID_AA64DFR1_WRPs, read_id_aa64dfr1_el1());
+		if(num_wps == 0UL) {
+			num_wps = MAX_WPS - 1UL;
+		}
+	}
+
+	feature_flag = INPLACE(RMI_FEATURE_REGISTER_0_NUM_BPS, num_bps) |
+		       INPLACE(RMI_FEATURE_REGISTER_0_NUM_WPS, num_wps);
+
+	if (!host_create_activate_realm_payload(&realm,
+					(u_register_t)REALM_IMAGE_BASE,
+					feature_flag,RTT_MIN_LEVEL, rec_flag, 1U, 0U)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	if (!host_create_shared_mem(&realm)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	write_mdscr_el1(read_mdscr_el1() |
+			MDSCR_EL1_TDCC_BIT |
+			MDSCR_EL1_KDE_BIT |
+			MDSCR_EL1_MDE_BIT);
+
+	INFO("NS: Write random values\n");
+
+	ns_set_bps(num_bps, expected_bp_addr);
+	ns_set_wps(num_wps, expected_wp_addr);
+
+	/*
+	 * Enter Realm and write random values to the self-hosted debug
+	 * registers. We will later check that there is no corruption of
+	 * register values on NS or Realm side.
+	 */
+	if (!host_enter_realm_execute(&realm, REALM_DEBUG_FILL_REGS,
+				      RMI_EXIT_HOST_CALL, 0)) {
+		test_rc = TEST_RESULT_FAIL;
+		goto destroy_realm;
+	}
+
+	INFO("NS: Read and compare\n");
+
+	/*
+	 * Verify that the self-hosted debug registers have been saved and
+	 * restored correctly.
+	 */
+	if ((ns_check_bps(num_bps, expected_bp_addr) != TEST_RESULT_SUCCESS) ||
+	    (ns_check_wps(num_wps, expected_wp_addr) != TEST_RESULT_SUCCESS)) {
+		test_rc = TEST_RESULT_FAIL;
+		goto destroy_realm;
+	}
+
+	/*
+	 * Enter Realm and verify that the self-hosted debug registers have been
+	 * saved and restored correctly.
+	 */
+	if (!host_enter_realm_execute(&realm, REALM_DEBUG_CMP_REGS,
+				      RMI_EXIT_HOST_CALL, 0)) {
+		test_rc = TEST_RESULT_FAIL;
+	}
+
+destroy_realm:
+	if (!host_destroy_realm(&realm)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return test_rc;
+}
