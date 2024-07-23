3d8b3ed5eeea59c9ef2f96f8bcdf1dcd8d7df1ec
Alex Liang <alex.liang2@arm.com>
2024-07-23 16:42:16 -0500

    test(fuzz) adding fuzzing for vendor-el3 smccc calls
    Change-Id: I4fd64c0a4c02de6d67a372c9c4bf86bcb9e4d091
    Signed-off-by: Alex Liang <alex.liang2@arm.com>

--- smc_fuzz/include/sdei_fuzz_helper.h
+++ smc_fuzz/include/sdei_fuzz_helper.h
@@ -4,15 +4,18 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdlib.h>
+#include <time.h>
+
 #include <fuzz_helper.h>
+#include "smcmalloc.h"
+
 #include <power_management.h>
 #include <sdei.h>
 #include <test_helpers.h>
 #include <tftf_lib.h>
 #include <timer.h>
-#include <time.h>
-#include <stdlib.h>
-#include "smcmalloc.h"
+
 
 #ifndef sdei_version_funcid
 #define sdei_version_funcid 0
@@ -96,5 +99,5 @@
 int64_t tftf_test_sdei_noarg(int64_t (*sdei_func)(void), char *funcstr);
 void tftf_test_sdei_singlearg(int64_t (*sdei_func)(uint64_t), char *funcstr);
 void run_sdei_fuzz(int funcid, struct memmod *mmod, bool inrange, int cntid);
-char* return_str(int64_t ret);
+char *return_str(int64_t ret);
 void print_ret(char *funcstr, int64_t ret);
