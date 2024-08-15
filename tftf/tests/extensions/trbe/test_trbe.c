fa0ce0421abdad23fe927bee9ba6208bab614368
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2024-08-15 11:49:49 -0500

    test: update TRBE test to handle TRBE disable case
    This patch handles the scenario where TRBE feature is
    manually disabled in a core that supports TRBE feature.
    Historically we have always enabled TRBE in most cores
    that support it but the asymmetric feature allows
    certain cores that support TRBE to have them disabled
    for a performance improvement.
    
    Change-Id: Ibad187277c5443349f85496af3b961f5d2da7778
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>

--- tftf/tests/extensions/trbe/test_trbe.c
+++ tftf/tests/extensions/trbe/test_trbe.c
@@ -6,9 +6,28 @@
 
 #include <string.h>
 
+#include <arch.h>
+#include <arch_helpers.h>
 #include <test_helpers.h>
 #include <tftf_lib.h>
 #include <tftf.h>
+
+#ifdef __aarch64__
+#include <sync.h>
+
+static volatile bool undef_injection_triggered;
+
+static bool trbe_trap_exception_handler(void)
+{
+	uint64_t esr_el2 = read_esr_el2();
+	if (EC_BITS(esr_el2) == EC_UNKNOWN) {
+		undef_injection_triggered = true;
+		return true;
+	}
+
+	return false;
+}
+#endif /* __aarch64__ */
 
 /*
  * EL3 is expected to allow access to trace control registers from EL2.
@@ -17,10 +36,15 @@
  */
 test_result_t test_trbe_enabled(void)
 {
-        SKIP_TEST_IF_AARCH32();
+	SKIP_TEST_IF_AARCH32();
 
 #ifdef __aarch64__
 	SKIP_TEST_IF_TRBE_NOT_SUPPORTED();
+
+	undef_injection_triggered = false;
+
+	register_custom_sync_exception_handler(trbe_trap_exception_handler);
+
 	read_trblimitr_el1();
 	read_trbptr_el1();
 	read_trbbaser_el1();
@@ -28,6 +52,14 @@ test_result_t test_trbe_enabled(void)
 	read_trbmar_el1();
 	read_trbtrg_el1();
 	read_trbidr_el1();
+
+	unregister_custom_sync_exception_handler();
+
+	if (undef_injection_triggered == true) {
+		VERBOSE("UNDEF injection from EL3. TRBE is Disabled\n");
+		return TEST_RESULT_SKIPPED;
+	}
+
 	return TEST_RESULT_SUCCESS;
 #endif  /* __aarch64__ */
 }
