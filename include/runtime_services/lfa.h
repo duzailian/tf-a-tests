373a0e123603bdb5dfbeb57d9a826f4b4d7127f7
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2024-08-16 09:13:28 +0100

    feat(lfa): add LFA SMCs tests using a single CPU
    Add a LFA tests for single CPU without the Activate SMC.
    Also, include a negative test case where the firmware
    is unavailable for activation during the Prime phase,
    resulting in a failure.
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: I9fff281f77de87580f5b3c1fecd69fbf0f5d1f2d

---
+++ include/runtime_services/lfa.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LFA_H
+#define LFA_H
+
+#include <arm_arch_svc.h>
+#include <arch_helpers.h>
+#include <smccc.h>
+#include <stdint.h>
+
+#define LFA_VERSION                     U(0xC40001E0)
+#define LFA_FEATURES                    U(0xC40001E1)
+#define LFA_GET_INFO                    U(0xC40001E2)
+#define LFA_GET_INVENTORY               U(0xC40001E3)
+#define LFA_PRIME                       U(0xC40001E4)
+#define LFA_ACTIVATE                    U(0xC40001E5)
+#define LFA_CANCEL                      U(0xC40001E6)
+#define LFA_INVALID			LFA_CANCEL + 1U
+
+#define BL31_X1 			0x4698fe4c6d08d447
+#define BL31_X2 			0x005abdcb5029959b
+
+#endif /* LFA_H */
