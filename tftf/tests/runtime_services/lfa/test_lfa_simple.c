aa3b613dc78c2ffdeb601d8b08cf3f446455e28f
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2024-08-16 09:13:28 +0100

    feat(lfa): add LFA SMCs tests using a single CPU
    Add a LFA tests for single CPU without the Activate SMC.
    Also, include a negative test case where the firmware
    is unavailable for activation during the Prime phase,
    resulting in a failure.
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: I9fff281f77de87580f5b3c1fecd69fbf0f5d1f2d

---
+++ tftf/tests/runtime_services/lfa/test_lfa_simple.c
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <debug.h>
+#include <test_helpers.h>
+#include <tftf_lib.h>
+
+#include <lfa.h>
+
+static uint64_t num_components = 0;
+static uint64_t fw_id;
+
+/*
+ * @Test_Aim@ Simple surface tests for Live Firmware Activation.
+ *
+ * This test checks the version number. It runs on the lead CPU.
+ */
+test_result_t test_lfa_version(void)
+{
+	smc_args args = { LFA_VERSION };
+	uint32_t major, minor;
+	smc_ret_values ret = tftf_smc(&args);
+
+	major = (uint32_t)(ret.ret0 >> 16);
+	minor = (uint32_t)(ret.ret0 & 0xffff);
+
+	tftf_testcase_printf("%s LFA API Version : %d.%d\n", __func__,
+							major, minor);
+
+	if ((major != 1) || (minor != 0)) {
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test for LFA_FEATURES.
+ *
+ * This test checks LFA_FEATURES for the two extreme FID values and then does
+ * one negative test.
+ */
+test_result_t test_lfa_features(void)
+{
+	smc_args args = { .fid = LFA_FEATURES, .arg1 = LFA_VERSION };
+	smc_ret_values ret = tftf_smc(&args);
+	if (ret.ret0 != 0) {
+		tftf_testcase_printf("%s: Features for FID=0x%08lx returned %08lx\n",
+			__func__, args.arg1, ret.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	args.arg1 = LFA_CANCEL;
+	ret = tftf_smc(&args);
+	if (ret.ret0 != 0) {
+		tftf_testcase_printf("%s: Features for FID=0x%08lx returned %08lx\n",
+			__func__, args.arg1, ret.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	args.arg1 = LFA_CANCEL + 3;
+	ret = tftf_smc(&args);
+	if (ret.ret0 == 0) {
+		tftf_testcase_printf("%s: Features for FID=0x%08lx returned %08lx\n",
+			__func__, args.arg1, ret.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test for LFA_GET_INFO.
+ *
+ * This test checks that LFA_GET_INFO returns the expected value for
+ * lfa_num_components and the proper error if an invalid lfa_selector is
+ * provided.
+ */
+test_result_t test_lfa_get_info(void)
+{
+	smc_args args = { .fid = LFA_GET_INFO, .arg1 = 0 };
+	smc_ret_values ret = tftf_smc(&args);
+	if (ret.ret0 != 0) {
+		tftf_testcase_printf("%s: Unexpected error: 0x%08lx\n", __func__, ret.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	if (ret.ret1 < 3) {
+		tftf_testcase_printf("%s: Unexpected value for num_components: %ld\n",
+			__func__, ret.ret1);
+		return TEST_RESULT_FAIL;
+	}
+
+	num_components = ret.ret1;
+
+	args.arg1 = 1;
+	ret = tftf_smc(&args);
+	if (ret.ret0 == 0) {
+		tftf_testcase_printf("%s: Unexpected success\n", __func__);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test for LFA_GET_INVENTORY.
+ */
+test_result_t test_lfa_get_inventory(void)
+{
+	smc_args args = { .fid = LFA_GET_INVENTORY, .arg1 = 0 };
+	smc_ret_values ret;
+
+	for (uint64_t i = 0; i < num_components; i++) {
+		args.arg1 = i;
+		ret = tftf_smc(&args);
+		tftf_testcase_printf("%s: test_lfa_get_inventory(%lld) => 0x%08lx\n", __func__, i, ret.ret0);
+
+		ret = tftf_smc(&args);
+		if (ret.ret0 != 0) {
+			tftf_testcase_printf("%s: Unexpected error: 0x%08lx\n", __func__, ret.ret0);
+			return TEST_RESULT_FAIL;
+		}
+
+		if ((ret.ret1 == BL31_X1) && (ret.ret2 == BL31_X2)) {
+			fw_id = i;
+		}
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test for LFA_PRIME.
+ */
+test_result_t test_lfa_prime(void)
+{
+	smc_args args = { .fid = LFA_PRIME, .arg1 = fw_id };
+	smc_ret_values ret = tftf_smc(&args);
+	if (ret.ret0 != 0) {
+		tftf_testcase_printf("%s: Unexpected error: 0x%08lx\n", __func__, ret.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test for LFA_CANCEL.
+ */
+test_result_t test_lfa_cancel(void)
+{
+	smc_args args = { .fid = LFA_CANCEL, .arg1 = 0 };
+	smc_ret_values ret;
+
+	ret = tftf_smc(&args);
+	if (ret.ret0 != 0) {
+		tftf_testcase_printf("%s: Unexpected error: 0x%08lx\n", __func__, ret.ret0);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
