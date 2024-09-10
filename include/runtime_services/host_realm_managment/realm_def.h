57e385824b1bedb4aade896d6a078133254837db
AlexeiFedorov <Alexei.Fedorov@arm.com>
2024-09-10 15:50:44 +0100

    fix(realm): fix calculation of Realm's REC index
    This patch fixes the issues related to the calculation of
    Realm's REC index based on the read value of MPIDR_EL1 register
    and REC's mpidr parameter from the REC's index.
    RMM reports MPIDR_EL1.Aff0 field matching RmiRecMpidr type
    with [7:4] bits RES0, making MPIDR_EL1=0x80000100 represent
    REC 16, but not 256 as it is implemented in the existing code.
    The patch adds the following macros:
    - RMI_REC_MPIDR(idx) which calculates RmiRecMpidr value based
    on REC index.
    - REC_IDX(mpidr) gets REC index from MPIDR_EL1.
    
    Change-Id: Ieac473984f3a50d2815dcfe8d291d31bd70ebae7
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>

--- include/runtime_services/host_realm_managment/realm_def.h
+++ include/runtime_services/host_realm_managment/realm_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023, Arm Limited. All rights reserved.
+ * Copyright (c) 2022-2024, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -28,5 +28,83 @@
 #else
 #error "Undefined value for PAGE_SIZE"
 #endif
+
+/*
+ * MPIDR_EL1 definitions
+ * 'MPIDR_EL1_AFF<n>_VAL_SHIFT' constants specify the right shift
+ * for affinity field <n> that gives the field's actual value.
+ */
+
+/* Aff0[3:0] - Affinity level 0
+ * For compatibility with GICv3 only Aff0[3:0] field is used,
+ * and Aff0[7:4] of MPIDR_EL1 value is RES0 to match RmiRecMpidr.
+ */
+#define MPIDR_EL1_AFF0_SHIFT		0
+#define MPIDR_EL1_AFF0_WIDTH		U(4)
+#define MPIDR_EL1_AFF0_MASK		0xF
+#define MPIDR_EL1_AFF0_VAL_SHIFT	0
+
+/* Aff1[15:8] - Affinity level 1 */
+#define MPIDR_EL1_AFF1_SHIFT		8
+#define MPIDR_EL1_AFF1_WIDTH		U(8)
+#define MPIDR_EL1_AFF1_VAL_SHIFT	4
+
+/* Aff2[23:16] - Affinity level 2 */
+#define MPIDR_EL1_AFF2_SHIFT		16
+#define MPIDR_EL1_AFF2_WIDTH		U(8)
+#define MPIDR_EL1_AFF2_VAL_SHIFT	4
+
+/* Aff3[39:32] - Affinity level 3 */
+#define MPIDR_EL1_AFF3_SHIFT		32
+#define MPIDR_EL1_AFF3_WIDTH		U(8)
+#define MPIDR_EL1_AFF3_VAL_SHIFT	12
+
+/* Aff20[23:0] - Affinity levels 2-0 mask */
+#define MPIDR_AFF20_MASK					\
+		((MPIDR_AFFLVL_MASK << MPIDR_AFF0_SHIFT) |	\
+		 (MPIDR_AFFLVL_MASK << MPIDR_AFF1_SHIFT) |	\
+		 (MPIDR_AFFLVL_MASK << MPIDR_AFF2_SHIFT))
+
+/*
+ * Extract the value of MPIDR_EL1.Aff<n> register field shifted right
+ * so it can be evaluated directly.
+ */
+#define MPIDR_EL1_AFF(n, reg)	\
+	(((reg) & MASK(MPIDR_EL1_AFF##n)) >> MPIDR_EL1_AFF##n##_VAL_SHIFT)
+
+/* Calculate REC index from mpidr of MPIDR_EL1 register type */
+#define REC_IDX(mpidr)				\
+		(MPIDR_EL1_AFF(0, mpidr) |	\
+		 MPIDR_EL1_AFF(1, mpidr) |	\
+		 MPIDR_EL1_AFF(2, mpidr) |	\
+		 MPIDR_EL1_AFF(3, mpidr))
+
+/*
+ * RmiRecMpidr type definitions.
+ *
+ * Aff0[3:0] - Affinity level 0
+ * For compatibility with GICv3 only Aff0[3:0] field is used,
+ * and Aff0[7:4] of a REC MPIDR value is RES0.
+ */
+#define RMI_MPIDR_AFF0_SHIFT		0
+#define RMI_MPIDR_AFF0_WIDTH		U(4)
+
+/* Aff1[15:8] - Affinity level 1 */
+#define RMI_MPIDR_AFF1_SHIFT		8
+#define RMI_MPIDR_AFF1_WIDTH		U(8)
+
+/* Aff2[23:16] - Affinity level 2 */
+#define RMI_MPIDR_AFF2_SHIFT		16
+#define RMI_MPIDR_AFF2_WIDTH		U(8)
+
+/* Aff3[31:24] - Affinity level 3 */
+#define RMI_MPIDR_AFF3_SHIFT		24
+#define RMI_MPIDR_AFF3_WIDTH		U(8)
+
+/* Convert REC linear index to RmiRecMpidr */
+#define RMI_REC_MPIDR(idx)				\
+		(((idx) & MASK(RMI_MPIDR_AFF0)) |	\
+		(((idx) & ~MASK(RMI_MPIDR_AFF0)) << 	\
+		(RMI_MPIDR_AFF1_SHIFT - RMI_MPIDR_AFF0_WIDTH)));
 
 #endif /* REALM_DEF_H */
