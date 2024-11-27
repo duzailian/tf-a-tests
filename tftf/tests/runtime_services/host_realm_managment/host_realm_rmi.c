a47553a01d6058eeb764118f50831ca5b402dd8f
Shruti Gupta <shruti.gupta@arm.com>
2024-11-27 09:34:35 +0000

    test(realm): handle permission fault for planes
    Add support for handling permission fault in planes.
    Set s2ap in RTTs.
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: I590fc5a1c43357b117fa5cb76e8c699c4c7eebad

--- tftf/tests/runtime_services/host_realm_managment/host_realm_rmi.c
+++ tftf/tests/runtime_services/host_realm_managment/host_realm_rmi.c
@@ -1726,6 +1726,101 @@ u_register_t host_realm_rec_enter(struct realm *realm,
 			}
 		}
 
+		if ((run->exit.exit_reason == RMI_EXIT_SYNC) &&
+				(((run->exit.hpfar >> 4U) >= (PAGE_POOL_BASE >> PAGE_SIZE_SHIFT) &&
+				 ((run->exit.hpfar >> 4U) <=
+				  (((PAGE_POOL_BASE + (PAGE_POOL_MAX_SIZE)) >>
+				    PAGE_SIZE_SHIFT)))))) {
+
+			/*
+			 * If exception is not in primary rtt
+			 * Map Adr in failing Aux RTT and re-enter rec
+			 * Note - PlaneN uses Primary RTT tree index 0
+			 */
+
+			if (!realm->rtt_tree_single &&
+					(realm->num_aux_planes > 0U) &&
+					(run->exit.rtt_tree != 0U) &&
+					(((run->exit.hpfar << 8U) >= PAGE_POOL_BASE) &&
+					 ((run->exit.hpfar << 8U) <= PAGE_POOL_END))) {
+
+				u_register_t fail_index;
+				u_register_t level_pri;
+				u_register_t state;
+				u_register_t ripas;
+
+				VERBOSE("host aux_map 0x%lx rtt 0x%lx\n",
+						run->exit.hpfar, run->exit.rtt_tree);
+
+				ret = host_realm_aux_map_protected_data(realm,
+						run->exit.hpfar << 8U,
+						PAGE_SIZE,
+						run->exit.rtt_tree,
+						&fail_index, &level_pri, &state, &ripas);
+
+				if (ret != REALM_SUCCESS) {
+					ERROR("host_realm_aux_map_protected_data failed\n");
+					break;
+				}
+			re_enter_rec = true;
+			}
+		}
+
+		if ((run->exit.exit_reason == RMI_EXIT_S2AP_CHANGE) &&
+			       (((run->exit.s2ap_base) >= PAGE_POOL_BASE) &&
+				((run->exit.s2ap_base) <= PAGE_POOL_END))) {
+
+			u_register_t new_base = run->exit.s2ap_base;
+			u_register_t top = run->exit.s2ap_top;
+			u_register_t rtt_tree;
+
+			while (new_base != top) {
+				ret = host_rmi_rtt_set_s2ap(realm->rd,
+						    realm->rec[0U],
+						    new_base,
+						    top, &new_base,
+						    &rtt_tree);
+
+				if (RMI_RETURN_STATUS(ret) == RMI_ERROR_RTT_AUX) {
+
+					assert(rtt_tree != 0U);
+					/* create missing aux rtt level */
+					VERBOSE("set s2ap fail missing aux rtt=%lx 0x%lx 0x%lx\n",
+							rtt_tree, new_base, top);
+
+					ret = host_rmi_create_rtt_aux_levels(realm, new_base,
+							RMI_RETURN_INDEX(ret),
+							RMI_RETURN_INDEX(ret) + 1U, rtt_tree);
+					if (ret != RMI_SUCCESS) {
+						INFO("host_rmi_create_rtt_aux_levels \
+								failed 0x%lx\n", new_base);
+						break;
+					}
+					continue;
+				} else if (RMI_RETURN_STATUS(ret) == RMI_ERROR_RTT) {
+					assert(rtt_tree == 0U);
+					INFO("set s2ap failed missing rtt=0x%lx 0x%lx 0x%lx\n\n",
+							rtt_tree, new_base, top);
+
+					ret = host_rmi_create_rtt_levels(realm, new_base,
+							RMI_RETURN_INDEX(ret),
+							RMI_RETURN_INDEX(ret) + 1U);
+
+					if (ret != RMI_SUCCESS) {
+						INFO("host_rmi_create_rtt_levels \
+								failed 0x%lx\n", new_base);
+						break;
+					}
+
+					break;
+				} else if (ret != RMI_SUCCESS) {
+					INFO("host set s2ap failed\n");
+					break;
+				}
+			}
+			re_enter_rec = true;
+		}
+
 		if (ret != RMI_SUCCESS) {
 			return ret;
 		}
