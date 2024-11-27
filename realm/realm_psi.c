e14f5f99543a761a95c95ed25d61ed0f7eb81a29
Shruti Gupta <shruti.gupta@arm.com>
2024-11-27 05:29:55 +0000

    test(realm): add plane PSI interface
    Add Plane service routine interface.
    Aux plane can communicate with primary plane using PSI
    PSI uses hvc conduit from aux plane.
    Add initial printf support for planes
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: I484cbd32791970c6e22c5d63e13b43807f4f3c06

---
+++ realm/realm_psi.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+#include <host_realm_rmi.h>
+#include <realm_helpers.h>
+#include <realm_psi.h>
+#include <realm_rsi.h>
+#include <smccc.h>
+
+void realm_psi_exit_to_plane0(u_register_t psi_cmd)
+{
+	if (realm_is_plane0()) {
+		return;
+	}
+	tftf_hvc(&(hvc_args) {psi_cmd, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL});
+}
+
+u_register_t realm_psi_get_plane_id(void)
+{
+	hvc_ret_values res = tftf_hvc(&(hvc_args) {PSI_CALL_GET_PLANE_ID_CMD, 0UL, 0UL,
+			0UL, 0UL, 0UL, 0UL, 0UL});
+
+	if (res.ret0 != RSI_SUCCESS) {
+		return (u_register_t)-1;
+	}
+	return res.ret1;
+}
+
