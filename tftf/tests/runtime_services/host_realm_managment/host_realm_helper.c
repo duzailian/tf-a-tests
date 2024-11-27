78d6ac3e65e6380e4bea53efe5e4e71e23285480
Shruti Gupta <shruti.gupta@arm.com>
2024-11-27 05:29:55 +0000

    test(realm): add plane PSI interface
    Add Plane service routine interface.
    Aux plane can communicate with primary plane using PSI
    PSI uses hvc conduit from aux plane.
    Also add printf support for planes
    
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: I484cbd32791970c6e22c5d63e13b43807f4f3c06

--- tftf/tests/runtime_services/host_realm_managment/host_realm_helper.c
+++ tftf/tests/runtime_services/host_realm_managment/host_realm_helper.c
@@ -45,14 +45,14 @@ const char *rmi_exit[] = {
  * The function handler to print the Realm logged buffer,
  * executed by the secondary core
  */
-void realm_print_handler(struct realm *realm_ptr, unsigned int rec_num)
+void realm_print_handler(struct realm *realm_ptr, unsigned int plane_num, unsigned int rec_num)
 {
 	size_t str_len = 0UL;
 	host_shared_data_t *host_shared_data;
 	char *log_buffer;
 
 	assert(realm_ptr != NULL);
-	host_shared_data = host_get_shared_structure(realm_ptr, 0U, rec_num);
+	host_shared_data = host_get_shared_structure(realm_ptr, plane_num, rec_num);
 	log_buffer = (char *)host_shared_data->log_buffer;
 	str_len = strlen((const char *)log_buffer);
 
@@ -63,7 +63,8 @@ void realm_print_handler(struct realm *realm_ptr, unsigned int rec_num)
 	if (str_len != 0UL) {
 		/* Avoid memory overflow */
 		log_buffer[MAX_BUF_SIZE - 1] = 0U;
-		mp_printf("[VMID %u][Rec %u]: %s", realm_ptr->vmid, rec_num, log_buffer);
+		mp_printf("[VMID %u][Rec %u]: %s", plane_num == 0U ? realm_ptr->vmid :
+				realm_ptr->aux_vmid[plane_num - 1U], rec_num, log_buffer);
 		(void)memset((char *)log_buffer, 0, MAX_BUF_SIZE);
 	}
 }
