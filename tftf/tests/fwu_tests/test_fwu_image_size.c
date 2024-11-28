cd1cbf359b92f5608e529ab529f6a10fd47affed
Thaddeus Gonzalez-Serna <Thaddeus.Gonzalez-Serna@arm.com>
2024-11-28 01:12:15 -0600

    test(fwu):validate scenario of invalid FWU IMAGE SIZE
    Test to Increase code coverage in bl1 stage where
    bl2 and bl2u are both corrupt with invalid image sizes
    
    bl1_fwu.c cc% improved by 10%
    bl1_main.c cc% imporved by 1%
    
    Change-Id: I4637a9e566e4edda8474da37ea4f3511fa32b6a7
    Signed-off-by: Thaddeus Gonzalez-Serna <Thaddeus.Gonzalez-Serna@arm.com>

---
+++ tftf/tests/fwu_tests/test_fwu_image_size.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2025, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <firmware_image_package.h>
+#include <fwu_nvm.h>
+#include <io_storage.h>
+#include <image_loader.h>
+#include <platform.h>
+#include <psci.h>
+#include <smccc.h>
+#include <status.h>
+#include <tftf_lib.h>
+#include <uuid.h>
+#include <uuid_utils.h>
+
+/*
+ * @TEST_AIM@ validate FWU IMAGE SIZE invalid scenario
+ * TEST SUCCESS if FWU FIP IMAGE SIZE is found invalid and Fails to Proceed
+ * TEST FAIL in case FWU process is able to reboot
+ */
+
+static fip_toc_entry_t *
+find_fiptoc_entry_t(const int fip_base, const uuid_t *uuid)
+{
+	fip_toc_entry_t *current_file =
+		(fip_toc_entry_t *) (fip_base + sizeof(fip_toc_header_t));
+
+	while (!is_uuid_null(&(current_file->uuid))) {
+		if (uuid_equal(&(current_file->uuid), uuid)) {
+			return current_file;
+		}
+		current_file += 1;
+	};
+	return NULL;
+}
+
+test_result_t test_fwu_image_size(void)
+{
+	STATUS status;
+	int fip_base = PLAT_ARM_FIP_BASE;
+	int fwu_fip_base = PLAT_ARM_FWU_FIP_BASE;
+
+	const uuid_t bl2	= UUID_TRUSTED_BOOT_FIRMWARE_BL2;
+	const uuid_t bl2u	= UUID_FIRMWARE_UPDATE_BL2U;
+
+	fip_toc_entry_t * bl2_entry;
+	fip_toc_entry_t * bl2u_entry;
+
+	uintptr_t bl2_entry_address, bl2u_entry_address;
+
+	int corrupt_size = 0xFFFF;
+
+	smc_args args = { SMC_PSCI_SYSTEM_RESET };
+	smc_ret_values ret = {0};
+
+
+	//retrieve bl2 and bl2u
+	bl2_entry = find_fiptoc_entry_t(fip_base, &bl2);
+	bl2_entry_address = (uintptr_t)bl2_entry -fip_base;
+
+	bl2u_entry = find_fiptoc_entry_t(fwu_fip_base, &bl2u);
+	bl2u_entry_address = (intptr_t)bl2u_entry -fwu_fip_base;
+
+	tftf_testcase_printf("bl2_entry size value is (%llx) \n", bl2_entry->size);
+	tftf_testcase_printf("bl2_entry offset value is (%llx) \n", bl2_entry->offset_address);
+
+		/* Reboot has not occured yet */
+		tftf_testcase_printf("not rebooted yet \n");
+
+		/* Corrupt FWU_FIP_SIZE */
+		tftf_testcase_printf("bl2_entry_address value is (%lx) \n", bl2_entry_address);
+
+		/*
+		 * corrupt bl2 in fip
+		 * corrupt toc entry size field of bl2u
+		*/
+		
+		//corrupt bl2 image
+		status = fwu_nvm_write(bl2_entry->offset_address, &corrupt_size, sizeof(uint64_t));
+
+		if(status != STATUS_SUCCESS){
+				tftf_testcase_printf("STATUS NOT SUCCESS ERROR %d \n", status);
+		}
+
+		//corrupt toc bl2u entry
+		int offset = bl2u_entry_address + sizeof(uuid_t) + sizeof(uint64_t);
+		
+		status = fwu_nvm_write(offset, &corrupt_size, sizeof(uint64_t));
+		if(status != STATUS_SUCCESS){
+						tftf_testcase_printf("STATUS NOT SUCCESS ERROR %d \n",status);
+		}
+
+		//read buffer to confirm
+		int buffer;
+		status = fwu_nvm_read(offset, &buffer, sizeof(uint64_t));
+		tftf_testcase_printf("offset value is %x \n", offset);
+		tftf_testcase_printf("post nvm write+read %x \n", buffer);
+
+		if(status != STATUS_SUCCESS){
+			tftf_testcase_printf("STATUS NOT SUCCESS ERROR %d \n",status);
+		}
+
+		//add printstatetment for expcet script is board is not expected to be recovered
+		tftf_testcase_printf("NVM READs");
+		tftf_testcase_printf("bl2_entry size value is (%llx) \n", bl2_entry->size);
+		tftf_testcase_printf("bl2_entry offset value is (%llx) \n", bl2_entry->offset_address);
+
+	/* Notify that we are rebooting now. */
+	tftf_notify_reboot();
+
+	/* Request PSCI system reset. */
+	ret = tftf_smc(&args); 
+
+	/* The PSCI SYSTEM_RESET call is not supposed to return */
+	tftf_testcase_printf("System didn't reboot properly (%d)\n",
+	(unsigned int)ret.ret0);
+
+	return TEST_RESULT_FAIL;
+}
