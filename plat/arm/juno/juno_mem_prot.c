750a4c12edb21df95331dcb4f6a8c5a58e19353e
Harrison Mutai <harrison.mutai@arm.com>
2025-02-28 15:48:04 +0000

    fix(arm): increase reserved DRAM1 mem for NS images
    Defconfig kernels are now approaching 50MB, making the previous 64MB
    allocation for both the kernel and initrd insufficient. To accommodate
    this growth, the reserved NS memory has been increased to 128MB in TF-A
    [1]. Adjust the expected ranges in TFTF accordingly.
    
    [1] https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/35851
    
    Change-Id: I5ae4d63d75300bb85817d7947c092550a0e57faf
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- plat/arm/juno/juno_mem_prot.c
+++ plat/arm/juno/juno_mem_prot.c
@@ -10,7 +10,7 @@
 #include <xlat_tables_v2.h>
 
 #define NS_IMAGE_OFFSET		TFTF_BASE
-#define NS_IMAGE_LIMIT		(NS_IMAGE_OFFSET + (32 << TWO_MB_SHIFT))
+#define NS_IMAGE_LIMIT		(NS_IMAGE_OFFSET + SZ_128M)
 
 static const mem_region_t juno_ram_ranges[] = {
 	{NS_IMAGE_LIMIT, 128 << TWO_MB_SHIFT},
