eb3a64fa4f7b5b862dad1d25d0b17c28f81a90f2
Juan Pablo Conde <juanpablo.conde@arm.com>
2025-05-07 14:42:50 -0500

    feat(realm): add tests for FEAT_MEC
    This patch creates two test in order to verify the correct
    assignment of MECIDs to realms.
    
    Signed-off-by: Juan Pablo Conde <juanpablo.conde@arm.com>
    Change-Id: I9de84f1ba8a8b42e55b1ea163f90e9daf56c74c9

--- tftf/tests/runtime_services/realm_payload/host_realm_payload_tests.c
+++ tftf/tests/runtime_services/realm_payload/host_realm_payload_tests.c
@@ -3487,3 +3487,94 @@ test_result_t host_realm_test_attestation_fault(void)
 
 	return TEST_RESULT_SUCCESS;
 }
+
+/*
+ * @Test_Aim@ Test MECID assignment to Realms
+ * Test whether two realms accept different MECIDs
+ */
+test_result_t host_realm_test_mecid(void)
+{
+	bool ret1 = false, ret2 = false, fail = false;
+	u_register_t rec_flag[] = {RMI_RUNNABLE};
+	struct realm realm1, realm2;
+	u_register_t feature_flag0 = 0UL;
+	long sl = RTT_MIN_LEVEL;
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	if (is_feat_52b_on_4k_2_supported()) {
+		feature_flag0 = RMI_FEATURE_REGISTER_0_LPA2;
+		sl = RTT_MIN_LEVEL_LPA2;
+	}
+
+	if (!host_create_activate_realm_payload(&realm1, (u_register_t)REALM_IMAGE_BASE,
+				feature_flag0, 0U, sl, rec_flag, 1U, 0U, TEST_MECID1)) {
+		fail = true;
+	}
+
+	if (!host_create_activate_realm_payload(&realm2, (u_register_t)REALM_IMAGE_BASE,
+				feature_flag0, 0U, sl, rec_flag, 1U, 0U, TEST_MECID2)) {
+		fail = true;
+		goto destroy_realm1;
+	}
+
+	ret2 = host_destroy_realm(&realm2);
+destroy_realm1:
+	ret1 = host_destroy_realm(&realm1);
+
+	if (fail || !ret1 || !ret2) {
+		ERROR("%s(): fail=%d destroy1=%d destroy2=%d\n",
+				__func__, fail, ret1, ret2);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
+
+/*
+ * @Test_Aim@ Test MECID assignment to Realms
+ * Test whether two realms accept the same MECID
+ */
+test_result_t host_realm_test_mecid_fault(void)
+{
+	bool ret1 = false, ret2 = false, fail = false;
+	u_register_t rec_flag[] = {RMI_RUNNABLE};
+	struct realm realm1, realm2;
+	u_register_t feature_flag0 = 0UL;
+	long sl = RTT_MIN_LEVEL;
+
+	SKIP_TEST_IF_RME_NOT_SUPPORTED_OR_RMM_IS_TRP();
+
+	if (is_feat_52b_on_4k_2_supported()) {
+		feature_flag0 = RMI_FEATURE_REGISTER_0_LPA2;
+		sl = RTT_MIN_LEVEL_LPA2;
+	}
+
+	if (!host_create_activate_realm_payload(&realm1, (u_register_t)REALM_IMAGE_BASE,
+				feature_flag0, 0U, sl, rec_flag, 1U, 0U, TEST_MECID1)) {
+		fail = true;
+	}
+
+	if (!host_create_activate_realm_payload(&realm2, (u_register_t)REALM_IMAGE_BASE,
+				feature_flag0, 0U, sl, rec_flag, 1U, 0U, TEST_MECID1)) {
+		/*
+		 * Creation should fail as there should not be two Realms with the same
+		 * MECID.
+		 */
+
+		goto destroy_realm1;
+	}
+	fail = true;
+
+	ret2 = host_destroy_realm(&realm2);
+destroy_realm1:
+	ret1 = host_destroy_realm(&realm1);
+
+	if (fail || !ret1 || !ret2) {
+		ERROR("%s(): fail=%d destroy1=%d destroy2=%d\n",
+				__func__, fail, ret1, ret2);
+		return TEST_RESULT_FAIL;
+	}
+
+	return TEST_RESULT_SUCCESS;
+}
