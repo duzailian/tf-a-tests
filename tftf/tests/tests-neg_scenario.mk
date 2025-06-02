1c5cf8195789ef42037b398ed493b5161743940e
Thaddeus Gonzalez-Serna <Thaddeus.Gonzalez-Serna@arm.com>
2025-06-02 09:42:49 -0500

    feat(neg-boot): tftf invalid rotpk test
    Test to improve code coverage in scenario with corrupt rotpk in fip certificate
    
    Change-Id: I995ab6d9f1488fda44e10ae9ef70c9056d780a89
    Signed-off-by: Thaddeus Gonzalez-Serna <Thaddeus.Gonzalez-Serna@arm.com>

--- tftf/tests/tests-neg_scenario.mk
+++ tftf/tests/tests-neg_scenario.mk
@@ -5,9 +5,36 @@
 #
 
 TESTS_SOURCES	+=	$(addprefix tftf/tests/neg_scenario_tests/,	\
+<<<<<<< HEAD
 				test_fwu_image_size.c \
+=======
+					test_invalid_rotpk.c \
+					neg_scenario_test_infra.c \
+>>>>>>> 47f4dc52 (feat(neg-boot): tftf invalid rotpk test)
 )
+TESTS_SOURCES	+=							\
+	$(addprefix tftf/tests/runtime_services/host_realm_managment/,	\
+	)
 
 TESTS_SOURCES	+=	plat/common/fwu_nvm_accessors.c \
+<<<<<<< HEAD
 				plat/common/image_loader.c \
 				plat/arm/common/arm_fwu_io_storage.c
+=======
+					plat/common/image_loader.c \
+					plat/arm/common/arm_fwu_io_storage.c \
+					plat/arm/fvp/fvp_trusted_boot.c \
+
+TESTS_SOURCES	+= $(addprefix drivers/auth/,	\
+		img_parser_mod.c	\
+	)
+
+
+TESTS_SOURCES	+= $(addprefix ext/mbedtls/library/,	\
+		error.c \
+		memory_buffer_alloc.c \
+	)
+
+include lib/ext_mbedtls/mbedtls.mk
+
+>>>>>>> 47f4dc52 (feat(neg-boot): tftf invalid rotpk test)
