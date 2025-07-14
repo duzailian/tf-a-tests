ac475868f7e107c14234de24e9c55df4d57280d8
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2025-07-14 11:43:35 +0100

    feat(host_realm_mgmt): invoke RMI PDEV notify and IDE reset ABIs
    Add support to call IDE key refresh and IDE reset on PDEV once it is in
    READY state.
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: Ib8afb11c53cffe002e08cad5dd88fd7a4606a65b

--- tftf/tests/runtime_services/host_realm_managment/host_realm_rmi.c
+++ tftf/tests/runtime_services/host_realm_managment/host_realm_rmi.c
@@ -1998,6 +1998,18 @@ u_register_t host_rmi_pdev_stop(u_register_t pdev_ptr)
 				2U).ret0;
 }
 
+u_register_t host_rmi_pdev_notify(u_register_t pdev_ptr, u_register_t event)
+{
+	return host_rmi_handler(&(smc_args) {SMC_RMI_PDEV_NOTIFY, pdev_ptr,
+					     event}, 3U).ret0;
+}
+
+u_register_t host_rmi_pdev_ide_reset(u_register_t pdev_ptr)
+{
+	return host_rmi_handler(&(smc_args) {SMC_RMI_PDEV_IDE_RESET, pdev_ptr},
+				2U).ret0;
+}
+
 u_register_t host_rmi_pdev_destroy(u_register_t pdev_ptr)
 {
 	return host_rmi_handler(&(smc_args) {SMC_RMI_PDEV_DESTROY, pdev_ptr},
