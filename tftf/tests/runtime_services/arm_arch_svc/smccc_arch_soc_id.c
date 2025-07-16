c869e0e59e2124e7b2ea4d1c9fb21563d6ef4c55
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2025-07-16 16:16:26 -0500

    test(smccc): add SoC Name test support to SMCCC_ARCH_SOC_ID
    This patch tests the SoC Name functionality of SMCCC_ARCH_SOC_ID
    as part of SMCCC 1.6 update.
    
    Reference: https://developer.arm.com/documentation/den0028/latest/
    
    Change-Id: Ic3be2bdf4e7268b28d392c338533f0603f0716e8
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>

--- tftf/tests/runtime_services/arm_arch_svc/smccc_arch_soc_id.c
+++ tftf/tests/runtime_services/arm_arch_svc/smccc_arch_soc_id.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Arm Limited. All rights reserved.
+ * Copyright (c) 2025, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -28,12 +28,50 @@ static smc_ret_values get_soc_id_param(u_register_t arg)
 	return ret;
 }
 
+/*
+ * Return SOC ID SMC64 parameters(SOC Name) according
+ * to argument passed
+ */
+static smc_ret_values_ext get_soc_id_smc64_param(u_register_t arg)
+{
+	smc_args_ext args;
+	smc_ret_values_ext ret = {};
+
+	memset(&args, 0, sizeof(args));
+	args.fid = SMCCC_ARCH_SOC_ID | (SMC_64 << FUNCID_CC_SHIFT);
+	args.arg1 = arg;
+	tftf_smc_no_retval_x8(&args, &ret);
+
+	return ret;
+}
+
+/* Checks if the SoC Name is a null terminated ASCII string */
+static int32_t is_soc_name_valid(char *soc_name)
+{
+	int i;
+
+	for (i = 0; i < SMCCC_SOC_NAME_LEN; i++) {
+		char ch = soc_name[i];
+		if (ch == '\0') {
+			return 0;
+		}
+		else if(ch > 0x7F) {
+			tftf_testcase_printf("Non-ASCII character detected\n");
+			return -1;
+		}
+	}
+
+	return -1;
+}
+
 /* Entry function to execute SMCCC_ARCH_SOC_ID test */
 test_result_t test_smccc_arch_soc_id(void)
 {
 	smc_args args;
 	smc_ret_values ret;
+	smc_ret_values_ext ext_ret;
 	int32_t expected_ver;
+	char soc_name[SMCCC_SOC_NAME_LEN];
 
 	/* Check if SMCCC version is at least v1.2 */
 	expected_ver = MAKE_SMCCC_VERSION(1, 2);
@@ -83,6 +121,37 @@ test_result_t test_smccc_arch_soc_id(void)
 		}
 
 		tftf_testcase_printf("SOC Ver = 0x%x\n", (int)ret.ret0);
+
+		ext_ret = get_soc_id_smc64_param(SMC_GET_SOC_NAME);
+
+		if ((int)ext_ret.ret0 == SMC_ARCH_CALL_INVAL_PARAM) {
+			tftf_testcase_printf("Invalid param passed to	\
+					SMCCC_ARCH_SOC_ID\n");
+			return TEST_RESULT_FAIL;
+		} else if ((int)ext_ret.ret0 == SMC_ARCH_CALL_NOT_SUPPORTED) {
+			tftf_testcase_printf("SOC Name is not implemented\n");
+			return TEST_RESULT_SKIPPED;
+		}
+
+		memcpy(&soc_name, &ext_ret.ret1, SMCCC_SOC_NAME_LEN);
+
+		if (is_soc_name_valid(soc_name) != 0) {
+			return TEST_RESULT_FAIL;
+		}
+
+		tftf_testcase_printf("SOC Name = %s\n",soc_name);
+
+		/*
+		 * Negative test to check if SMC_GET_SOC_NAME is present
+		 * in SMC32. This should not return SMC_ARCH_CALL_SUCCESS.
+		 */
+		ret = get_soc_id_param(SMC_GET_SOC_NAME);
+
+		if ((int)ret.ret0 == SMC_ARCH_CALL_SUCCESS) {
+			tftf_testcase_printf("SoC Name should only be present \
+					for SMC64 invocations\n");
+			return TEST_RESULT_FAIL;
+		}
 
 	} else {
 		ERROR("Invalid error during SMCCC_ARCH_FEATURES call = 0x%x\n",
