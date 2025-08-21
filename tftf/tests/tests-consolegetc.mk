5641362f379b16f55338c109514fe73830ad2fda
Slava Andrianov <slava.andrianov@arm.com>
2025-08-21 12:05:05 -0500

    feat(debugfs): expose TF-A console driver through debugfs
    Exposing the TF-A console driver via the debugfs interface enables
    easier testing of this driver via TFTF
    
    Change-Id: I0256e6a3ef608f77f096282d5e1dcdc612b9d252
    Signed-off-by: Slava Andrianov <slava.andrianov@arm.com>

---
+++ tftf/tests/tests-consolegetc.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2024, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_SOURCES	+=	tftf/tests/runtime_services/sip_service/test_consolegetc.c
